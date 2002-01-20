f02958c3995d6494e4495f862c999f2c9df4f78b
Hans Häggström <zzorn@iki.fi>
2002-01-20 21:10:07 +0000

    Added DimeService singleton, for accessing all dime services.

---
+++ src/main/DimeServices.h
@@ -0,0 +1,182 @@
+/*
+    Copyright (C) 2002  Hans Häggström
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef DIME_SERVICES_H
+#define DIME_SERVICES_H
+
+// Include headers from the current project here
+#include "TestService.h"
+
+// Include custom library headers here
+
+// Include system headers here
+
+namespace dime {
+namespace main {
+
+/**
+ * This is a singleton class that is used to access instances of all the
+ * different Dime services.
+ *
+ * There's a getServiceName() method for each service. <p>
+ *
+ * TODO: Should this class also create the instances of the services,
+ * or should it have set methods for them?  <p>
+ *
+ * Example: <p>
+ * <code>
+ *
+ *   DimeServices.getInstance()->getLoggingService()->log( message );  <br/>
+ *   ... = DimeServices.getInstance()->getMetaServerService()->getMetaServerList();
+ *
+ * </code>
+ *
+ * @author Hans Häggström
+ */
+class DimeServices
+
+{
+    //======================================================================
+    // Inner Classes
+    //======================================================================
+
+
+    //======================================================================
+    // Public Constants
+    //======================================================================
+    public:
+
+
+    //======================================================================
+    // Private Constants
+    //======================================================================
+    private:
+
+
+    //======================================================================
+    // Private Variables
+    //======================================================================
+    private:
+
+
+    /**
+     * This variable is used to keep track of the next free ID number for a new gizmo.
+     */
+    static DimeServices theInstance;
+
+
+    //======================================================================
+    // Public Methods
+    //======================================================================
+    public:
+
+
+    //----------------------------------------------------------------------
+    // Destructor
+
+    /**
+     * Deletes a DimeServices instance.
+     */
+    virtual ~DimeServices()
+    {
+        // TODO: Free any allocated resources here.
+    }
+
+
+    //----------------------------------------------------------------------
+    // Singleton
+
+    /**
+     * Returns the DimeServices instance.
+     */
+    static DimeServices *getInstance() const
+    {
+        return &theInstance;
+    }
+
+
+    //----------------------------------------------------------------------
+    // Getters
+
+    /**
+     * Returns an instance of the TestService.
+     */
+    dime::services::test::TestService *getTestService()
+    {
+        // TODO
+        return null;
+    }
+
+
+    //----------------------------------------------------------------------
+    // Setters
+
+    //----------------------------------------------------------------------
+    // Other public methods
+
+
+    //======================================================================
+    // Protected Methods
+    //======================================================================
+    protected:
+
+
+    //======================================================================
+    // Private Methods
+    //======================================================================
+    private:
+
+
+    //======================================================================
+    // Disabled constructors and operators
+    //======================================================================
+    private:
+
+    /**
+     * The constructor is private, and only used to create the singleton instance.
+     */
+    DimeServices()
+    {
+    }
+
+
+    /**
+     * Copy constructor not provided.
+     */
+    DimeServices( const DimeServices &source )
+    {
+    }
+
+
+    /**
+     * Assignment operator not provided.
+     */
+    DimeServices &operator= ( const DimeServices &source )
+    {
+        return &this;
+    }
+
+
+}; // End of class
+
+} // End of subsystem namespace
+} // End of application namespace
+
+#endif
+
+
