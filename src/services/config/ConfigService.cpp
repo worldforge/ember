8c578fc8779bff6f81db394c08aaedd6f6aa6587
Miguel Guzmán <aglanor@gmail.com>
2002-01-25 01:19:43 +0000

    basic design for the Dime Configuration Service, and a sample configuration file

---
+++ src/services/config/ConfigService.cpp
@@ -0,0 +1,56 @@
+#include "ConfigService.h"
+
+#include <iostream.h>
+
+namespace dime
+{
+   namespace services
+     {
+	namespace config
+	  {
+
+	/* ctor */
+	ConfigService::ConfigService()
+	{
+		//myTestMessage = "Hello WorldForge !!!";
+		  //setDescription("I am a simple test service");
+		  // setActive = 0;	// inactive
+		  //setStatus(0);	// stopped
+
+        //setName("Test Service");
+        //setDescription("Basic test service for testing and debugging.");
+// TODO(zzorn, 2002-01-19): Set the status of the service to OK.
+//        setStatus( Service::Status::OK );
+
+	}
+
+	/* Method for starting the test service 	*/
+	int ConfigService::start()
+	{
+		setStatus(1);
+        setRunning( true );
+		// TODO: has to call the Persistance Service and load user configuration data into the hashtable (agla)
+		return 0;
+	}
+
+	/* Interface method for stopping the test service 	*/
+	void ConfigService::stop(int code)
+	{
+		// TODO: stop the service, save any data that might be unsaved (agla)
+	}
+
+	void getConfig(std::string StringKey)   // what the hell should this return?
+	{
+		// TODO: implement. Easier said than done :)  (agla)
+	}
+	
+	bool ConfigService::saveConfig()
+	{
+		// TODO: call the Persistance Service and save the data.
+		return true;  // or false, if couldn't write to disk
+	}
+	
+		} // namespace testservice
+	} // namespace services
+} // namespace dime
+
---
+++ src/services/config/ConfigService.h
@@ -0,0 +1,84 @@
+/*
+    Copyright (C) 2002  Miguel Guzman Miranda [Aglanor]
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef CONFIGSERVICE_H
+#define CONFIGSERVICE_H
+
+#include "../AbstractService.h"
+#include <string>
+
+namespace dime {
+	namespace services {
+		namespace config {
+
+/**
+ * Dime Configuration Service
+ *
+ * @author Miguel Guzman Miranda [Aglanor]
+ *
+ * @see dime::Service
+ */
+class ConfigService: public Service
+{
+    //======================================================================
+    // Private Variables
+    //======================================================================
+    private:
+		std::string myTestMessage;
+		int myTestNumber;
+
+		
+    //----------------------------------------------------------------------
+    // Constructors & Destructor
+
+	public:
+	
+    /** Creates a new ConfigService using default values. */
+    ConfigService();
+
+
+    /** Deletes a ConfigService instance. */
+    ~ConfigService()
+    {
+    }
+
+
+    //----------------------------------------------------------------------
+    // Getters & Setters
+
+    //----------------------------------------------------------------------
+    // Methods
+	
+   int start();
+
+   void stop(int code) ;
+	
+	/**
+	 * Saves the configuration parameters to a file
+	 */
+   	bool ConfigService::saveConfig();
+
+}; //ConfigService
+
+		} // namespace config
+	} // namespace services
+} // namespace dime
+
+#endif
+
+
