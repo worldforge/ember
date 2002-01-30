9431e8ba56f9754355fed39055b6bb157c1af053
Miguel Guzmán <aglanor@gmail.com>
2002-01-30 00:26:47 +0000

    Layout of the Server Service files

---
+++ src/services/server/ServerService.h
@@ -0,0 +1,88 @@
+/*
+    Copyright (C) 2002  Miguel Guzman Miranda [Aglanor]
+    Based on YUP::Metacmd code by Adam Wendt
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef METASERVERSERVICE_H
+#define METASERVERSERVICE_H
+
+#include <framework/Service.h>
+#include <Eris/Metaserver.h>
+#include <Eris/Utils.h>
+#include <stdint.h>
+#include <sigc++/object.h>
+#include <Eris/Metaserver.h>
+#include <Eris/ServerInfo.h>
+#include <Eris/Utils.h>
+
+namespace dime {
+
+/**
+ * Dime Metaserver Service
+ *
+ * @author Miguel Guzman Miranda [Aglanor]
+ *
+ * @see dime::Service
+ * @see dime::ServerService
+ */
+class MetaserverService: public Service, public SigC::Object
+{
+    //======================================================================
+    // Private Variables
+    //======================================================================
+    private:
+
+    Eris::Meta * msrv;
+    //Eris::Serverlist serverlist;
+    bool listed;
+
+		
+    //----------------------------------------------------------------------
+    // Constructors & Destructor
+
+	public:
+	
+    /** Creates a new ConfigService using default values. */
+    MetaserverService();
+
+
+    /** Deletes a ConfigService instance. */
+    ~MetaserverService();
+
+    //----------------------------------------------------------------------
+    // Getters & Setters
+
+    //----------------------------------------------------------------------
+    // Methods
+	
+	int start();
+
+	void stop(int code) ;
+
+   	void GotServerCount(int count);
+	
+	void GotFailure(string msg);
+	
+	void poll();
+
+}; //MetaserverService
+
+} // namespace dime
+
+#endif
+
+
