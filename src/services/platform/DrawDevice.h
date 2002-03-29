fdcd9910b678d8b5c50c945d3ec320032355911e
Lakin Wecker <nikal@nucleus.com>
2002-03-29 22:37:01 +0000

    Added Color.h which is a simple Color encapsulation allowing division multiplication addition and subtraction.  Updated DrawDevice.h and SDLDrawDevice.* to do Gradients and to use the Color class

--- src/services/platform/DrawDevice.h
+++ src/services/platform/DrawDevice.h
@@ -1,37 +1,88 @@
 /*
-    Copyright (C) 2002  Lakin Wecker
+  Copyright (C) 2002  Lakin Wecker
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#include <SDL/SDL.h>
 
 #ifndef DRAWDEVICE_H
 #define DRAWDEVICE_H
 
+#include "Color.h"
+
+#include <SDL/SDL.h>
+
+namespace dime {
+    
 class DrawDevice {
+
+    /**
+     * Interface for DrawDevice.
+     *
+     * @author Lakin Wecker
+     *
+     */
+
+
+    //======================================================================
+    // Public Methods
+    //======================================================================
 	
-  public:
+public:
+    /**
+     * Creates a new DrawDevice using default values.
+     */
     DrawDevice(){}
-	    
+    
+    /**
+     * Virtual destructor
+     */
     virtual ~DrawDevice(){}
-    virtual void drawPixel(int x, int y, Uint32 color) = 0;
-    virtual void drawLine(int x1, int y1, int x2, int y2, Uint32 color) = 0;
-    virtual void fillRect(SDL_Rect *destRect, Uint32 color) = 0;
+
+    /**
+     * Draws a pixel of color at (x,y)
+     */
+    virtual void drawPixel(int x, int y, Color color) = 0;
+    
+    /**
+     * Draws a line of color from (x1,y1) to (x2,y2)
+     */
+    virtual void drawLine(int x1, int y1, int x2, int y2, Color color) = 0;
+    
+    /*
+     * Fills the rectangle destRect with color
+     */
+    virtual void fillRect(SDL_Rect *destRect, Color color) = 0;
+    
+    /*
+     * Fills destRect with a gradient produces from the four colors
+     *;/
+    virtual void gradientRect(SDL_Rect *destRect,
+                              Color leftTop, 
+                              Color rightTop, 
+                              Color leftBottom, 
+                              Color rightBottom) = 0;
+                 
+    /*
+    * blits the surface.  if either srcRect is null it blits the entire surface,
+    * if destRect is null then it starts blitting at (0,0)
+    */
     virtual void blitSurface(SDL_Rect *srcRect, SDL_Rect *destRect, SDL_Surface *src) = 0;
     virtual void update() = 0;
 };
+ 
+}
 
 #endif
--- src/services/platform/SDLDrawDevice.cpp
+++ src/services/platform/SDLDrawDevice.cpp
@@ -1,19 +1,19 @@
-/*
-    Copyright (C) 2002  Lakin Wecker
+/* 
+   Copyright (C) 2002  Lakin Wecker
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 
@@ -24,109 +24,157 @@
 // the algo for this is stolen from Frederic Ntwaniga's code, (a good friend of mine)
 // and I believe he stole it from the SDL Documentation site.
 
-void SDLDrawDevice::drawPixel(int x, int y, Uint32 color)
+void SDLDrawDevice::drawPixel(int x, int y, Color color)
 {
-  if ( SDL_MUSTLOCK(mySurface) ) {
-    if ( SDL_LockSurface(mySurface) < 0 ) {
-      return;
-    }
-  }  
-  switch (mySurface->format->BytesPerPixel) {
-  case 1: { /* Assuming 8-bpp */
-    Uint8 *bufp;
-    bufp = (Uint8 *)mySurface->pixels + y*mySurface->pitch + x;
-    *bufp = color;
-  }
-  break;
-  
-  case 2: { /* Probably 15-bpp or 16-bpp */
-    Uint16 *bufp;
-    bufp = (Uint16 *)mySurface->pixels + y*mySurface->pitch/2 + x;
-    *bufp = color;
-  }
-  break;
-  
-  case 3: { /* Slow 24-bpp mode, usually not used */
-    Uint8 *bufp;
-    
-    bufp = (Uint8 *)mySurface->pixels + y*mySurface->pitch + x * 3;
-    if(SDL_BYTEORDER == SDL_LIL_ENDIAN) {
-      bufp[0] = color;
-      bufp[1] = color >> 8;
-      bufp[2] = color >> 16;
-    } else {
-      bufp[2] = color;
-      bufp[1] = color >> 8;
-      bufp[0] = color >> 16;
-    }
-  }
-  break;
-  
-  case 4: { /* Probably 32-bpp */
-    Uint32 *bufp;
-    bufp = (Uint32 *)mySurface->pixels + y*mySurface->pitch/4 + x;
-    *bufp = color;
-  }
-  break;
-  }
- 
-  if ( SDL_MUSTLOCK(mySurface) ) {
-    SDL_UnlockSurface(mySurface);
-  }
+    Uint32 myColor = SDL_MapRGB(mySurface->format, 
+                                (Uint8)color.getR(), 
+                                (Uint8)color.getG(),
+                                (Uint8)color.getB());
+    if(x < mySurface->w && y < mySurface->h)
+        {
+            if ( SDL_MUSTLOCK(mySurface) ) {
+                if ( SDL_LockSurface(mySurface) < 0 ) {
+                    return;
+                }
+            }  
+            switch (mySurface->format->BytesPerPixel) {
+            case 1: { /* Assuming 8-bpp */
+                Uint8 *bufp;
+                bufp = (Uint8 *)mySurface->pixels + y*mySurface->pitch + x;
+                *bufp = myColor;
+            }
+                break;
+	  
+            case 2: { /* Probably 15-bpp or 16-bpp */
+                Uint16 *bufp;
+                bufp = (Uint16 *)mySurface->pixels + y*mySurface->pitch/2 + x;
+                *bufp = myColor;
+            }
+                break;
+	  
+            case 3: { /* Slow 24-bpp mode, usually not used */
+                Uint8 *bufp;
+	    
+                bufp = (Uint8 *)mySurface->pixels + y*mySurface->pitch + x * 3;
+                if(SDL_BYTEORDER == SDL_LIL_ENDIAN) {
+                    bufp[0] = myColor;
+                    bufp[1] = myColor >> 8;
+                    bufp[2] = myColor >> 16;
+                } else {
+                    bufp[2	] = myColor;
+                    bufp[1] = myColor >> 8;
+                    bufp[0] = myColor >> 16;
+                }
+            }
+                break;
+	  
+            case 4: { /* Probably 32-bpp */
+                Uint32 *bufp;
+                bufp = (Uint32 *)mySurface->pixels + y*mySurface->pitch/4 + x;
+                *bufp = myColor;
+            }
+                break;
+            }
+	 
+            if ( SDL_MUSTLOCK(mySurface) ) {
+                SDL_UnlockSurface(mySurface);
+            }
+        }
 }
-
 // the algo for this is stolen from Frederic Ntwaniga's code, (a good friend of mine)
 // and I believe he stole it from some university's site, credit goes out to whoever wrote this.
 
-void SDLDrawDevice::drawLine(int x1, int y1, int x2, int y2, Uint32 color)
+void SDLDrawDevice::drawLine(int x1, int y1, int x2, int y2, Color color)
 {
-  int dy;
-  int dx;
-  float t;  
-  if(x1>=mySurface->w) x1=mySurface->w-1;
-  if(x2>=mySurface->w) x2=mySurface->w-1;
-  if(y1>=mySurface->h) y1=mySurface->h-1;
-  if(y2>=mySurface->h) y2=mySurface->h-1;
+    int dy;
+    int dx;
+    float t;  
+    if(x1>=mySurface->w) x1=mySurface->w-1;
+    if(x2>=mySurface->w) x2=mySurface->w-1;
+    if(y1>=mySurface->h) y1=mySurface->h-1;
+    if(y2>=mySurface->h) y2=mySurface->h-1;
  
-  dy = y2 - y1; 
-  dx= x2 - x1;
-  t = (float) 0.5; // offset for rounding
+    dy = y2 - y1; 
+    dx= x2 - x1;
+    t = (float) 0.5; // offset for rounding
   
-  drawPixel(x1, y1,color);
-  if (abs(dx) > abs(dy)) {          // slope < 1
-    float m = (float) dy / (float) dx;      // compute slope
-    t += y1;
-    dx = (dx < 0) ? -1 : 1;
-    m *= dx;
-    while (x1 != x2) {
-      x1 += dx;                           // step to next x value
-       t += m;                             // add slope to y value
-       drawPixel(x1, (int) t,color);
+    drawPixel(x1, y1,color);
+    if (abs(dx) > abs(dy)) {          // slope < 1
+        float m = (float) dy / (float) dx;      // compute slope
+        t += y1;
+        dx = (dx < 0) ? -1 : 1;
+        m *= dx;
+        while (x1 != x2) {
+            x1 += dx;                           // step to next x value
+            t += m;                             // add slope to y value
+            drawPixel(x1, (int) t,color);
+        }
+    } else {                                    // slope >= 1
+        float m = (float) dx / (float) dy;      // compute slope
+        t += x1;
+        dy = (dy < 0) ? -1 : 1;
+        m *= dy;
+        while (y1 != y2) {
+            y1 += dy;                           // step to next y value
+            t += m;                             // add slope to x value
+            drawPixel((int) t, y1,color);
+        }
     }
-  } else {                                    // slope >= 1
-    float m = (float) dx / (float) dy;      // compute slope
-    t += x1;
-    dy = (dy < 0) ? -1 : 1;
-    m *= dy;
-    while (y1 != y2) {
-      y1 += dy;                           // step to next y value
-      t += m;                             // add slope to x value
-      drawPixel((int) t, y1,color);
-    }
-  }
 }
 
 void SDLDrawDevice::blitSurface(SDL_Rect *srcRect, SDL_Rect *destRect, SDL_Surface *src)
 {
-  SDL_BlitSurface(src, srcRect, mySurface, destRect);
+    SDL_BlitSurface(src, srcRect, mySurface, destRect);
 }
 
 void SDLDrawDevice::update()
 {
-  SDL_UpdateRect(mySurface, 0, 0, mySurface->w, mySurface->h);
+    SDL_UpdateRect(mySurface, 0, 0, mySurface->w, mySurface->h);
 }
 
-void SDLDrawDevice::fillRect(SDL_Rect *destRect, Uint32 color)
+void SDLDrawDevice::fillRect(SDL_Rect *destRect, Color color)
 {
-  SDL_FillRect(mySurface, destRect, color);
+    Uint32 myColor = SDL_MapRGB(mySurface->format,
+                                (unsigned char)color.getR(),
+                                (unsigned char)color.getG(), 
+                                (unsigned char)color.getB());
+    SDL_FillRect(mySurface, destRect, myColor);
+}
+
+void SDLDrawDevice::gradientRect(SDL_Rect *destRect, 
+                                 Color leftTop, 
+                                 Color rightTop, 
+                                 Color leftBottom, 
+                                 Color rightBottom)
+{
+	int x1 = destRect->x;
+	int x2 = destRect->x+destRect->w;
+	int y1 = destRect->y;
+	int y2 = destRect->y+destRect->h;
+ 	int width   = x2 - x1;
+	int height  = y2 - y1; 
+
+	Color leftDelta = (leftBottom - leftTop) / height * 1.0;
+	Color rightDelta = (rightBottom - rightTop) / height * 1.0;
+
+	Color leftColor  = leftTop;
+	Color rightColor = rightTop; 
+
+	Color pixelColor;
+	Color pixelDelta;
+
+	for (int y = y1; y <= y2; ++y)
+        { 
+            pixelDelta = (rightColor - leftColor) / width;
+            pixelColor = leftColor;
+	
+            for (int x = x1; x <= x2; ++x )
+                { 
+                    drawPixel( x, y, pixelColor );
+                    pixelColor = pixelColor + pixelDelta;
+                } 
+	
+            leftColor = leftColor + leftDelta;
+            rightColor = rightColor + rightDelta;
+        }    					      						      	
 }
