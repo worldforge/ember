0c19b3ed0c3bc8131228214b965ae1ea6ce9ed96
Martin Pollard <xmp@blueyonder.co.uk>
2002-05-04 20:13:24 +0000

    Set up for fontrenderer

---
+++ src/services/font/FontRenderer.h
@@ -0,0 +1,172 @@
+/*
+    Copyright (C) 2002  Nikal
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef FONTRENDERER_H
+#define FONTRENDERER_H
+
+// Included headers from the current project
+
+// Included custom library headers
+
+// Included system headers
+
+
+namespace dime {
+
+/**
+ * Short sentence with a general description of the class, ending in period.
+ *
+ * More detailed description of the class, it's purpose, what it does,
+ * how to use it and so on.
+ *
+ * A short piece of example code demonstarting how this class it is used,
+ * and in what context, is encouraged.
+ *
+ * @author Nikal
+ * @author Xmp (Martin Pollard)
+ *
+ * NOTE: You can also specify the author for individual methods
+ * if different persons have created them.
+ * It is also possible to have multiple @author tags for a method.
+ * Only add yourself as an @author if you have done serious work
+ * on a class/method, and can help fixing bugs in it, etc.
+ * If you just fixed a bug or added a short code snipplet you
+ * don't need to add yourself.
+ *
+ * @see OtherSubsystem::AnOtherRelatedClass
+ *
+ * NOTE: Add other related classes here, doxygen will create links to them.
+ */
+class FontRenderer
+
+{
+    //======================================================================
+    // Inner Classes, Typedefs, and Enums
+    //======================================================================
+    public:
+
+
+    //======================================================================
+    // Public Constants
+    //======================================================================
+    public:
+
+
+    //======================================================================
+    // Private Constants
+    //======================================================================
+    private:
+
+
+    //======================================================================
+    // Private Variables
+    //======================================================================/
+    private:
+
+    // NOTE: Class variables are prefixed with "my", static variables are
+    //       prefixed with "the".
+
+
+
+    //======================================================================
+    // Public Methods
+    //======================================================================
+    public:
+
+    //----------------------------------------------------------------------
+    // Constructors
+
+    /**
+     * Creates a new FontRenderer using default values.
+     */
+    FontRenderer()
+    {
+    }
+
+
+    /**
+     * Copy constructor.
+     */
+    FontRenderer( const FontRenderer &source )
+    {
+        // Use assignment operator to do the copy
+        // NOTE: If you need to do custom initialization in the constructor this may not be enough.
+        *this = source;
+    }
+
+
+    /**
+     * Assignment operator.
+     */
+    FontRenderer &operator= ( const FontRenderer &source )
+    {
+        // Copy fields from source class to this class here.
+
+        // Return this object with new value
+        return *this;
+    }
+
+
+    //----------------------------------------------------------------------
+    // Destructor
+
+    /**
+     * Deletes a FontRenderer instance.
+     */
+    virtual ~FontRenderer()
+    {
+        // TODO: Free any allocated resources here.
+    }
+
+
+    //----------------------------------------------------------------------
+    // Getters
+
+
+
+    //----------------------------------------------------------------------
+    // Setters
+
+    //----------------------------------------------------------------------
+    // Other public methods
+
+
+    //======================================================================
+    // Protected Methods
+    //======================================================================
+    protected:
+
+
+    //======================================================================
+    // Private Methods
+    //======================================================================
+    private:
+
+
+    //======================================================================
+    // Disabled constructors and operators
+    //======================================================================
+    private:
+
+}; // End of class
+
+} // End of application namespace
+
+#endif
+
+
