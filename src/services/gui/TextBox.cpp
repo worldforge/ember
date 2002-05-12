2d96570ddc4d8ad843879c9c75d72318bf097a1a
Martin Pollard <xmp@blueyonder.co.uk>
2002-05-12 12:27:19 +0000

    moved keypress to cpp file now it's grown too big

--- src/services/gui/TextBox.cpp
+++ src/services/gui/TextBox.cpp
@@ -1,6 +1,8 @@
 #include "TextBox.h"
 
-int dime::TextBox::draw(DrawDevice *target)
+using namespace dime;
+
+int TextBox::draw(DrawDevice *target)
 {
 	// Draw the background rect
 	myBackRect.render(target);
@@ -9,4 +11,40 @@ int dime::TextBox::draw(DrawDevice *target)
 	Label::draw(target);
 	//TODO: what should be returned here?
 	return 0;
+}
+
+virtual bool TextBox::keyPress( KeyPressEvent *event)
+ { 
+    if (event->getState() == KeyPressEvent::PRESSED)
+        {
+		std::string myText = Label::getText();
+            int i = 0;
+            int w;
+            std::string fittedText;
+
+            if(event->getSDLKey() == SDLK_BACKSPACE)
+                {
+                    if(myText.length() > 0)
+                        {
+                            myText = myText.substr(0, myText.length()-1);
+                        }
+                }
+            else
+                {
+                    myText += event->getKey();
+                }
+            Label::setText(myText);
+            Font *font = myFontRenderer->getFont();
+            font->sizeText(myText.substr(i, myText.length()-i), &w, NULL);
+            
+            while(w > Widget::getRectangle().getWidth())
+                {
+                    ++i;
+                    font->sizeText(myText.substr(i, myText.length()-i), &w, NULL);
+                }
+            fittedText = myText.substr(i, myText.length()-i);
+            
+            myFontRenderer->setText(fittedText);
+        }
+    return true;        
 }
--- src/services/gui/TextBox.h
+++ src/services/gui/TextBox.h
@@ -151,41 +151,8 @@ class TextBox : public Label
     /**
      * Takes keyPresses and feeds them into the textbuffer.
      */
-    virtual bool keyPress( KeyPressEvent *event)
-    { 
-        if (event->getState() == KeyPressEvent::PRESSED)
-            {
-			std::string myText = Label::getText();
-                int i = 0;
-                int w;
-                std::string fittedText;
+    virtual bool keyPress( KeyPressEvent *event);
 
-                if(event->getSDLKey() == SDLK_BACKSPACE)
-                    {
-                        if(myText.length() > 0)
-                            {
-                                myText = myText.substr(0, myText.length()-1);
-                            }
-                    }
-                else
-                    {
-                        myText += event->getKey();
-                    }
-                Label::setText(myText);
-                Font *font = myFontRenderer->getFont();
-                font->sizeText(myText.substr(i, myText.length()-i), &w, NULL);
-                
-                while(w > Widget::getRectangle().getWidth())
-                    {
-                        ++i;
-                        font->sizeText(myText.substr(i, myText.length()-i), &w, NULL);
-                    }
-                fittedText = myText.substr(i, myText.length()-i);
-                
-                myFontRenderer->setText(fittedText);
-            }
-        return true;        
-    }
     virtual void mouseUp( MouseButtonEvent* event) {
         Widget::onMouseUp(event);
     }
