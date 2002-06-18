dab6019d50ac22d4dfcb84ee5c807d4426b05fee
Michael Koch <konqueror@gmx.de>
2002-06-18 12:27:31 +0000

    only use "-I /usr/local/share/aclocal" is present

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
 rm -f config.cache
-aclocal -I /usr/local/share/aclocal
+
+if test -d /usr/local/share/aclocal ; then
+	ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I /usr/local/share/aclocal"
+fi
+aclocal $ACLOCAL_FLAGS
 libtoolize --force --copy
 autoheader
 automake --foreign --add-missing --copy
