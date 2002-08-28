5b086b4b17a994096ae3555cb15eee3485bdb0dd
Martin Pollard <xmp@blueyonder.co.uk>
2002-08-28 18:15:15 +0000

    -Add a few copyright notices that went astray -Get rid of a few header includes

--- src/main/Ember.cpp
+++ src/main/Ember.cpp
@@ -1,3 +1,21 @@
+/*
+    Copyright (C) 2001,2002  Martin Pollard (Xmp), Lakin Weckerd (nikal)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #include "Application.h"
 #include <test/TestServices.h>
 #include <main/DimeServices.h>
@@ -8,8 +26,7 @@
 #include <services/gui/TextBox.h>
 #include <services/gui/Console.h>
 #include <framework/Exception.h>
-//#include <SDL/SDL_keysym.h>
-#include <iostream>
+
 
 void onMouseButtonDown(dime::MouseButtonEvent *event)
 {
