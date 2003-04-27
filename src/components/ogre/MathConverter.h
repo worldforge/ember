3e476087c3b9b85522e9936e275038b82b51e0f8
Miguel Guzmán <aglanor@gmail.com>
2003-04-27 23:53:06 +0000

    skipped this file in the cvs adds

---
+++ src/components/ogre/MathConverter.h
@@ -0,0 +1,110 @@
+/*
+-----------------------------------------------------------------------------
+MathConverter.h by Miguel Guzman Miranda (Aglanor)
+
+Copyright © 2003 The Worldforge Project (http://www.worldforge.org)
+This file is part of Dime client (http://www.worldforge.org/dev/eng/clients/dime)
+
+This code is distributed under the GNU GPL (General Public License).
+See file COPYING for details.
+
+ *  Change History (most recent first):
+ *
+ *      $Log$
+ *      Revision 1.1  2003-04-27 23:53:06  aglanor
+ *      skipped this file in the cvs adds
+ *
+
+-----------------------------------------------------------------------------
+*/
+/*
+-----------------------------------------------------------------------------
+Filename:    MathConverter.h
+Description:	Point, Vector and Quaternion converter
+	for world-centric coordinates (Atlas-wfmath like)
+	to/from screen-centric coordinates (Ogre like).
+
+	Atlas is World-centric (picture a map):
+		x is east
+		y is north
+		z is up
+
+	Ogre is screen-centric (picture your monitor screen):
+		x is right
+		y is up
+		z is depth (negative towards the screen,
+			positive is towards you,
+			so the coord system is also dextrogyrous)
+
+	--------------------------------------------------------
+	Example of Atlas --> Ogre conversion
+
+	Picture this is the world map, in both cases:
+	^ North
+	|
+	|     East
+	(Up)--->
+
+
+	Atlas			Ogre
+
+	^ a.y			^ -o.z
+	|			|
+	|     a.x		|   o.x
+	(a.z)--->		(o.y)--->
+
+	--------------------------------------------------------
+	Example of Ogre --> Atlas conversion
+
+	Picture this is your computer screen, in both cases:
+	^ Up
+	|
+	|      Left
+	(You)--->
+
+	Ogre			Atlas
+
+	^ o.y			^ a.z
+	|			|
+	|     o.x		|      a.x
+	(o.z)--->		(-a.y)--->
+
+	The math is:
+
+	Atlas.x = Ogre.x
+	Atlas.y = -Ogre.z
+	Atlas.z = Ogre.y
+
+	Ogre.x = Atlas.x
+	Ogre.y = Atlas.z
+	Ogre.z = -Atlas.y
+
+-----------------------------------------------------------------------------
+*/
+
+#ifndef __MATH_CONVERTER_H__
+#define __MATH_CONVERTER_H__
+
+// ------------------------------
+// Include Ogre header files
+// ------------------------------
+#include <Ogre.h>
+
+// ------------------------------
+// Include WFmath header files
+// ------------------------------
+#include <wfmath/point.h>
+#include <wfmath/vector.h>
+#include <wfmath/axisbox.h>
+#include <wfmath/quaternion.h>
+
+inline WFMath::Point<3> Ogre2Atlas(Ogre::Vector3 p) {
+	return WFMath::Point<3>(p.x,-p.z,p.y);
+}
+
+inline Ogre::Vector3 Atlas2Ogre(WFMath::Point<3> p){
+	return Ogre::Vector3(p.x(),p.z(),-p.y());
+}
+
+
+#endif
