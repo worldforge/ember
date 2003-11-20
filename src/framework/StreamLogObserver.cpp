39dc983e4810140ae51f097b8555b4bf05f319bb
Lakin Wecker <nikal@nucleus.com>
2003-11-20 05:59:22 +0000

    Adding StreamLogObserver to the framework.  StreamLogObserver will log messages to any ostream. This can be used for files, or for std::cerr, etc.

---
+++ src/framework/StreamLogObserver.cpp
@@ -0,0 +1,101 @@
+/*
+ Copyright (C) 2002  Lakin Wecker
+	
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. 
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "StreamLogObserver.h"
+#include <ctime>
+
+namespace dime {
+    /**
+     * Creates a new StreamLogObserver using default values.
+     */
+    StreamLogObserver::StreamLogObserver(std::ostream &out) 
+        : myOut(out)
+    {
+    }
+
+    /**
+     * Copy constructor.
+     */
+    StreamLogObserver::StreamLogObserver( const StreamLogObserver &source )
+        : myOut(source.myOut)
+        {
+        }
+
+    //----------------------------------------------------------------------
+    // Destructor
+
+    /**
+     * Deletes a StreamLogObserver instance.
+     */
+    StreamLogObserver::~StreamLogObserver () {}
+
+    //----------------------------------------------------------------------
+    // Implemented methods from LoggingService::Observer
+
+    /**
+     * Prints out the message provided with file, line and datestamp to myOut;
+     */
+    void StreamLogObserver::onNewMessage(const std::string & message, const std::string & file, const int & line, 
+                                                 const dime::LoggingService::MessageImportance & importance, const time_t & timeStamp)
+    {
+        tm * ctm = localtime(&timeStamp); //currentLocalTime was too long, sorry
+            
+        myOut.fill('0');
+        myOut << "[";
+        myOut.width(2);		
+        myOut << (ctm->tm_year/*+1900*/)%100 << "-";
+        myOut.width(2);					
+        myOut << ctm->tm_mon+1 << "-";
+        myOut.width(2);			
+        myOut << ctm->tm_mday << " ";
+        myOut.width(2);
+        myOut << ctm->tm_hour << ":";
+        myOut.width(2);
+        myOut <<  ctm->tm_min << ":";
+        myOut.width(2);			
+        myOut << ctm->tm_sec << "] ";			
+        myOut  << "[File: " << file << ", Line #:" <<  line << "] (";
+
+        if(importance == dime::LoggingService::CRITICAL)
+            {
+                myOut << "CRITICAL";
+            }
+        else  if(importance == dime::LoggingService::FAILURE)
+            {
+                myOut << "FAILURE";
+            } 
+        else if(importance == dime::LoggingService::WARNING)
+            {
+                myOut << "WARNING";
+            }
+        else if(importance == dime::LoggingService::INFO)
+            {
+                myOut << "INFO";
+            }
+        else
+            {
+                myOut << "VERBOSE";
+            }
+        myOut << ") " <<message << std::endl;
+
+    }
+}; //end namespace dime
---
+++ src/framework/StreamLogObserver.h
@@ -0,0 +1,109 @@
+/*
+ Copyright (C) 2002  Lakin Wecker
+	
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. 
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef STREAMLOGOBSERVER_H
+#define STREAMLOGOBSERVER_H
+
+// Included headers from the current project
+#include "services/logging/LoggingService.h"
+
+// Included custom library headers
+
+// Included system headers
+#include <iostream>
+
+namespace dime {
+
+/**
+ * Log Observer which logs stuff to the stream provided on creation.
+ *
+ * This log observer takes an arbitrary ostream at it's creation
+ * and logs all messages sent to it to this stream.
+ * 
+ * LoggingService *logging = DimeServices::getInstance()->getLoggingService();
+ * //Create log observer that prints everything to cerr
+ * StreamLogObserver* obs = new StreamLogObserver(std::cerr);
+ * obs->setFilter(LoggingService::VERBOSE);
+ * logging->addObserver(obs);
+ *
+ * @author Lakin Wecker aka nikal
+ *
+ * @see LoggingService::Observer
+ */
+
+class StreamLogObserver : public dime::LoggingService::Observer
+{
+
+    //======================================================================
+    // Private Variables
+    //======================================================================/
+    private:
+
+        std::ostream &myOut;
+
+    //======================================================================
+    // Public Methods
+    //======================================================================
+    public:
+
+    //----------------------------------------------------------------------
+    // Constructors
+
+    /**
+     * Creates a new StreamLogObserver using default values.
+     */
+     StreamLogObserver(std::ostream &out);
+
+    /**
+     * Copy constructor.
+     */
+    StreamLogObserver( const StreamLogObserver &source );
+
+
+
+
+    //----------------------------------------------------------------------
+    // Destructor
+
+    /**
+     * Deletes a StreamLogObserver instance.
+     */
+    virtual ~StreamLogObserver ();
+
+    //----------------------------------------------------------------------
+    // Implmented methods from LogginService::Observer
+
+    /**
+     * Prints out the message provided with file, line and datestamp to myOut;
+     */
+    virtual void onNewMessage(const std::string & message, const std::string & file, const int & line, 
+                                  const dime::LoggingService::MessageImportance & importance, const time_t & timeStamp);
+
+    //----------------------------------------------------------------------
+    // Disable Assignment operator
+    private:
+    /**
+     * Disabled Assignment operator.
+     */
+    StreamLogObserver &operator= ( const StreamLogObserver &source );
+
+}; // End of StreamLogObserver
+
+} // End of dime namespace
+
+#endif
