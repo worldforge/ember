f2eabbe4f79b98164fee9330fdf29ea437e8bc95
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2004-10-06 22:32:00 +0000

    2004-10-06 Erik Hjortsberg <erik@hysteriskt.nu>
    *Changed order of inclusion of headers, since Xercesc must be included before Ogre when compiling in debug mode.

--- src/components/ogre/EmberEntityFactory.cpp
+++ src/components/ogre/EmberEntityFactory.cpp
@@ -19,7 +19,7 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
-
+#include "Model.h"
 #include "services/server/ServerService.h"
 #include "services/DimeServices.h"
 
@@ -35,7 +35,7 @@
 #include "AvatarDimeEntity.h"
 #include "DimeEntityFactory.h"
 #include "WorldDimeEntity.h"
-#include "Model.h"
+
 
 namespace DimeOgre {
 
