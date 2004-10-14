173445505ed73ca68ee52cf562d7e7b7601abe11
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2004-10-14 00:11:46 +0000

    2004-10-14 Erik Hjortsberg <erik@hysteriskt.nu>
    * more features for the server browser, refreshing now works, though the "connect" button won't do anything. When getting a connection to a server the widget disappears. Also renamed it.

---
+++ src/components/ogre/widgets/ServerBrowserWidget.cpp
@@ -0,0 +1,154 @@
+//
+// C++ Implementation: ServerBrowser
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2004
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+
+#include <elements/CEGUIListboxItem.h> 
+#include <elements/CEGUIListboxTextItem.h> 
+#include "services/DimeServices.h"
+#include "services/server/ServerService.h"
+#include "GUIManager.h"
+#include "ServerBrowserWidget.h"
+
+namespace DimeOgre {
+
+class MyListItem : public CEGUI::ListboxTextItem
+{
+public:
+	MyListItem(const CEGUI::String& text) : ListboxTextItem(text)
+	{
+		setSelectionBrushImage((CEGUI::utf8*)"TaharezLook", (CEGUI::utf8*)"MultiListSelectionBrush");
+	}
+};
+
+ServerBrowserWidget::ServerBrowserWidget(GUIManager* guiManager) :  Widget::Widget(guiManager)
+{
+}
+
+
+ServerBrowserWidget::~ServerBrowserWidget()
+{
+	delete mMainWindow;
+}
+
+void ServerBrowserWidget::buildWidget()
+{
+
+
+	mMainWindow = CEGUI::WindowManager::getSingleton().loadWindowLayout((CEGUI::utf8*)"widgets/ServerBrowserWidget.xml");
+
+	mServerList = static_cast<CEGUI::MultiColumnList*>(mMainWindow->getChild((CEGUI::utf8*)"ServerBrowser/ServerList"));
+
+	mServerList->addColumn((CEGUI::utf8*)"Server Name", 0, 0.2f);
+	mServerList->addColumn((CEGUI::utf8*)"Address", 1, 0.2f);
+	mServerList->addColumn((CEGUI::utf8*)"Ping", 2, 0.1f);
+	mServerList->addColumn((CEGUI::utf8*)"Clients", 3, 0.1f);
+	mServerList->addColumn((CEGUI::utf8*)"Ruleset", 4, 0.2f);
+	mServerList->addColumn((CEGUI::utf8*)"Server type", 5, 0.2f);
+	mServerList->setSelectionMode(CEGUI::MultiColumnList::RowSingle);
+
+	CEGUI::PushButton* refresh = static_cast<CEGUI::PushButton*>(mMainWindow->getChild((CEGUI::utf8*)"ServerBrowser/Refresh"));
+	refresh->subscribeEvent(CEGUI::ButtonBase::EventMouseClick, 
+		boost::bind(&ServerBrowserWidget::Refresh_Click, this, _1));
+	
+	
+	getMainSheet()->addChildWindow(mMainWindow); 
+
+	
+	dime::DimeServices::getInstance()->getServerService()->GotConnection.connect(SigC::slot(*this, &ServerBrowserWidget::connectedToServer));
+
+		
+	connectToServer();
+	metaServer->refresh();
+
+
+}
+
+void ServerBrowserWidget::connectedToServer(Eris::Connection* connection) 
+{
+	mGuiManager->removeWidget(this);
+	delete this;
+}
+	
+
+bool ServerBrowserWidget::Refresh_Click(const CEGUI::EventArgs& args)
+{
+	mServerList->resetList();
+	metaServer->refresh();
+	return true;
+
+}
+
+void ServerBrowserWidget::connectToServer()
+{
+    metaServer = new Eris::Meta("ember", "metaserver.worldforge.org", 10);
+    metaServer->GotServerCount.connect(SigC::slot(*this, &ServerBrowserWidget::gotServerCount));
+    metaServer->Failure.connect(SigC::slot(*this, &ServerBrowserWidget::gotFailure));
+    metaServer->ReceivedServerInfo.connect(SigC::slot(*this, &ServerBrowserWidget::receivedServerInfo));
+    metaServer->CompletedServerList.connect(SigC::slot(*this, &ServerBrowserWidget::completedServerList));
+}
+
+void ServerBrowserWidget::gotServerCount(int count)
+{
+}
+	
+void ServerBrowserWidget::gotFailure(const std::string& msg)
+{
+}
+
+
+
+
+void ServerBrowserWidget::receivedServerInfo(const Eris::ServerInfo& sInfo)
+{
+	mGuiManager->setDebugText("Got server info.");
+	
+	int rowNumber = mServerList->getRowCount();
+	mServerList->addRow();
+	
+	MyListItem* item = new MyListItem(CEGUI::String(sInfo.getServername()));
+//	item->setUserData(&sInfo);
+	
+	
+	mServerList->setItem(item, 0, rowNumber);
+	mServerList->setItem(new MyListItem(CEGUI::String(sInfo.getHostname())), 1, rowNumber);
+	std::stringstream ss_ping;
+	ss_ping << sInfo.getPing();
+	mServerList->setItem(new MyListItem(CEGUI::String(ss_ping.str())), 2, rowNumber);
+	std::stringstream ss_clientNum;
+	ss_clientNum << sInfo.getNumClients();
+	mServerList->setItem(new MyListItem(CEGUI::String(ss_clientNum.str())), 3 ,rowNumber);
+	mServerList->setItem(new MyListItem(CEGUI::String(sInfo.getRuleset())), 4, rowNumber);
+	mServerList->setItem(new MyListItem(CEGUI::String(sInfo.getServer())), 5, rowNumber);
+	
+	
+	
+	//CEGUI::ListboxItem* item = new CEGUI::ListboxItem;
+	//item->setUserData(sInfo);
+}
+
+
+void ServerBrowserWidget::completedServerList()
+{
+}
+
+
+}
---
+++ src/components/ogre/widgets/ServerBrowserWidget.h
@@ -0,0 +1,71 @@
+//
+// C++ Interface: ServerBrowser
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2004
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef DIMEOGRESERVERBROWSER_H
+#define DIMEOGRESERVERBROWSER_H
+
+#include <Eris/Metaserver.h>
+#include <Eris/ServerInfo.h>
+#include <Eris/Utils.h>
+#include <Eris/Connection.h>
+
+#include "Widget.h"
+
+namespace DimeOgre {
+
+class GUIManager;
+/**
+@author Erik Hjortsberg
+*/
+class ServerBrowserWidget : public Widget
+{
+public:
+    ServerBrowserWidget(GUIManager* guiManager);
+
+    ~ServerBrowserWidget();
+	void buildWidget();
+
+protected:
+
+	CEGUI::MultiColumnList* mServerList;
+
+	void gotServerCount(int count);
+	
+    void gotFailure(const std::string& msg);
+
+    void receivedServerInfo(const Eris::ServerInfo& sInfo);
+	
+    void completedServerList();
+	
+	void connectToServer();
+	
+	Eris::Meta* metaServer;
+	bool Refresh_Click(const CEGUI::EventArgs& args);
+	
+	void connectedToServer(Eris::Connection* connection);
+
+
+};
+
+}
+
+#endif
