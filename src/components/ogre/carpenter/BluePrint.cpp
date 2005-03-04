a02f00993de77503fc5c29ade8e55569b528fc0a
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2005-03-04 01:24:22 +0000

    2005-03-04  Erik Hjortsberg  <erik@katastrof.nu>
    	* compile fix

--- src/components/ogre/carpenter/BluePrint.cpp
+++ src/components/ogre/carpenter/BluePrint.cpp
@@ -137,9 +137,11 @@ const std::vector< BuildingBlock*> BluePrint::getAttachedBlocks() const
 
 BuildingBlock* BluePrint::createBuildingBlock(BuildingBlockDefinition definition)
 {
-	if (definition.mName == "") {
-		definition.mName = std::string("_buildingBlock") + mBuildingBlocks.size();
-	}
+// 	if (definition.mName == "") {
+// 		std::stringstream ss;
+// 		ss << mBuildingBlocks.size();
+// 		definition.mName = std::string("_buildingBlock") + ss.str();
+// 	}
 	mBuildingBlocks[definition.mName];
 	BuildingBlockSpec *buildingBlockSpec = mCarpenter->getBuildingBlockSpec(definition.mBuildingBlockSpec);
 	mBuildingBlocks[definition.mName].mBlockDefinition = definition;
