e7ad39c9f901ddd83ef069aa91403f667073a05f
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2005-04-24 12:30:02 +0000

    2005-04-24  Erik Hjortsberg  <erik@katastrof.nu>
    	* src/services/config/ConfigService.cpp:
    		check for config.h

--- src/services/config/ConfigService.cpp
+++ src/services/config/ConfigService.cpp
@@ -17,6 +17,10 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "ConfigService.h"
 #include "services/logging/LoggingService.h"
 #include "framework/prefix.h"
