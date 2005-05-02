d78bbecd649da92c0d9e9eb855f8317c32d664d6
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2005-05-02 21:55:54 +0000

    2005-05-02  Erik Hjortsberg  <erik@katastrof.nu>
    	* src/components/ogre/widgets/Help.*: Added help widget.

---
+++ src/components/ogre/widgets/Help.h
@@ -0,0 +1,62 @@
+//
+// C++ Interface: Help
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2005
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef EMBEROGREHELP_H
+#define EMBEROGREHELP_H
+
+#include "Widget.h"
+#include "framework/ConsoleObject.h"
+#include <elements/CEGUIStaticImage.h>
+#include <CEGUIEvent.h> 
+
+namespace EmberOgre {
+class GUIManager;
+class Widget;
+
+/**
+@author Erik Hjortsberg
+*/
+class Help : public Widget
+{
+public:
+    Help();
+
+    ~Help();
+	virtual void buildWidget();
+	void runCommand(const std::string &command, const std::string &args);
+
+	static const char * const HELP;
+	
+protected:
+	CEGUI::StaticImage* mHelpIcon;
+	
+	bool Icon_Click(const CEGUI::EventArgs& args);
+	
+	void show();
+
+	void hide();
+
+};
+
+};
+
+#endif
