0669cd258dc0b9285265c3946484face78b2ccec
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2005-06-20 22:58:58 +0000

    2005-06-21  Erik Hjortsberg  <erik@katastrof.nu>
    	* src/components/ogre/EmberEntityUserObject.*: added new class to be used with mouse picking, which encapsules the entity, the model and all OgreOpcode::CollisionObjects belonging to it

---
+++ src/components/ogre/EmberEntityUserObject.cpp
@@ -0,0 +1,60 @@
+//
+// C++ Implementation: EmberEntityUserObject
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2005
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#include "ogreopcode/include/OgreCollisionObject.h"
+#include "EmberEntity.h"
+#include "model/Model.h"
+#include "EmberEntityUserObject.h"
+
+
+namespace EmberOgre {
+
+EmberEntityUserObject::EmberEntityUserObject(EmberEntity* emberEntity,  Model* model,  CollisionObjectStore collisionObjects)
+: mModel(model),
+mEmberEntity(emberEntity),
+mCollisionObjects(collisionObjects)
+{
+}
+
+// CollisionObjectStore EmberEntityUserObject::getCollisionObjects() const
+// {
+// 	return mCollisionObjects;
+// }
+
+EmberEntityUserObject::~EmberEntityUserObject()
+{
+}
+
+EmberEntity*  EmberEntityUserObject::getEmberEntity() const
+{
+	return mEmberEntity;
+}
+
+ Model*  EmberEntityUserObject::getModel() const
+{
+	return mModel;
+}
+
+
+
+
+};
---
+++ src/components/ogre/EmberEntityUserObject.h
@@ -0,0 +1,64 @@
+//
+// C++ Interface: EmberEntityUserObject
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2005
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef EMBEROGREEMBERENTITYUSEROBJECT_H
+#define EMBEROGREEMBERENTITYUSEROBJECT_H
+
+#include "EmberOgrePrerequisites.h"
+
+namespace Ogre
+{
+	class Entity;
+	class CollisionObject;
+};
+
+namespace EmberOgre {
+
+class Model;
+class EmberEntity;
+/**
+@author Erik Hjortsberg
+*/
+class EmberEntityUserObject : public Ogre::UserDefinedObject
+{
+public:
+
+	typedef std::vector<Ogre::CollisionObject*> CollisionObjectStore;
+	
+    EmberEntityUserObject(EmberEntity* emberEntity , Model* model, CollisionObjectStore collisionObject);
+
+    ~EmberEntityUserObject();
+	
+	EmberEntity* getEmberEntity() const;
+	Model* getModel() const ;
+	CollisionObjectStore* getCollisionObjects() { return &mCollisionObjects; }
+
+private:
+	EmberEntity*  mEmberEntity;
+	 Model*   mModel;
+	CollisionObjectStore   mCollisionObjects;
+
+};
+
+};
+
+#endif
