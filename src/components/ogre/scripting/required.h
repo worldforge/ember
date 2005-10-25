60614bf8f8fa63fb3d969e2bb8e3dabeac8cefb2
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2005-10-25 23:43:49 +0000

    2005-10-26  Erik Hjortsberg  <erik@katastrof.nu>
    	* src/components/ogre/scripting/*: scripting module, provides lua bindings for the main Ember system through tolua++. Very much unfinished right now, and thus disabled.

---
+++ src/components/ogre/scripting/required.h
@@ -0,0 +1,35 @@
+#include <SDL.h>
+
+#include <sigc++/sigc++.h>
+
+// #include <Eris/View.h>
+
+#include "../AvatarController.h"
+#include "../Avatar.h"
+#include "../AvatarCamera.h"
+#include "../EmberOgre.h"
+
+#include "../EmberEntityFactory.h"
+
+#include "../EmberEntity.h"
+#include "../EmberPhysicalEntity.h"
+#include "../PersonEmberEntity.h"
+#include "../AvatarEmberEntity.h"
+#include "../WorldEmberEntity.h"
+
+#include "../MousePicker.h"
+
+#include "../MotionManager.h"
+#include "../GUIManager.h"
+#include "../TerrainGenerator.h"
+
+#include "../model/Model.h"
+
+#include "../widgets/Widget.h"
+
+#include "../input/Input.h"
+
+#include "../jesus/Jesus.h"
+
+
+// Ember::EmberServices::getInstance()->getConfigService()->EventChangedConfigItem.connect(sigc::mem_fun(*this, &AvatarCamera::ConfigService_EventChangedConfigItem));
