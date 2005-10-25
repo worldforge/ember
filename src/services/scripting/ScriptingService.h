86d2ccf96b57cb41510acc157322db3fd0b433a3
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2005-10-25 23:05:16 +0000

    2005-10-26  Erik Hjortsberg  <erik@katastrof.nu>
    	* src/services/Makefile.am, src/services/scripting/Makefile.am, src/services/scripting/ScriptingService.*: new sevice for scripting

---
+++ src/services/scripting/ScriptingService.h
@@ -0,0 +1,47 @@
+//
+// C++ Interface: ScriptingService
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2005
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef EMBERSCRIPTINGSERVICE_H
+#define EMBERSCRIPTINGSERVICE_H
+
+#include <framework/Service.h>
+#include <framework/ConsoleObject.h>
+
+namespace Ember {
+
+/**
+@author Erik Hjortsberg
+*/
+class ScriptingService : public Service, public ConsoleObject
+{
+public:
+    ScriptingService();
+
+    ~ScriptingService();
+
+	virtual Service::Status start();
+
+};
+
+}
+
+#endif
