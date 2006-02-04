ad2d3d391776e742d21ae282ab5dcba8fea12103
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2006-02-04 22:50:12 +0000

    2006-02-04 Erik Hjortsberg  <erik@katastrof.nu>
        * src/components/ogre/widgets/ChatWidget.cpp, src/components/ogre/widgets/ChatWidget.h, src/components/ogre/widgets/ChatWidget.xml, src/components/ogre/widgets/DebugWidget.cpp, src/components/ogre/widgets/DebugWidget.h, src/components/ogre/widgets/DebugWidget.xml, src/components/ogre/widgets/EntityPickerWidget.cpp, src/components/ogre/widgets/EntityPickerWidget.h, src/components/ogre/widgets/GiveWidget.cpp, src/components/ogre/widgets/GiveWidget.h, src/components/ogre/widgets/GiveWidget.xml, src/components/ogre/widgets/Performance.cpp, src/components/ogre/widgets/Performance.h, src/components/ogre/widgets/Performance.widget, src/components/ogre/widgets/ServerBrowserWidget.cpp, src/components/ogre/widgets/ServerBrowserWidget.h, src/components/ogre/widgets/ServerBrowserWidget.xml, src/components/ogre/widgets/Makefile.am, src/components/ogre/widgets/WidgetDefinitions.cpp: Removed obsolete files

--- src/components/ogre/widgets/WidgetDefinitions.cpp
+++ src/components/ogre/widgets/WidgetDefinitions.cpp
@@ -23,18 +23,12 @@
 
 #include "WidgetDefinitions.h"
 
-// #include "ServerBrowserWidget.h"
 #include "ServerWidget.h"
-// #include "ChatWidget.h"
-// #include "EntityPickerWidget.h"
 #include "InventoryWidget.h"
 #include "InspectWidget.h"
 #include "MakeEntityWidget.h"
-//#include "GiveWidget.h"
-#include "DebugWidget.h"
 #include "IngameChatWidget.h"
 #include "ConsoleWidget.h"
-// #include "Performance.h"
 #include "JesusEdit.h"
 #include "Help.h"
 #include "Quit.h"
@@ -49,18 +43,12 @@ namespace EmberOgre
 WidgetDefinitions::WidgetDefinitions()
 {
 	WidgetLoader::registerWidget("Widget", &WidgetLoader::createWidgetInstance<Widget>);
-// 	WidgetLoader::registerWidget("ServerBrowserWidget", &WidgetLoader::createWidgetInstance<ServerBrowserWidget>);
 	WidgetLoader::registerWidget("ServerWidget", &WidgetLoader::createWidgetInstance<ServerWidget>);
-// 	WidgetLoader::registerWidget("ChatWidget", &WidgetLoader::createWidgetInstance<ChatWidget>);
-// 	WidgetLoader::registerWidget("EntityPickerWidget", &WidgetLoader::createWidgetInstance<EntityPickerWidget>);
 	WidgetLoader::registerWidget("InventoryWidget", &WidgetLoader::createWidgetInstance<InventoryWidget>);
 	WidgetLoader::registerWidget("InspectWidget", &WidgetLoader::createWidgetInstance<InspectWidget>);
 	WidgetLoader::registerWidget("MakeEntityWidget", &WidgetLoader::createWidgetInstance<MakeEntityWidget>);
-	//WidgetLoader::registerWidget("GiveWidget", &WidgetLoader::createWidgetInstance<GiveWidget>);
-	WidgetLoader::registerWidget("DebugWidget", &WidgetLoader::createWidgetInstance<DebugWidget>);
 	WidgetLoader::registerWidget("IngameChatWidget", &WidgetLoader::createWidgetInstance<IngameChatWidget>);
 	WidgetLoader::registerWidget("ConsoleWidget", &WidgetLoader::createWidgetInstance<ConsoleWidget>);
-// 	WidgetLoader::registerWidget("Performance", &WidgetLoader::createWidgetInstance<Performance>);
 	WidgetLoader::registerWidget("JesusEdit", &WidgetLoader::createWidgetInstance<JesusEdit>);
 	WidgetLoader::registerWidget("Help", &WidgetLoader::createWidgetInstance<Help>);
 	WidgetLoader::registerWidget("Quit", &WidgetLoader::createWidgetInstance<Quit>);
