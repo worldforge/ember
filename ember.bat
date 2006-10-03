591287bf3e09191f91cb046b0ee0dc9d3769eb34
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2006-10-03 18:28:09 +0000

    2006-10-03 Erik Hjortsberg  <erik@katastrof.nu>
    	* ember.bat: updated batch with changes from Elendrim

--- ember.bat
+++ ember.bat
@@ -1,10 +1,26 @@
 @Echo off
-rem if not exist java.exe (
-rem 	echo I couldn't find java, which is needed for the media update. Go to www.java.com to install it. Aborting.
-rem 	goto :EOF
-rem )
 set OLDDIR=%CD%
+
+:WFUT
 echo Updating media from the server, this might take a while if you have a slow connection (circa 30Mb).
 java -DserverRoot=http://amber.worldforge.org/WFUT/ -jar "WFUT.jar" dest "%APPDATA%\\Ember" update ember-media-0.4.1
+if %errorlevel% LEQ 1 goto EMBER
+GOTO ERROR
+
+:EMBER
 ember.exe
 cd %OLDDIR%
+goto END
+
+:ERROR
+echo Java is needed for the media update.
+echo I couldn't find java, or something failed when updating media.
+echo.
+echo Go to www.java.com to install Java.
+echo Aborting.
+pause
+goto END
+
+
+:END
+echo END.
