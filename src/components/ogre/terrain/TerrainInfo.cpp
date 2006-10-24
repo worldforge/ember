1460e8473d061eada78b106949380efbe0367bc4
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2006-10-24 21:50:20 +0000

    2006-10-24 Erik Hjortsberg  <erik@katastrof.nu>
    	* src/components/ogre/terrain/TerrainInfo.cpp, src/components/ogre/terrain/TerrainInfo.h: added class for holding terrain information and calculating page sizes etc.

---
+++ src/components/ogre/terrain/TerrainInfo.cpp
@@ -0,0 +1,120 @@
+//
+// C++ Implementation: TerrainInfo
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2006
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#include "TerrainInfo.h"
+
+namespace EmberOgre {
+
+
+TerrainInfo::TerrainInfo()
+:  mXminBasePoint(0), mXmaxBasePoint(0), mYminBasePoint(0), mYmaxBasePoint(0),
+	 mXminBasePointAdjusted(0), mXmaxBasePointAdjusted(0), mYminBasePointAdjusted(0), mYmaxBasePointAdjusted(0)
+{
+}
+
+
+void TerrainInfo::setBasePoint(const WFMath::Point<2>& position, const Mercator::BasePoint& basepoint)
+{
+	mXminBasePoint = std::min<WFMath::CoordType>(position.x(), mXminBasePoint);
+	mXmaxBasePoint = std::max<WFMath::CoordType>(position.x(), mXmaxBasePoint);
+	mYminBasePoint = std::min<WFMath::CoordType>(position.y(), mYminBasePoint);
+	mYmaxBasePoint = std::max<WFMath::CoordType>(position.y(), mYmaxBasePoint);
+	
+	adjustBasePointPositionDown(mXminBasePointAdjusted, mXminBasePoint);
+	adjustBasePointPositionUp(mXmaxBasePointAdjusted, mXmaxBasePoint);
+	adjustBasePointPositionDown(mYminBasePointAdjusted, mYminBasePoint);
+	adjustBasePointPositionUp(mYmaxBasePointAdjusted, mYmaxBasePoint);
+	
+	mCalculatedSize = WFMath::AxisBox<2>(WFMath::Point<2>(mXminBasePointAdjusted * 64, mYminBasePointAdjusted * 64), WFMath::Point<2>(mXmaxBasePointAdjusted * 64, mYmaxBasePointAdjusted * 64));
+	mCalculatedSegmentSize = WFMath::AxisBox<2>(WFMath::Point<2>(mXminBasePointAdjusted, mYminBasePointAdjusted ), WFMath::Point<2>(mXmaxBasePointAdjusted , mYmaxBasePointAdjusted ));
+	
+	int mercatorSegmentsPerOgrePage = pageIndicesSize / 64;
+	mCalculatedPageSize = WFMath::AxisBox<2>(WFMath::Point<2>(mXminBasePointAdjusted / mercatorSegmentsPerOgrePage, mYminBasePointAdjusted / mercatorSegmentsPerOgrePage), WFMath::Point<2>(mXmaxBasePointAdjusted / mercatorSegmentsPerOgrePage, mYmaxBasePointAdjusted / mercatorSegmentsPerOgrePage));
+}
+
+void TerrainInfo::adjustBasePointPositionUp(WFMath::CoordType& basePointPositionAdjusted, WFMath::CoordType newBasePointPosition)
+{
+	int mercatorSegmentsPerOgrePage = pageIndicesSize / 64;
+	int remainder = static_cast<int>(newBasePointPosition) % mercatorSegmentsPerOgrePage;
+	if (remainder != 0) {
+		///adjust the position
+		basePointPositionAdjusted = newBasePointPosition + (mercatorSegmentsPerOgrePage - remainder);
+	}
+}
+
+void TerrainInfo::adjustBasePointPositionDown(WFMath::CoordType& basePointPositionAdjusted, WFMath::CoordType newBasePointPosition)
+{
+	int mercatorSegmentsPerOgrePage = pageIndicesSize / 64;
+	int remainder = std::abs(static_cast<int>(newBasePointPosition) % mercatorSegmentsPerOgrePage);
+	if (remainder != 0) {
+		///adjust the position
+		basePointPositionAdjusted = newBasePointPosition - (mercatorSegmentsPerOgrePage - remainder);
+	}
+}
+
+const WFMath::AxisBox<2>& TerrainInfo::getWorldSizeInSegments() const
+{
+	return mCalculatedSegmentSize;
+}
+
+
+const WFMath::AxisBox<2>& TerrainInfo::getWorldSizeInIndices() const
+{
+	return mCalculatedSize;
+}
+
+const WFMath::AxisBox<2>& TerrainInfo::getWorldSizeInPages() const
+{
+	return mCalculatedPageSize;
+}
+
+
+double TerrainInfo::getWorldSizeX() const
+{
+	return mCalculatedSize.highCorner().x() - mCalculatedSize.lowCorner().x();
+}
+
+double TerrainInfo::getWorldSizeY() const
+{
+	return mCalculatedSize.highCorner().y() - mCalculatedSize.lowCorner().y();
+}
+
+int TerrainInfo::getTotalNumberOfPagesX() const
+{
+	return static_cast<int>(mCalculatedPageSize.highCorner().x() - mCalculatedPageSize.lowCorner().x());
+}
+int TerrainInfo::getTotalNumberOfPagesY() const
+{
+	return static_cast<int>(mCalculatedPageSize.highCorner().y() - mCalculatedPageSize.lowCorner().y());
+}
+
+int TerrainInfo::getPageOffsetX() const
+{
+	return -mCalculatedPageSize.lowCorner().x();
+}
+int TerrainInfo::getPageOffsetY() const
+{
+	return -mCalculatedPageSize.lowCorner().y();
+}
+
+
+}
---
+++ src/components/ogre/terrain/TerrainInfo.h
@@ -0,0 +1,81 @@
+//
+// C++ Interface: TerrainInfo
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2006
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef EMBEROGRETERRAININFO_H
+#define EMBEROGRETERRAININFO_H
+
+#include "../EmberOgrePrerequisites.h"
+#include <Mercator/BasePoint.h>
+
+
+namespace EmberOgre {
+
+/**
+	@author Erik Hjortsberg <erik@katastrof.nu>
+	Class holding information about the terrain.
+*/
+class TerrainInfo
+{
+public:
+	TerrainInfo();
+	double getWorldSizeX() const;
+	double getWorldSizeY() const;
+	int getTotalNumberOfPagesX()  const;
+	int getTotalNumberOfPagesY() const;
+	int getPageOffsetX() const;
+	int getPageOffsetY() const;
+	int pageIndicesSize;
+	
+/*	void setNewXMax(int xMax);
+	void setNewXMin(int xMin);
+	void setNewYMax(int yMax);
+	void setNewYMin(int yMin);*/
+	
+	void setBasePoint(const WFMath::Point<2>& position, const Mercator::BasePoint& basepoint);
+	const WFMath::AxisBox<2>& getWorldSizeInIndices() const;
+	const WFMath::AxisBox<2>& getWorldSizeInSegments() const;
+	const WFMath::AxisBox<2>& getWorldSizeInPages() const;
+	
+private:
+	
+	void adjustBasePointPositionDown(WFMath::CoordType& basePointPositionAdjusted, WFMath::CoordType newBasePointPosition);
+	
+	void adjustBasePointPositionUp(WFMath::CoordType& basePointPositionAdjusted, WFMath::CoordType newBasePointPosition);
+	
+	/**
+	* 
+	*/
+	WFMath::AxisBox<2> mCalculatedSize, mCalculatedSegmentSize, mCalculatedPageSize;
+//	int mCalculatedXMin, mCalculatedXmax, mCalculatedYmin, mCalculatedYmax;
+	
+	/**
+	 * the min and max indices for segments
+	 */
+// 	int mXmin, mXmax, mYmin, mYmax;
+	WFMath::CoordType mXminBasePoint, mXmaxBasePoint, mYminBasePoint, mYmaxBasePoint;
+	WFMath::CoordType mXminBasePointAdjusted, mXmaxBasePointAdjusted, mYminBasePointAdjusted, mYmaxBasePointAdjusted;
+
+};
+
+}
+
+#endif
