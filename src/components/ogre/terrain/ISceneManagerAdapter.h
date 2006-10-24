c07bcc7b8595c12c9a3fd74ff0a13955e159ec55
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2006-10-24 21:49:02 +0000

    2006-10-24 Erik Hjortsberg  <erik@katastrof.nu>
    	* src/components/ogre/terrain/ISceneManagerAdapter.h,src/components/ogre/terrain/Makefile.am : added interface for an adapter for a scene manager

---
+++ src/components/ogre/terrain/ISceneManagerAdapter.h
@@ -0,0 +1,53 @@
+//
+// C++ Interface: ISceneManagerAdapter
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2006
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+
+namespace Ogre
+{
+class SceneManager;
+}
+
+namespace EmberOgre {
+
+class ISceneManagerAdapter
+{
+public:
+
+	virtual int getPageSize() = 0;
+	
+	virtual void setWorldPagesDimensions(int numberOfPagesHeight, int numberOfPagesWidth) = 0;
+
+	virtual void setCamera(Ogre::Camera* camera) = 0;
+	virtual void setResourceGroupName(const std::string& groupName) = 0;
+	virtual void loadOptions(const std::string& filePath) = 0;
+	virtual void resize(Ogre::AxisAlignedBox newSize, int levels) = 0;
+	virtual void loadScene() = 0;
+	
+	virtual bool setOption(const std::string& strKey, const void* pValue) = 0;
+	virtual bool getOption(const std::string& strKey, void* pDestValue) = 0;
+	
+	virtual Ogre::SceneManager* getSceneManager() const = 0;
+	
+	virtual void reloadAllPages() = 0;
+};
+
+}
