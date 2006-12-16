03d4753b7a3f4965d0cb36aa1ea7d47454072abd
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2006-12-16 22:52:46 +0000

    2006-12-16 Erik Hjortsberg  <erik@katastrof.nu>
    	* src/components/ogre/Makefile.am, src/components/ogre/OgreInfo.cpp, src/components/ogre/OgreInfo.h: Added class for providing some Ogre information, such as if we're using indirect rendering.

---
+++ src/components/ogre/OgreInfo.cpp
@@ -0,0 +1,58 @@
+//
+// C++ Implementation: OgreInfo
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2006
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#include "OgreInfo.h"
+
+#ifdef WIN32
+#else
+	#include <GL/gl.h> 
+#endif
+
+
+namespace EmberOgre {
+
+OgreInfo::OgreInfo()
+{
+}
+
+
+OgreInfo::~OgreInfo()
+{
+}
+
+
+bool OgreInfo::isIndirect() const
+{
+#ifdef WIN32
+	///TODO: add checks for win32 too
+	return false;
+#else
+	const GLubyte* pcRenderer = glGetString(GL_RENDERER);
+	const std::string renderer((const char*)pcRenderer);
+	
+	return renderer.find("Indirect") != -1;
+#endif
+	
+}
+
+
+}
