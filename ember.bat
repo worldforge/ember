7f7a7309cdca0c8171d6fdb85207b3b969db66b6
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2007-03-12 21:31:07 +0000

    2007-03-12 Erik Hjortsberg  <erik.hjortsberg@iteam.se>
    	* ember.bat, ember.in: with libwfut there's no longer any need to use the java WFUT tool

--- ember.bat
+++ ember.bat
@@ -1,11 +1,6 @@
 @Echo off
 set OLDDIR=%CD%
 
-:WFUT
-echo Updating media from the server, this requires java.
-java -DserverRoot=http://amber.worldforge.org/WFUT/ -jar "WFUT.jar" dest "%APPDATA%\\Ember" update ember-media-0.4.2
-GOTO EMBER
-
 :EMBER
 ember.exe
 cd %OLDDIR%
