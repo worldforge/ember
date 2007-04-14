443f1fbade8e49fff0f502876daf33903e9b501a
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2007-04-14 15:23:48 +0000

    2007-04-14 Erik Hjortsberg  <erik.hjortsberg@iteam.se>
    	* src/components/ogre/OgreResourceProvider.cpp, src/components/ogre/OgreResourceProvider.h, src/components/ogre/Makefile.am: added a IResourceProvider which uses the Ogre resource system

---
+++ src/components/ogre/OgreResourceProvider.cpp
@@ -0,0 +1,93 @@
+//
+// C++ Implementation: OgreResourceProvider
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2007
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#include "OgreResourceProvider.h"
+#include "framework/Exception.h"
+
+namespace EmberOgre {
+
+OgreResourceWrapper::OgreResourceWrapper(Ogre::DataStreamPtr dataStream)
+{
+	mSize = dataStream->size();
+	mBuffer = new char[mSize];
+	dataStream->read(mBuffer, mSize);
+}
+
+OgreResourceWrapper::~OgreResourceWrapper()
+{
+	delete[] mBuffer;
+}
+
+char* OgreResourceWrapper::getDataPtr()
+{
+	return mBuffer;
+}
+
+bool OgreResourceWrapper::hasData()
+{
+	return mSize != 0;
+}
+
+size_t OgreResourceWrapper::getSize()
+{
+	return mSize;
+}
+
+
+
+OgreResourceProvider::OgreResourceProvider(const std::string& groupName)
+: mGroupName(groupName)
+{
+}
+
+
+OgreResourceProvider::~OgreResourceProvider()
+{
+}
+
+Ember::ResourceWrapper OgreResourceProvider::getResource(const std::string& name)
+{
+	Ogre::DataStreamPtr input =
+		Ogre::ResourceGroupManager::getSingleton().openResource(name.c_str(), mGroupName.c_str());
+	
+	if (input.isNull())
+	{
+		throw Ember::Exception("Unable to open resource file '" + name + "' in resource group '" + name + "'.");
+	}
+	OgreResourceWrapper* wrapper = new OgreResourceWrapper(input);
+	input->close();
+	return Ember::ResourceWrapper(wrapper, name);
+	
+// 	Ogre::String buf = input->getAsString();
+// 	const size_t memBuffSize = buf.length();
+// 	
+// 	unsigned char* mem = new unsigned char[memBuffSize];
+// 	memcpy(mem, buf.c_str(), memBuffSize);
+// 	
+// 	output.setData(mem);
+// 	output.setSize(memBuffSize);
+}
+
+
+
+
+}
---
+++ src/components/ogre/OgreResourceProvider.h
@@ -0,0 +1,65 @@
+//
+// C++ Interface: OgreResourceProvider
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik@katastrof.nu>, (C) 2007
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef EMBEROGREOGRERESOURCEPROVIDER_H
+#define EMBEROGREOGRERESOURCEPROVIDER_H
+
+#include "framework/IScriptingProvider.h"
+#include <Ogre.h>
+
+namespace EmberOgre {
+
+
+class OgreResourceWrapper : public Ember::IResourceWrapper
+{
+public:
+	OgreResourceWrapper(Ogre::DataStreamPtr dataStream);
+	virtual ~OgreResourceWrapper();
+	
+	virtual char* getDataPtr();
+	virtual bool hasData();
+	virtual size_t getSize();
+private:
+	char* mBuffer;
+	size_t mSize;
+};
+
+
+/**
+	@author Erik Hjortsberg <erik@katastrof.nu>
+*/
+class OgreResourceProvider : public Ember::IResourceProvider
+{
+public:
+    OgreResourceProvider(const std::string& groupName);
+
+    virtual ~OgreResourceProvider();
+    
+	virtual Ember::ResourceWrapper getResource(const std::string& name);
+private: 
+	std::string mGroupName;
+
+};
+
+}
+
+#endif
