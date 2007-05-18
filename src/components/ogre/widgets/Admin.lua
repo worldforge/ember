aafb25adf3dc9749fcbca343c2d43ee289cec2d2
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2007-05-18 17:21:55 +0000

    2007-05-18 Erik Hjortsberg  <erik.hjortsberg@iteam.se>
    	* src/components/ogre/widgets/Admin.layout, src/components/ogre/widgets/Admin.lua: use a StackableContainer for the positioning, also added a button for the entityBrowser

--- src/components/ogre/widgets/Admin.lua
+++ src/components/ogre/widgets/Admin.lua
@@ -5,11 +5,20 @@ function Admin.buildWidget()
 	
 	Admin.widget:loadMainSheet("Admin.layout", "Admin/")
 	
+	local innerWindow = Admin.widget:getWindow("InnerWindow")
+	if innerWindow ~= nil then
+		Admin.stackableContainer = EmberOgre.Gui.StackableContainer:new_local(innerWindow)
+		Admin.stackableContainer:setInnerContainerWindow(innerWindow)
+		Admin.stackableContainer:setFlowDirection(EmberOgre.Gui.StackableContainer.Horizontal)
+		Admin.stackableContainer:repositionWindows()
+	end
+	
 	Admin.widget:getWindow("ModelEditor"):subscribeEvent("MouseClick", "Admin.ModelEditor_Click")
 	Admin.widget:getWindow("TerrainEditor"):subscribeEvent("MouseClick", "Admin.TerrainEditor_Click")
 	Admin.widget:getWindow("SwitchCamera"):subscribeEvent("MouseClick", "Admin.SwitchCamera_Click")
 	Admin.widget:getWindow("EntityCreator"):subscribeEvent("MouseClick", "Admin.EntityCreator_Click")
 	Admin.widget:getWindow("ScriptEditor"):subscribeEvent("MouseClick", "Admin.ScriptEditor_Click")
+	Admin.widget:getWindow("EntityBrowser"):subscribeEvent("MouseClick", "Admin.EntityBrowser_Click")
 
 end
 
@@ -37,6 +46,9 @@ function Admin.ScriptEditor_Click(args)
 	console:runCommand("/show_scriptEdit")
 end
 
+function Admin.ScriptEditor_Click(args)
+	console:runCommand("/show_entityBrowser")
+end
 
 function Admin.createdAvatarEmberEntity(avatarEntity)
 	if avatarEntity:getAvatar():isAdmin() then
