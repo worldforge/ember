11243e1b1a126ab72a5c95295e0ec28f91280420
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2007-06-06 17:01:15 +0000

    2007-06-06 Erik Hjortsberg  <erik.hjortsberg@iteam.se>
    	* src/components/ogre/widgets/adapters/atlas/OrientationAdapter.cpp, src/components/ogre/widgets/adapters/atlas/OrientationAdapter.h, src/components/ogre/widgets/adapters/atlas/OrientationAdapter.layout: added an adapter for orientation

---
+++ src/components/ogre/widgets/adapters/atlas/OrientationAdapter.cpp
@@ -0,0 +1,121 @@
+//
+// C++ Implementation: OrientationAdapter
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik.hjortsberg@iteam.se>, (C) 2007
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#include "OrientationAdapter.h"
+#include <wfmath/quaternion.h>
+#include <wfmath/atlasconv.h>
+
+namespace EmberOgre {
+
+namespace Gui {
+
+namespace Adapters {
+
+namespace Atlas {
+
+OrientationAdapter::OrientationAdapter(const ::Atlas::Message::Element& element, CEGUI::Window* xWindow, CEGUI::Window* yWindow, CEGUI::Window* zWindow, CEGUI::Window* scalarWindow)
+: AdapterBase(element), mXWindow(xWindow), mYWindow(yWindow), mZWindow(zWindow), mScalarWindow(scalarWindow)
+{
+	if (mXWindow) {
+		mXWindow->subscribeEvent(CEGUI::Window::EventTextChanged, CEGUI::Event::Subscriber(&OrientationAdapter::window_TextChanged, this)); 
+	}
+	if (mYWindow) {
+		mYWindow->subscribeEvent(CEGUI::Window::EventTextChanged, CEGUI::Event::Subscriber(&OrientationAdapter::window_TextChanged, this)); 
+	}
+	if (mZWindow) {
+		mZWindow->subscribeEvent(CEGUI::Window::EventTextChanged, CEGUI::Event::Subscriber(&OrientationAdapter::window_TextChanged, this)); 
+	}
+	if (mScalarWindow) {
+		mScalarWindow->subscribeEvent(CEGUI::Window::EventTextChanged, CEGUI::Event::Subscriber(&OrientationAdapter::window_TextChanged, this)); 
+	}
+
+	
+	updateGui(mOriginalElement);
+}
+
+
+OrientationAdapter::~OrientationAdapter()
+{
+}
+
+void OrientationAdapter::updateGui(const ::Atlas::Message::Element& element)
+{
+	mSelfUpdate = true;
+	WFMath::Quaternion orientation(element);
+// 	axisBox.fromAtlas(element.asList());
+	if (mXWindow) {
+		mXWindow->setText(toString(orientation.vector().x())); 
+	}
+	if (mYWindow) {
+		mYWindow->setText(toString(orientation.vector().y())); 
+	}
+	if (mZWindow) {
+		mZWindow->setText(toString(orientation.vector().z())); 
+	}
+	if (mScalarWindow) {
+		mScalarWindow->setText(toString(orientation.scalar())); 
+	}
+
+	mSelfUpdate = false;
+}
+
+bool OrientationAdapter::window_TextChanged(const CEGUI::EventArgs& e)
+{
+	if (!mSelfUpdate) {
+		EventValueChanged.emit();
+	}
+	return true;
+}
+
+void OrientationAdapter::fillElementFromGui()
+{
+	float x(0), y(0), z(0), scalar(0);
+	if (mXWindow) {
+		x = atof(mXWindow->getText().c_str()); 
+	}
+	if (mYWindow) {
+		y = atof(mYWindow->getText().c_str()); 
+	}
+	if (mZWindow) {
+		z = atof(mZWindow->getText().c_str()); 
+	}
+	if (mScalarWindow) {
+		scalar = atof(mScalarWindow->getText().c_str()); 
+	}
+	WFMath::Quaternion orientation(scalar, x, y, z);
+	mEditedElement = orientation.toAtlas();
+}
+
+bool OrientationAdapter::_hasChanges()
+{
+	WFMath::Quaternion originalOrientation(mOriginalElement);
+	WFMath::Quaternion newOrientation(getValue());
+	return originalOrientation != newOrientation;
+}
+
+}
+
+}
+
+}
+
+}
