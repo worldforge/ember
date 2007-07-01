0f19cd8b0513ff4afb2b959f0bd357dc19fc05a8
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2007-07-01 20:02:55 +0000

    2007-07-01 Erik Hjortsberg  <erik.hjortsberg@iteam.se>
    	* src/components/ogre/scripting/bindings/lua/AvatarController.pkg, src/components/ogre/scripting/bindings/lua/lua_EmberOgre.cpp: updated bindings

--- src/components/ogre/scripting/bindings/lua/lua_EmberOgre.cpp
+++ src/components/ogre/scripting/bindings/lua/lua_EmberOgre.cpp
@@ -1,6 +1,6 @@
 /*
 ** Lua binding: EmberOgre
-** Generated automatically by tolua++-1.0.92 on Tue Jun 19 00:18:35 2007.
+** Generated automatically by tolua++-1.0.92 on Sun Jul  1 12:34:32 2007.
 */
 
 #ifndef __cplusplus
@@ -2789,6 +2789,39 @@ static int tolua_EmberOgre_EmberOgre_AvatarController_getCurrentMovement00(lua_S
 #ifndef TOLUA_RELEASE
  tolua_lerror:
  tolua_error(tolua_S,"#ferror in function 'getCurrentMovement'.",&tolua_err);
+ return 0;
+#endif
+}
+#endif //#ifndef TOLUA_DISABLE
+
+/* method: moveToPoint of class  EmberOgre::AvatarController */
+#ifndef TOLUA_DISABLE_tolua_EmberOgre_EmberOgre_AvatarController_moveToPoint00
+static int tolua_EmberOgre_EmberOgre_AvatarController_moveToPoint00(lua_State* tolua_S)
+{
+#ifndef TOLUA_RELEASE
+ tolua_Error tolua_err;
+ if (
+     !tolua_isusertype(tolua_S,1,"EmberOgre::AvatarController",0,&tolua_err) ||
+     !tolua_isusertype(tolua_S,2,"const Ogre::Vector3",0,&tolua_err) ||
+     !tolua_isnoobj(tolua_S,3,&tolua_err)
+ )
+  goto tolua_lerror;
+ else
+#endif
+ {
+  EmberOgre::AvatarController* self = (EmberOgre::AvatarController*)  tolua_tousertype(tolua_S,1,0);
+  const Ogre::Vector3* point = ((const Ogre::Vector3*)  tolua_tousertype(tolua_S,2,0));
+#ifndef TOLUA_RELEASE
+  if (!self) tolua_error(tolua_S,"invalid 'self' in function 'moveToPoint'",NULL);
+#endif
+  {
+   self->moveToPoint(*point);
+  }
+ }
+ return 0;
+#ifndef TOLUA_RELEASE
+ tolua_lerror:
+ tolua_error(tolua_S,"#ferror in function 'moveToPoint'.",&tolua_err);
  return 0;
 #endif
 }
@@ -17519,6 +17552,7 @@ TOLUA_API int tolua_EmberOgre_open (lua_State* tolua_S)
     tolua_function(tolua_S,"detachCamera",tolua_EmberOgre_EmberOgre_AvatarController_detachCamera00);
     tolua_function(tolua_S,"attachCamera",tolua_EmberOgre_EmberOgre_AvatarController_attachCamera00);
     tolua_function(tolua_S,"getCurrentMovement",tolua_EmberOgre_EmberOgre_AvatarController_getCurrentMovement00);
+    tolua_function(tolua_S,"moveToPoint",tolua_EmberOgre_EmberOgre_AvatarController_moveToPoint00);
    tolua_endmodule(tolua_S);
   tolua_endmodule(tolua_S);
   tolua_module(tolua_S,"EmberOgre",0);
