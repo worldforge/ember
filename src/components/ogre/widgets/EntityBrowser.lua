72aa6644881f1f4338efcc4bf5f08e50c594e9a7
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2007-07-15 06:07:15 +0000

    2007-07-15 Erik Hjortsberg  <erik.hjortsberg@iteam.se>
    	* src/components/ogre/widgets/EntityBrowser.layout, src/components/ogre/widgets/EntityBrowser.lua: added listing of all ogre scene nodes. even though you currently can't select any
    	* src/components/ogre/widgets/EntityEditor.layout: fixed invalid xml which caused random behaviour

--- src/components/ogre/widgets/EntityBrowser.lua
+++ src/components/ogre/widgets/EntityBrowser.lua
@@ -4,8 +4,15 @@ EntityBrowser = {}
 EntityBrowser.connectors = {}
 EntityBrowser.listbox = nil
 
+EntityBrowser.sceneNodes = {}
+EntityBrowser.sceneNodes.listbox = nil
+
 function EntityBrowser.Refresh_MouseClick(args)
 	EntityBrowser.refresh()
+end
+
+function EntityBrowser.RefreshSceneNodes_MouseClick(args)
+	EntityBrowser.refreshSceneNodes();
 end
 
 function EntityBrowser.EntityList_SelectionChanged(args)
@@ -19,6 +26,32 @@ function EntityBrowser.EntityList_SelectionChanged(args)
 	end
 	
 end
+
+function EntityBrowser.refreshSceneNodes()
+	EntityBrowser.sceneNodes.listholder:resetList()
+	EntityBrowser.addSceneNode(emberOgre:getRootSceneNode(), 0)
+end
+
+function EntityBrowser.addSceneNode(sceneNode, level)
+--	if entity ~= nil then
+		local label = ""
+		for i = 0, level  do
+			label = label .. " "
+		end	
+		label = label .. sceneNode:getName()
+		
+		local item = EmberOgre.Gui.ColouredListItem:new(label, 0, sceneNode)
+		EntityBrowser.sceneNodes.listholder:addItem(item)
+	
+	
+		local numContained = sceneNode:numChildren()
+		for i = 0, numContained - 1 do
+			local chilNode = sceneNode:getChildByIndex(i)
+			EntityBrowser.addSceneNode(chilNode, level + 1)
+		end 
+--	end
+end
+
 
 function EntityBrowser.refresh()
 	EntityBrowser.listholder:resetList()
@@ -50,16 +83,21 @@ function EntityBrowser.buildWidget()
 	EntityBrowser.widget = guiManager:createWidget()
 	EntityBrowser.widget:loadMainSheet("EntityBrowser.layout", "EntityBrowser/")
 	
+	--the eris part
 	EntityBrowser.listbox = CEGUI.toListbox(EntityBrowser.widget:getWindow("EntityList"))
 	EntityBrowser.listbox:subscribeEvent("ItemSelectionChanged", "EntityBrowser.EntityList_SelectionChanged")
 	
 	EntityBrowser.filter = CEGUI.toEditbox(EntityBrowser.widget:getWindow("FilterEntities"))
 	EntityBrowser.listholder = EmberOgre.ListHolder:new_local(EntityBrowser.listbox, EntityBrowser.filter)
-	
-	
 	EntityBrowser.widget:getWindow("Refresh"):subscribeEvent("MouseClick", "EntityBrowser.Refresh_MouseClick")
 
-
+	
+	--the ogre scene nodes part
+	EntityBrowser.sceneNodes.listbox = CEGUI.toListbox(EntityBrowser.widget:getWindow("SceneNodesList"))
+	EntityBrowser.sceneNodes.listbox:subscribeEvent("ItemSelectionChanged", "EntityBrowser.SceneNodesList_SelectionChanged")
+	EntityBrowser.sceneNodes.filter = CEGUI.toEditbox(EntityBrowser.widget:getWindow("FilterSceneNodes"))
+	EntityBrowser.sceneNodes.listholder = EmberOgre.ListHolder:new_local(EntityBrowser.sceneNodes.listbox, EntityBrowser.sceneNodes.filter)
+	EntityBrowser.widget:getWindow("RefreshSceneNodes"):subscribeEvent("MouseClick", "EntityBrowser.RefreshSceneNodes_MouseClick")
 
 
 	EntityBrowser.widget:registerConsoleVisibilityToggleCommand("entityBrowser")
