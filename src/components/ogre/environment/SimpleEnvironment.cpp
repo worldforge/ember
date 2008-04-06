7e58f8cff718a164324fa8ed98bb4628e5f17758
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2008-04-06 19:28:14 +0200

    Added a very, very simple environment which can be used as fallback whenever the Caelum system fails to initialize (when there's no shader support for example)

---
+++ src/components/ogre/environment/SimpleEnvironment.cpp
@@ -0,0 +1,111 @@
+//
+// C++ Implementation: SimpleEnvironment
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik.hjortsberg@iteam.se>, (C) 2008
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "SimpleEnvironment.h"
+#include "Water.h"
+
+namespace EmberOgre {
+
+namespace Environment {
+
+void SimpleSun::setAmbientLight(const Ogre::ColourValue& colour)
+{
+}
+
+Ogre::Vector3 SimpleSun::getSunDirection() const
+{
+	return Ogre::Vector3(-0.5f, -1.0f, -0.5f);
+}
+
+
+
+
+void SimpleFog::setDensity(float density)
+{
+}
+
+float SimpleFog::getDensity() const
+{
+	return 1.0f;
+}
+
+
+SimpleEnvironment::SimpleEnvironment(Ogre::SceneManager *sceneMgr, Ogre::RenderWindow* window, Ogre::Camera* camera)
+: mSceneMgr(sceneMgr), mWindow(window), mCamera(camera)
+,mSun(0), mSky(0), mFog(0), mWater(0)
+{
+}
+
+
+SimpleEnvironment::~SimpleEnvironment()
+{
+	delete mSun;
+	delete mSky;
+	delete mFog;
+	delete mWater;
+}
+
+void SimpleEnvironment::createEnvironment()
+{
+	mSun = new SimpleSun();
+	mSky = new SimpleSky();
+	mFog = new SimpleFog();
+	mWater = new Water(mCamera, mSceneMgr);
+}
+
+ISun* SimpleEnvironment::getSun()
+{
+	return mSun;
+}
+
+ISky* SimpleEnvironment::getSky()
+{
+	return mSky;
+}
+
+IFog* SimpleEnvironment::getFog()
+{
+	return mFog;
+}
+
+IWater* SimpleEnvironment::getWater()
+{
+	return mWater;
+}
+
+void SimpleEnvironment::setTime(int hour, int minute, int second)
+{
+}
+
+void SimpleEnvironment::setTime(int seconds)
+{
+}
+
+
+
+}
+
+}
---
+++ src/components/ogre/environment/SimpleEnvironment.h
@@ -0,0 +1,113 @@
+//
+// C++ Interface: SimpleEnvironment
+//
+// Description: 
+//
+//
+// Author: Erik Hjortsberg <erik.hjortsberg@iteam.se>, (C) 2008
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef EMBEROGRE_ENVIRONMENTSIMPLEENVIRONMENT_H
+#define EMBEROGRE_ENVIRONMENTSIMPLEENVIRONMENT_H
+
+#include "Environment.h"
+
+
+namespace Ogre
+{
+class SceneManager;
+class RenderWindow;
+class Camera;
+}
+
+namespace EmberOgre {
+
+namespace Environment {
+
+/**
+A very simple sun which always will return the same direction.
+*/
+class SimpleSun : public ISun
+{
+public:
+	virtual void setAmbientLight(const Ogre::ColourValue& colour);
+	virtual Ogre::Vector3 getSunDirection() const;
+protected:
+};
+
+/**
+A very simple sky which won't do anything currently.
+*/
+class SimpleSky : public ISky
+{
+public:
+protected:
+};
+
+/**
+A very simple fog which will always return a density of 1.0
+*/
+class SimpleFog : public IFog
+{
+public:
+	virtual void setDensity(float density);
+	virtual float getDensity() const;
+protected:
+};
+
+class Water;
+
+
+
+/**
+	@author Erik Hjortsberg <erik.hjortsberg@iteam.se>
+	
+	A very simple environment which can be used as a fallback environment if a more advanced environment fails to load.
+*/
+class SimpleEnvironment : public IEnvironmentProvider
+{
+public:
+    SimpleEnvironment(Ogre::SceneManager *sceneMgr, Ogre::RenderWindow* window, Ogre::Camera* camera);
+
+    virtual ~SimpleEnvironment();
+    
+	virtual void createEnvironment();
+
+	virtual ISun* getSun();
+	virtual ISky* getSky();
+	virtual IFog* getFog();
+	virtual IWater* getWater();
+
+    virtual void setTime(int hour, int minute, int second = 0);
+	virtual void setTime(int seconds);
+	
+protected:
+	Ogre::SceneManager *mSceneMgr;
+	Ogre::RenderWindow* mWindow;
+	Ogre::Camera* mCamera;
+	
+	SimpleSun* mSun;
+	SimpleSky* mSky;
+	SimpleFog* mFog;
+	Water* mWater;
+
+};
+
+}
+
+}
+
+#endif
