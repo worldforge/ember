ebe5a27ccd3aecc9dcdc846f161bbe417fc738b7
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2008-05-06 22:53:23 +0200

    Updated the dev media script after we've changed the location of the make_dist_media.sh script.

--- refresh-amber-media-dev.sh
+++ refresh-amber-media-dev.sh
@@ -2,5 +2,5 @@
 
 #updates amber with the needed media. This will only work for me (Erik Hjortsberg) since only I know the password (as found in rsyncpasswd)
 
-sh optimized/make_dist_media.sh media-dev 256
+sh make_dist_media.sh media-dev 256
 rsync -uavz --password-file rsyncpasswd  media-dev/media/* amber::media-dev-update
