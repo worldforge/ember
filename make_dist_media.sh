a9bc9ad4a2b8529eb56318061b995e8172966629
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2008-05-15 01:25:09 +0200

    Update after switching to the svn for media.

--- make_dist_media.sh
+++ make_dist_media.sh
@@ -12,22 +12,26 @@ original_media=${PWD}/media
 user_dir=${media_dir}/media/user
 shared_dir=${media_dir}/media/shared
 
+ogre_dir=${original_media}/common/resources/ogre
+script_dir=${ogre_dir}/scripts
+material_dir=${script_dir}/materials
+
 #start with shared media
 
 mkdir -p ${shared_dir}/common
 cd ${shared_dir}
 #try to get the textures needed
 #don't include lines starting with "/"
-grep -rIE --no-filename "^[^/].*texture 3d"  ${original_media}/materials/scripts/*.material | sed -e 's/texture 3d_/3d_/g' > common_textures.list
-grep -rIE --no-filename "^[^/].*set_texture_alias DiffuseMap 3d"  ${original_media}/materials/scripts/*.material | sed -e 's/set_texture_alias DiffuseMap 3d_/3d_/g' >> common_textures.list
-grep -rIE --no-filename "^[^/].*set_texture_alias DiffuseMap art\/3d"  ${original_media}/materials/scripts/*.material | sed -e 's/set_texture_alias DiffuseMap art\/3d_/art\/3d_/g' >> common_textures.list
-grep -rIE --no-filename "^[^/].*texture themes"  ${original_media}/materials/scripts/*.material | sed -e 's/texture themes/themes/g' >> common_textures.list
+grep -rIE --no-filename "^[^/].*texture "  ${material_dir}/*.material | sed -e 's/texture //g' > common_textures.list
+grep -rIE --no-filename "^[^/].*set_texture_alias DiffuseMap "  ${material_dir}/*.material | sed -e 's/set_texture_alias DiffuseMap //g' >> common_textures.list
+# grep -rIE --no-filename "^[^/].*set_texture_alias DiffuseMap art\/3d"  ${original_media}/materials/scripts/*.material | sed -e 's/set_texture_alias DiffuseMap art\/3d_/art\/3d_/g' >> common_textures.list
+# grep -rIE --no-filename "^[^/].*texture themes"  ${original_media}/materials/scripts/*.material | sed -e 's/texture themes/themes/g' >> common_textures.list
 # grep -rIE --no-filename "^[^/].*set_texture_alias NormalMap 3d"  ${original_media}/materials/scripts/*.material | sed -e 's/set_texture_alias NormalMap 3d_/3d_/g' >> common_textures.list
-grep -rIE --no-filename "^[^/].*set_texture_alias NormalHeightMap 3d"  ${original_media}/materials/scripts/*.material | sed -e 's/set_texture_alias NormalHeightMap 3d_/3d_/g' >> common_textures.list
-grep -rIE --no-filename "^[^/].*set_texture_alias SpecularMap 3d"  ${original_media}/materials/scripts/*.material | sed -e 's/set_texture_alias SpecularMap 3d_/3d_/g' >> common_textures.list
-grep -orIE --no-filename "icon=\"3d[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef | sed -e 's/icon=\"//g' | sed -e 's/\"//g' >> common_textures.list
-grep -orIE --no-filename "diffusetexture=\"3d[^\"]*\"" ${original_media}/modeldefinitions/*.terrain | sed -e 's/diffusetexture=\"//g' | sed -e 's/\"//g' >> common_textures.list
-grep -orIE --no-filename "normalmaptexture=\"3d[^\"]*\"" ${original_media}/modeldefinitions/*.terrain | sed -e 's/normalmaptexture=\"//g' | sed -e 's/\"//g' >> common_textures.list
+grep -rIE --no-filename "^[^/].*set_texture_alias NormalHeightMap "  ${material_dir}/*.material | sed -e 's/set_texture_alias NormalHeightMap //g' >> common_textures.list
+grep -rIE --no-filename "^[^/].*set_texture_alias SpecularMap "  ${material_dir}/*.material | sed -e 's/set_texture_alias SpecularMap //g' >> common_textures.list
+grep -orIE --no-filename "icon=\"[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef | sed -e 's/icon=\"//g' | sed -e 's/\"//g' >> common_textures.list
+grep -orIE --no-filename "diffusetexture=\"[^\"]*\"" ${original_media}/modeldefinitions/*.terrain | sed -e 's/diffusetexture=\"//g' | sed -e 's/\"//g' >> common_textures.list
+grep -orIE --no-filename "normalmaptexture=\"[^\"]*\"" ${original_media}/modeldefinitions/*.terrain | sed -e 's/normalmaptexture=\"//g' | sed -e 's/\"//g' >> common_textures.list
 
 #remove all duplicates
 sort --unique common_textures.list > common_textures_cleaned.list
@@ -65,10 +69,10 @@ done
 #copy all meshes in use
 echo "Copying meshes"
 cd ${shared_dir}
-grep -orIE --no-filename "mesh=\"3d[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef | sed -e 's/mesh=\"//g' | sed -e 's/\"//g' > ${shared_dir}/common_meshes.list
-grep -orIE --no-filename "mesh=\"junk[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef | sed -e 's/mesh=\"//g' | sed -e 's/\"//g' >> ${shared_dir}/common_meshes.list
-grep -orIE --no-filename "mesh=\"3d[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef.xml | sed -e 's/mesh=\"//g' | sed -e 's/\"//g' >> ${shared_dir}/common_meshes.list
-grep -orIE --no-filename "mesh=\"junk[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef.xml | sed -e 's/mesh=\"//g' | sed -e 's/\"//g' >> ${shared_dir}/common_meshes.list
+grep -orIE --no-filename "mesh=\"[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef | sed -e 's/mesh=\"//g' | sed -e 's/\"//g' > ${shared_dir}/common_meshes.list
+# grep -orIE --no-filename "mesh=\"junk[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef | sed -e 's/mesh=\"//g' | sed -e 's/\"//g' >> ${shared_dir}/common_meshes.list
+grep -orIE --no-filename "mesh=\"[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef.xml | sed -e 's/mesh=\"//g' | sed -e 's/\"//g' >> ${shared_dir}/common_meshes.list
+# grep -orIE --no-filename "mesh=\"junk[^\"]*\"" ${original_media}/modeldefinitions/*.modeldef.xml | sed -e 's/mesh=\"//g' | sed -e 's/\"//g' >> ${shared_dir}/common_meshes.list
 cd ${original_media}/common ; tar cf - `cat ${shared_dir}/common_meshes.list ` | ( cd ${shared_dir}/common; tar --keep-newer-files -xvf -) 2>  /dev/null
 # exit
 mkdir -p ${shared_dir}/models
@@ -79,7 +83,8 @@ echo "Copying skeletons"
 #use meshmagick to figure out the needed skeletons
 for filename in `find ${shared_dir}/common -name "*.mesh"`
 do
-	meshmagick info 3d_skeletons/avian/models/chicken/chicken.mesh | grep -oE "skeletonfile.*.skeleton" | sed -e 's/skeletonfile //' >> ${shared_dir}/common_skeletons.list
+	meshmagick info ${filename} | grep -oE "skeletonfile.*.skeleton" | sed -e 's/skeletonfile //' >> ${shared_dir}/common_skeletons.list
+	meshmagick info ${filename} | grep -oE "skeleton file.*.skeleton" | sed -e 's/skeleton file //' >> ${shared_dir}/common_skeletons.list
 done
 cd ${original_media}/common ; tar cf - `cat ${shared_dir}/common_skeletons.list ` | ( cd ${shared_dir}/common; tar --keep-newer-files -xvf -) 2>  /dev/null
 
@@ -128,15 +133,16 @@ cd ${original_media}/modeldefinitions ; tar cf - `find . -iname \*.terrain` | ( 
 
 
 echo "Copying materials"
-mkdir -p ${user_dir}/materials
-cd ${original_media}/materials ; tar cf - `find . -iname \*.png` | ( cd ${user_dir}/materials; tar --keep-newer-files -xvf -) 2>  /dev/null
-cd ${original_media}/materials ; tar cf - `find . -iname \*.jpg` | ( cd ${user_dir}/materials; tar --keep-newer-files -xvf -) 2>  /dev/null
-cd ${original_media}/materials ; tar cf - `find . -iname \*.cg` | ( cd ${user_dir}/materials; tar --keep-newer-files -xvf -) 2>  /dev/null
-cd ${original_media}/materials ; tar cf - `find . -iname \*.program` | ( cd ${user_dir}/materials; tar --keep-newer-files -xvf -) 2>  /dev/null
-cd ${original_media}/materials ; tar cf - `find . -iname \*.asm` | ( cd ${user_dir}/materials; tar --keep-newer-files -xvf -) 2>  /dev/null
-cd ${original_media}/materials ; tar cf - `find . -iname \*.ps` | ( cd ${user_dir}/materials; tar --keep-newer-files -xvf -) 2>  /dev/null
-cd ${original_media}/materials ; tar cf - `find . -iname \*.material` | ( cd ${user_dir}/materials; tar --keep-newer-files -xvf -) 2>  /dev/null
-cd ${original_media}/materials ; tar cf - `find . -iname \*.overlay` | ( cd ${user_dir}/materials; tar --keep-newer-files -xvf -) 2>  /dev/null
+user_ogre_dir=${user_dir}/common/resources/ogre
+mkdir -p ${user_ogre_dir}
+cd ${ogre_dir} ; tar cf - `find . -iname \*.png` | ( cd ${user_ogre_dir}; tar --keep-newer-files -xvf -) 2>  /dev/null
+cd ${ogre_dir} ; tar cf - `find . -iname \*.jpg` | ( cd ${user_ogre_dir}; tar --keep-newer-files -xvf -) 2>  /dev/null
+cd ${ogre_dir} ; tar cf - `find . -iname \*.cg` | ( cd ${user_ogre_dir}; tar --keep-newer-files -xvf -) 2>  /dev/null
+cd ${ogre_dir} ; tar cf - `find . -iname \*.program` | ( cd ${user_ogre_dir}; tar --keep-newer-files -xvf -) 2>  /dev/null
+cd ${ogre_dir} ; tar cf - `find . -iname \*.asm` | ( cd ${user_ogre_dir}; tar --keep-newer-files -xvf -) 2>  /dev/null
+cd ${ogre_dir} ; tar cf - `find . -iname \*.ps` | ( cd ${user_ogre_dir}; tar --keep-newer-files -xvf -) 2>  /dev/null
+cd ${ogre_dir} ; tar cf - `find . -iname \*.material` | ( cd ${user_ogre_dir}; tar --keep-newer-files -xvf -) 2>  /dev/null
+cd ${ogre_dir} ; tar cf - `find . -iname \*.overlay` | ( cd ${user_ogre_dir}; tar --keep-newer-files -xvf -) 2>  /dev/null
 
 #cd ${original_media}/models ; tar cf - `find . -iname \*.mesh` | ( cd ${user_dir}/models; tar xvf -)
 #cd ${original_media}/models ; tar cf - `find . -iname \*.skeleton` | ( cd ${user_dir}/models; tar xvf -)
@@ -153,7 +159,6 @@ cd ${original_media} ; tar cf - `cat ${user_dir}/user_packs.list ` | ( cd ${user
 cd ${user_dir}
 cp -a ${original_media}/README .
 cp -a ${original_media}/LICENSING.txt .
-
 
 echo "Cleanup"
 rm -f ${shared_dir}/common_textures.list
