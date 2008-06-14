943a29bf88769953b5840345fbd08599d56cc29e
Alexey Torkhov <atorkhov@gmail.com>
2008-06-14 19:49:13 +0400

    Modifying entity creator widget to show all recipes at start.

--- src/components/ogre/widgets/EntityCreator.lua
+++ src/components/ogre/widgets/EntityCreator.lua
@@ -1,10 +1,31 @@
 EntityCreator = {connectors={}}
 
+function EntityCreator.fillRecipesList()
+	local list = EntityCreator.widget:getWindow("RecipesList")
+	list = CEGUI.toListbox(list)
+	list:resetList()
+
+	local recipeMgr = EmberOgre.EntityRecipeManager:getSingleton()
+	local I = recipeMgr:getResourceIterator()
+	local i = 0
+	while I:hasMoreElements() do
+		local recipePtr = I:getNext()
+		recipePtr = tolua.cast(recipePtr, "EmberOgre::EntityRecipePtr")
+		local recipe = recipePtr:get()
+		local name = recipe:getName()
+		local item = EmberOgre.Gui.ColouredListItem:new(name, i, name)
+		list:addItem(item)
+		i = i + 1
+	end
+end
+
 function EntityCreator.buildWidget()
 	EntityCreator.widget = guiManager:createWidget()
 	EntityCreator.widget:loadMainSheet("EntityCreator.layout", "EntityCreator/")
 
 	EntityCreator.helper = EmberOgre.Gui.EntityCreator()
+
+	EntityCreator.fillRecipesList()
 
 	EntityCreator.widget:registerConsoleVisibilityToggleCommand("advEntityCreator")
 	EntityCreator.widget:enableCloseButton()
