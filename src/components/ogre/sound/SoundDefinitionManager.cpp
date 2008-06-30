b653794296dab0b336ad93c92638f10aa14d8cad
Romulo Fernandes <abra185@yahoo.com>
2008-06-29 23:20:03 -0300

    The Real files

---
+++ src/components/ogre/sound/SoundDefinitionManager.cpp
@@ -0,0 +1,63 @@
+//
+// C++ Implementation: SoundDefinitionManager
+//
+// Description: 
+//
+//
+// Author: Romulo Fernandes <abra185@gmail.com>, (C) 2008
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "XMLSoundDefParser.h"
+#include "SoundDefinitionManager.h"
+
+namespace EmberOgre {
+
+template<> EmberOgre::SoundDefinitionManager* Ember::Singleton<EmberOgre::SoundDefinitionManager>::ms_Singleton = 0;
+
+SoundDefinitionManager::SoundDefinitionManager()
+{
+   mResourceType = "SoundDefinition";
+        
+	mScriptPatterns.push_back("*.sounddef");
+	Ogre::ResourceGroupManager::getSingleton()._registerScriptLoader(this);
+	Ogre::ResourceGroupManager::getSingleton()._registerResourceManager(mResourceType, this);
+}
+
+
+SoundDefinitionManager::~SoundDefinitionManager()
+{
+	Ogre::ResourceGroupManager::getSingleton()._unregisterScriptLoader(this);
+	Ogre::ResourceGroupManager::getSingleton()._unregisterResourceManager(mResourceType);
+}
+
+void SoundDefinitionManager::parseScript (Ogre::DataStreamPtr &stream, const Ogre::String &groupName)
+{
+	soundParser.parseScript(stream);
+}
+
+Ogre::Resource* SoundDefinitionManager::createImpl(const Ogre::String& name, Ogre::ResourceHandle handle, 
+    const Ogre::String& group, bool isManual, Ogre::ManualResourceLoader* loader, 
+    const Ogre::NameValuePairList* createParams)
+{
+	return 0;
+}
+
+}
+
