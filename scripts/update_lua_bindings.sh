ca5ae26910b58c1e85e021102a829cd743fdb55d
Alexey Torkhov <atorkhov@gmail.com>
2008-07-15 16:50:14 +0400

    Modified common tolua script to show and exit if error occured.

--- scripts/update_lua_bindings.sh
+++ scripts/update_lua_bindings.sh
@@ -2,4 +2,6 @@
 #tolua++ will for some reason translate "const std::string" into "const,std::string", so we need to remove these incorrect commas from the final code
 #some versions will also for some unexplainable reason not correctly remove the tolua specific directive tolua_outside, so we need to clean that out also
 echo "Updating lua bindings."
-tolua++ -n $1 $2 | sed -e 's/const,/const /g' | sed -e 's/tolua_outside//g' > $3
+tolua++ -n $1 $2 > $3
+grep -q '** tolua internal error' $3 && cat $3 && exit 1
+sed -i -e 's/const,/const /g' -e 's/tolua_outside//g' $3
