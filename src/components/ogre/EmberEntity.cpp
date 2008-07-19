26dff57f06ae1445f004429a6596136f97f96418
Tamas Bates <rhymer@gmail.com>
2008-07-18 18:26:06 -0700

    Ember now uses the same position that Cyphesis does when applying a terrain mod.

--- src/components/ogre/EmberEntity.cpp
+++ src/components/ogre/EmberEntity.cpp
@@ -542,17 +542,9 @@ void EmberEntity::updateTerrainModifiers(const Atlas::Message::Element& modifier
     }
 
     // Get modifier position
-/*    mod_I = modMap.find("pos");
-    if (mod_I != modMap.end()) {
-        const Atlas::Message::Element& modPosElem = mod_I->second;
-        if (modPosElem.isList()) {
-        const Atlas::Message::ListType& point = modPosElem.asList();
-        pos = WFMath::Point<3>((int)point[0].asNum(), (int)point[1].asNum(), (int)point[2].asNum());
-        }
-    }
-*/
+
     const Ogre::Vector3 & ent_pos = getSceneNode()->getPosition();
-    pos = WFMath::Point<3>((int)ent_pos.x, (int)ent_pos.y, (int)ent_pos.z);
+    pos = WFMath::Point<3>((int)ent_pos.x, -(int)ent_pos.z, (int)ent_pos.y);
     S_LOG_INFO("SceneNode Pos reported as: " << ent_pos.x << "," << ent_pos.y << "," << ent_pos.z);
     // Get modifier's shape
     mod_I = modMap.find("shape");
