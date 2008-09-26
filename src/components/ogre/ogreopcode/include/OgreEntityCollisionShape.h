14fc1114e667ed4a2a491b1875014789ab047dae
Alexey Torkhov <atorkhov@gmail.com>
2008-09-26 18:57:12 +0400

    Reverting GPL to LGPL change as there is no need in it. This reverts commit 055cee4d from 2007-12-18.

--- src/components/ogre/ogreopcode/include/OgreEntityCollisionShape.h
+++ src/components/ogre/ogreopcode/include/OgreEntityCollisionShape.h
@@ -18,7 +18,7 @@
 ///  OgreOpcode is distributed in the hope that it will be useful,
 ///  but WITHOUT ANY WARRANTY; without even the implied warranty of
 ///  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-///  General Public License for more details.
+///  Lesser General Public License for more details.
 ///
 ///  You should have received a copy of the GNU General Public
 ///  License along with OgreOpcode; if not, write to the Free Software
