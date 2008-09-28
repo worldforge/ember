6539d0a1e1b49cccb4ad41b1dc6dc56695ec14e3
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2008-09-28 18:22:29 +0200

    Handle invalid bounding boxes (for example a Model which only has a particle system) without getting crashes later on.

--- src/components/ogre/ModelMount.cpp
+++ src/components/ogre/ModelMount.cpp
@@ -72,78 +72,82 @@ void ModelMount::scaleNode(const WFMath::AxisBox<3>* wfBbox)
 	
 	///make a copy of the original bbox
 	Ogre::AxisAlignedBox defaultOgreBoundingBox = mModel.getBoundingBox();
-	///apply any transformations required first so the bounding box we use as reference represents the way to mesh is adjusted through rotations set in the model definition
-	Ogre::Matrix4 localTransform;
-	localTransform.makeTransform(getScaleNode()->getPosition(), getScaleNode()->getScale(), getScaleNode()->getOrientation());
- 	defaultOgreBoundingBox.transform(localTransform);
-// 	defaultOgreBoundingBox.transform(Ogre::Matrix4(getScaleNode()->getOrientation()));
 	
-	const Ogre::Vector3& defaultMax(defaultOgreBoundingBox.getMaximum());
-	const Ogre::Vector3& defaultMin(defaultOgreBoundingBox.getMinimum());
+	///We can only apply any meaninful scaling if there's a bounding box in the model. This might not be true if the model for example only contains a particle system or similiar, and no entities
+	if (!defaultOgreBoundingBox.isNull()) {
+		///apply any transformations required first so the bounding box we use as reference represents the way to mesh is adjusted through rotations set in the model definition
+		Ogre::Matrix4 localTransform;
+		localTransform.makeTransform(getScaleNode()->getPosition(), getScaleNode()->getScale(), getScaleNode()->getOrientation());
+		defaultOgreBoundingBox.transform(localTransform);
+	// 	defaultOgreBoundingBox.transform(Ogre::Matrix4(getScaleNode()->getOrientation()));
+		
+		const Ogre::Vector3& defaultMax(defaultOgreBoundingBox.getMaximum());
+		const Ogre::Vector3& defaultMin(defaultOgreBoundingBox.getMinimum());
+		
+		///Depending on whether the entity has a bounding box or not we'll use different scaling methods. Most entities should have bounding boxes, but not all.
+		if (wfBbox && wfBbox->isValid()) {
 	
-	///Depending on whether the entity has a bounding box or not we'll use different scaling methods. Most entities should have bounding boxes, but not all.
-	if (wfBbox && wfBbox->isValid()) {
-
-		///The entity has a bounding box. We'll now check the "usescaleof" setting.
-		///It it's either of "height", "width" or "depth" we'll scale the model uniformly so that the specified dimension matches up exactly between the bounding box of the Model and the bounding box of the entity.
-		///If it's "none" we won't apply any scaling.
-		///And if it's "all" we'll scale the Model so that it matches all dimensions of the entity's bounding box. This will in almost all cases however distort the Model so it's undesirable.
-		
-		///Note that after the Model has been scaled using the bounding box, it can still be scaled additionally through the "scale" setting in the ModelDefinition.
-		
-		Ogre::AxisAlignedBox ogreBbox(Atlas2Ogre(*wfBbox));
-		
-		const Ogre::Vector3& ogreMax(ogreBbox.getMaximum());
-		const Ogre::Vector3& ogreMin(ogreBbox.getMinimum());
-/*		const WFMath::Point<3>& wfMax(wfBbox.highCorner());
-		const WFMath::Point<3>& wfMin(wfBbox.lowCorner());*/
-		
-		Ogre::Real scaleX;
-		Ogre::Real scaleY;
-		Ogre::Real scaleZ;
-		
-		switch (getModel().getUseScaleOf()) {
-			case Model::ModelDefinition::MODEL_HEIGHT:
-				scaleX = scaleY = scaleZ = fabs((ogreMax.y - ogreMin.y) / (defaultMax.y - defaultMin.y));
-				break;
-			case Model::ModelDefinition::MODEL_WIDTH:
-				scaleX = scaleY = scaleZ = fabs((ogreMax.x - ogreMin.x) / (defaultMax.x - defaultMin.x));
-				break;
-			case Model::ModelDefinition::MODEL_DEPTH:
-				scaleX = scaleY = scaleZ = fabs((ogreMax.z - ogreMin.z) / (defaultMax.z - defaultMin.z));
-				break;
-			case Model::ModelDefinition::MODEL_NONE:
-				scaleX = scaleY = scaleZ = 1;
-				break;
-				
-			case Model::ModelDefinition::MODEL_ALL:
-			default:
-				///HACK: for some reason we have to switch x and z here. I'm not completely sure why, but it works. It hints at a problem elsewhere though
-				scaleZ = fabs((ogreMax.x - ogreMin.x) / (defaultMax.x - defaultMin.x));
-				scaleY = fabs((ogreMax.y - ogreMin.y) / (defaultMax.y - defaultMin.y));
-				scaleX = fabs((ogreMax.z - ogreMin.z) / (defaultMax.z - defaultMin.z));
+			///The entity has a bounding box. We'll now check the "usescaleof" setting.
+			///It it's either of "height", "width" or "depth" we'll scale the model uniformly so that the specified dimension matches up exactly between the bounding box of the Model and the bounding box of the entity.
+			///If it's "none" we won't apply any scaling.
+			///And if it's "all" we'll scale the Model so that it matches all dimensions of the entity's bounding box. This will in almost all cases however distort the Model so it's undesirable.
+			
+			///Note that after the Model has been scaled using the bounding box, it can still be scaled additionally through the "scale" setting in the ModelDefinition.
+			
+			Ogre::AxisAlignedBox ogreBbox(Atlas2Ogre(*wfBbox));
+			
+			const Ogre::Vector3& ogreMax(ogreBbox.getMaximum());
+			const Ogre::Vector3& ogreMin(ogreBbox.getMinimum());
+	/*		const WFMath::Point<3>& wfMax(wfBbox.highCorner());
+			const WFMath::Point<3>& wfMin(wfBbox.lowCorner());*/
+			
+			Ogre::Real scaleX;
+			Ogre::Real scaleY;
+			Ogre::Real scaleZ;
+			
+			switch (getModel().getUseScaleOf()) {
+				case Model::ModelDefinition::MODEL_HEIGHT:
+					scaleX = scaleY = scaleZ = fabs((ogreMax.y - ogreMin.y) / (defaultMax.y - defaultMin.y));
+					break;
+				case Model::ModelDefinition::MODEL_WIDTH:
+					scaleX = scaleY = scaleZ = fabs((ogreMax.x - ogreMin.x) / (defaultMax.x - defaultMin.x));
+					break;
+				case Model::ModelDefinition::MODEL_DEPTH:
+					scaleX = scaleY = scaleZ = fabs((ogreMax.z - ogreMin.z) / (defaultMax.z - defaultMin.z));
+					break;
+				case Model::ModelDefinition::MODEL_NONE:
+					scaleX = scaleY = scaleZ = 1;
+					break;
+					
+				case Model::ModelDefinition::MODEL_ALL:
+				default:
+					///HACK: for some reason we have to switch x and z here. I'm not completely sure why, but it works. It hints at a problem elsewhere though
+					scaleZ = fabs((ogreMax.x - ogreMin.x) / (defaultMax.x - defaultMin.x));
+					scaleY = fabs((ogreMax.y - ogreMin.y) / (defaultMax.y - defaultMin.y));
+					scaleX = fabs((ogreMax.z - ogreMin.z) / (defaultMax.z - defaultMin.z));
+			}
+			
+			
+			getScaleNode()->setScale(scaleX, scaleY, scaleZ);
+			
+		} else if (!getModel().getScale()) {
+			///If there's no bbox, and no scaling in the model (i.e. not even "1") we'll set the size of the model to a hardcoded small value (0.25 meters in each dimension).
+			///This is of course a last resort; all good models that can belong to entities without bounding boxes should have a scale set
+			
+			S_LOG_WARNING("Could not find any scale set in the model '" << getModel().getName() << "'. We'll thus default to scaling the mesh so it's 0.25 meters in each dimension.");
+			
+			Ogre::Real scaleX = (0.25 / (defaultMax.x - defaultMin.x));
+			Ogre::Real scaleY = (0.25 / (defaultMax.y - defaultMin.y));
+			Ogre::Real scaleZ = (0.25 / (defaultMax.z - defaultMin.z));
+			getScaleNode()->setScale(scaleX, scaleY, scaleZ);
 		}
-		
-		
-		getScaleNode()->setScale(scaleX, scaleY, scaleZ);
-		
-	} else if (!getModel().getScale()) {
-		///If there's no bbox, and no scaling in the model (i.e. not even "1") we'll set the size of the model to a hardcoded small value (0.25 meters in each dimension).
-		///This is of course a last resort; all good models that can belong to entities without bounding boxes should have a scale set
-		
-		S_LOG_WARNING("Could not find any scale set in the model '" << getModel().getName() << "'. We'll thus default to scaling the mesh so it's 0.25 meters in each dimension.");
-		
-		Ogre::Real scaleX = (0.25 / (defaultMax.x - defaultMin.x));
-		Ogre::Real scaleY = (0.25 / (defaultMax.y - defaultMin.y));
-		Ogre::Real scaleZ = (0.25 / (defaultMax.z - defaultMin.z));
-		getScaleNode()->setScale(scaleX, scaleY, scaleZ);
-	}
-
-	///Lastly, check if we also should scale the model. This scaling is applied after the Model has been scaled to fit with the bounding box.
-	if (getModel().getScale()) {
-		if (getModel().getScale() != 1) {
-			///only scale if it's not 1
-			getScaleNode()->scale(getModel().getScale(), getModel().getScale(), getModel().getScale());
+	
+		///Lastly, check if we also should scale the model. This scaling is applied after the Model has been scaled to fit with the bounding box.
+		if (getModel().getScale()) {
+			if (getModel().getScale() != 1) {
+				///only scale if it's not 1
+				getScaleNode()->scale(getModel().getScale(), getModel().getScale(), getModel().getScale());
+			}
 		}
 	}
 }
