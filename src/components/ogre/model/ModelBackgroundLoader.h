e5502719bd8a58de306cc7d3adb1f48853334ae8
Erik Hjortsberg <erik.hjortsberg@iteam.se>
2009-01-17 13:39:02 +0100

    Added docs.

--- src/components/ogre/model/ModelBackgroundLoader.h
+++ src/components/ogre/model/ModelBackgroundLoader.h
@@ -31,39 +31,95 @@ namespace Model {
 class Model;
 
 /**
-	@author Erik Hjortsberg <erik.hjortsberg@gmail.com>
+@brief Responsible for loading the resources needed by a Model.
+If thread support is enabled it will be used. You must then call poll() each frame to see if the background thread has finished loading.
+
+@author Erik Hjortsberg <erik.hjortsberg@gmail.com>
 */
 class ModelBackgroundLoader
 {
 public:
+	/**
+	 * @brief The diffrent loading states of the Model.
+	 * Loading normally progresses through these states in order.
+	*/
 	enum LoadingState {
+		/**
+		 * @brief The loading hasn't yet begun.
+		 */
 		LS_UNINITIALIZED,
+		/**
+		 * @brief The Meshes are being prepared.
+		 */
 		LS_MESH_PREPARING,
+		/**
+		 * @brief The Meshes has been prepared.
+		 */
 		LS_MESH_PREPARED,
+		/**
+		 * @brief The Materials are being prepared.
+		 */
 		LS_MATERIAL_PREPARING,
+		/**
+		 * @brief The Materials have been prepared.
+		 */
 		LS_MATERIAL_PREPARED,
+		/**
+		 * @brief Loading is done.
+		 */
 		LS_DONE
 	};
 	typedef std::list<Ogre::BackgroundProcessTicket> TicketStore;
 	
+	/**
+	 * @brief Ctor.
+	 * @param model The model which will be loaded. 
+	 */
 	ModelBackgroundLoader(Model& model);
 	
+	/**
+	 * @brief Dtor.
+	 */
 	~ModelBackgroundLoader();
 	
+	/**
+	 * @brief Polls the loading state (which might occur in a background thread).
+	 * If the loading state has progressed it will be updated.
+	 * @param reloadIfReady If set to true, the Model will be reloaded when the loading has completed.
+	 * @return True if the loading is complete.
+	 */
 	bool poll(bool reloadIfReady = true);
 	
-	void addTicket(Ogre::BackgroundProcessTicket ticket);
-	
-// 	void initialize();
-	
+	/**
+	 * @brief Gets the current loading state.
+	 * @return The current loading state.
+	 */
+	const LoadingState getState() const;
 	
 protected:
+	
+	/**
+	 * @brief The model which will be loaded.
+	 */
 	Model& mModel;
 	
+	/**
+	 * @brief The background loading tickets held by this instance.
+	 * As the tickets complete they will be removed from the list.
+	 */
 	TicketStore mTickets;
 	
+	/**
+	 * @brief The current loading state of the instance.
+	 */
 	LoadingState mState;
 	
+	/**
+	 * @brief Adds a loading ticket.
+	 * @param ticket The ticket.
+	 */
+	void addTicket(Ogre::BackgroundProcessTicket ticket);
+
 };
 
 }
