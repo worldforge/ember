cc56c642565c6f88b716eda8b304c1ca638b669c
Manuel A. Fernandez Montecelo <manuel.montezelo@gmail.com>
2009-05-26 18:51:17 +0200

    Enhanced doxygen comments and cleanup

--- src/services/sound/SoundGeneral.cpp
+++ src/services/sound/SoundGeneral.cpp
@@ -1,24 +1,26 @@
 /*
-    Copyright (C) 2008 Romulo Fernandes Machado (nightz)
-    Copyright (C) 2008 Erik Hjortsberg <erik.hjortsberg@gmail.com>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+ * Copyright (C) 2008 Romulo Fernandes Machado (nightz)
+ * Copyright (C) 2008 Erik Hjortsberg <erik.hjortsberg@gmail.com>
+ * Copyright (C) 2009 by Manuel A. Fernandez Montecelo <manuel.montezelo@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+ * Ave, Cambridge, MA 02139, USA.
+ */
 
 #include "SoundGeneral.h"
 #include "framework/LoggingInstance.h"
+
 #ifdef __APPLE__
 #include <OpenAL/al.h>
 #else
@@ -31,21 +33,21 @@
 #include <ALUT/alut.h>
 #endif
 
-namespace Ember 
+
+namespace Ember
 {
 	bool SoundGeneral::checkAlError(const std::string& description)
 	{
 		ALenum error = alGetError();
 		if (error == AL_NO_ERROR) {
 			return true;
-		}
-		
-		if (description != "") {
-			S_LOG_FAILURE("OpenAl error: " << alutGetErrorString(error));
 		} else {
-			S_LOG_FAILURE("OpenAl error: " << alutGetErrorString(error) << "\nDescription: " << description);
+			if (description != "") {
+				S_LOG_FAILURE("OpenAl error: " << alutGetErrorString(error));
+			} else {
+				S_LOG_FAILURE("OpenAl error: " << alutGetErrorString(error) << "\nDescription: " << description);
+			}
+			return false;
 		}
-		return false;
 	}
-
 }
--- src/services/sound/SoundGeneral.h
+++ src/services/sound/SoundGeneral.h
@@ -1,29 +1,28 @@
 /*
-    Copyright (C) 2008 Romulo Fernandes Machado (nightz)
-    Copyright (C) 2008 Erik Hjortsberg <erik.hjortsberg@gmail.com>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+ * Copyright (C) 2008 Romulo Fernandes Machado (nightz)
+ * Copyright (C) 2008 Erik Hjortsberg <erik.hjortsberg@gmail.com>
+ * Copyright (C) 2009 by Manuel A. Fernandez Montecelo <manuel.montezelo@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+ * Ave, Cambridge, MA 02139, USA.
+ */
 
 #ifndef SOUND_GENERAL_H
 #define SOUND_GENERAL_H
 
 #include <string>
 
-
-// #define OGG_BUFFER_SIZE (4096 * 8)
 
 namespace Ember 
 {
@@ -40,10 +39,18 @@ public:
 
 	/**
 	 * @brief Checks for any errors in the OpenAL context.
+	 *
 	 * If an error has occurred, it's logged and this method returns false.
-	 * Make sure to call this after you've interacted with OpenAL to make sure that the internal error state is reset in time for the next call to OpenAL. If not, the errors will carry over.
-	 * @param  description If an error occurs, an entry will be written to the log. If you provide a description this will be added too, which might be good when looking through the log and figuring out what's wrong.
-	 * @return False if an error has occurred, else true.
+	 *
+	 * Make sure to call this after you've interacted with OpenAL, so the
+	 * internal error state is reset before the next call to OpenAL
+	 * (otherwise the errors will carry over).
+	 *
+	 * @param description When an error occurs, an entry will be written to
+	 * the log; and this "description" will be appended, thus helping to
+	 * identify each entry.
+	 *
+	 * @return False if an error has occurred, true otherwise.
 	 */
 	static bool checkAlError(const std::string& description = "");
 };
