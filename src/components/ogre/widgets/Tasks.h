7a3058a2a0f82caaf6261e43cf8434963ff839cb
Alexey Torkhov <atorkhov@gmail.com>
2009-06-16 21:54:16 +0400

    Add helper class for tasks widget.

---
+++ src/components/ogre/widgets/Tasks.h
@@ -0,0 +1,59 @@
+//
+// C++ Interface: Tasks
+//
+// Description:
+//
+//
+// Author: Alexey Torkhov <atorkhov@gmail.com>, (C) 2009
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef EMBEROGRE_GUITASKS_H
+#define EMBEROGRE_GUITASKS_H
+
+namespace EmberOgre {
+
+namespace Gui {
+
+/**
+ * @brief Helper class for Tasks.
+ *
+ * Helper class for Tasks.
+ *
+ * @author Alexey Torkhov <atorkhov@gmail.com>
+ */
+class Tasks {
+public:
+	/**
+	 * Constructor.
+	 */
+	Tasks();
+
+	/**
+	 * Destructor.
+	 */
+	~Tasks();
+
+	/**
+	 * Does something.
+	 */
+	void doSomething();
+};
+
+}
+
+}
+
+#endif
