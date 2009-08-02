7327dcf40cdf108c67d5a02ffaaf81faf640e2b4
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2009-08-03 01:46:28 +0200

    Added docs.

--- src/components/ogre/model/ModelFitting.h
+++ src/components/ogre/model/ModelFitting.h
@@ -27,17 +27,58 @@ class EmberEntity;
 
 namespace Model
 {
+/**
+ * @author Erik Hjortsberg <erik.hjortsberg@gmail.com>
+ * @brief Represents a "model fitting", i.e. an attachment of a child entity to a bone on a model.
+ * An instance of this is used to keep track of which entities are attached to which attach points on a model.
+ * Note that it's not until attachChild() has been called that there's an entity attached.
+ * This is because it's not uncommon for the attribute data which tells that a certain entity should be attached to be available before the actual entity is available.
+ */
 class ModelFitting
 {
 public:
+	/**
+	 * @brief Ctor.
+	 * @param parent The parent entity to which other entities can be attached.
+	 * @param mountPoint The name of the mount point.
+	 * @param childEntityId The id of the child entity which should be attached.
+	 * Note that we don't submit the child entity in the constructor. This is done in attachChild() instead. The reason is that it's not certain that the child entity is available at the time of creation.
+	 */
 	ModelFitting(EmberEntity& parent, const std::string& mountPoint, const std::string& childEntityId);
+
+	/**
+	 * @brief Dtor.
+	 */
 	virtual ~ModelFitting();
 
+	/**
+	 * @brief Attaches a child entity to this fitting.
+	 * @param entity The child entity to attach.
+	 */
 	void attachChild(EmberEntity& entity);
 
+	/**
+	 * @brief Gets the parent entity.
+	 * @return The parent entity.
+	 */
 	EmberEntity& getParent() const;
+
+	/**
+	 * @brief Gets the attached entity.
+	 * @return The attached entity, or null if no entity yet is attached.
+	 */
 	EmberEntity* getChild() const;
+
+	/**
+	 * @brief Gets the name of the mount point.
+	 * @return The name of the mount point.
+	 */
 	const std::string& getMountPoint() const;
+
+	/**
+	 * @brief Gets the id of the attached entity.
+	 * @return The id of the attached entity.
+	 */
 	const std::string& getChildEntityId() const;
 
 protected:
