60fb6b066d97e90d4fd6378521dbc9a8d5d5132a
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2009-09-27 23:23:50 +0200

    Added file missing in previous commit.

--- src/components/ogre/EmberEntityFactory.h
+++ src/components/ogre/EmberEntityFactory.h
@@ -1,20 +1,20 @@
 /*
-    Copyright (C) 2004  Erik Hjortsberg
+ Copyright (C) 2004  Erik Hjortsberg
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #ifndef DIMEENTITYFACTORY_H
 #define DIMEENTITYFACTORY_H
@@ -31,18 +31,24 @@
 
 namespace Eris
 {
-	class Entity;
-	class View;
-	class TypeService;
-	class TypeInfo;
-	class Avatar;
+class Entity;
+class View;
+class TypeService;
+class TypeInfo;
+class Avatar;
 }
 
-namespace EmberOgre {
+namespace EmberOgre
+{
 
 class EmberEntity;
 class ViewEmberEntity;
 class WorldEmberEntity;
+
+namespace Authoring
+{
+class AuthoringManager;
+}
 
 /**
  * Creates the EmberEntities required.
@@ -50,40 +56,36 @@ class WorldEmberEntity;
  * @see Eris::Factory
  *
  */
-class EmberEntityFactory :
-public Eris::Factory,
-public sigc::trackable,
-public Ember::ConsoleObject
+class EmberEntityFactory: public Eris::Factory, public sigc::trackable, public Ember::ConsoleObject
 {
 public:
 
 	typedef std::set<std::string> StringSet;
 
-
 	/**
-	Default constructor. This should be instantiated by EmberOgre or similiar high level object. Note that Eris upon shutdown will delete all registered factories, so don't delete an instance of this yourself.
-	*/
-	EmberEntityFactory(Eris::View* view, Eris::TypeService* typeService);
+	 Default constructor. This should be instantiated by EmberOgre or similiar high level object. Note that Eris upon shutdown will delete all registered factories, so don't delete an instance of this yourself.
+	 */
+	EmberEntityFactory(Eris::View& view, Eris::TypeService& typeService);
 	virtual ~EmberEntityFactory();
 
 	/**
-	Will always return true.
-	*/
-    virtual bool accept(const Atlas::Objects::Entity::RootEntity &ge, Eris::TypeInfo* type);
-
-    /**
-    Creates instances of EmberEntity and its subclasses.
-    */
-    virtual Eris::Entity* instantiate(const Atlas::Objects::Entity::RootEntity &ge, Eris::TypeInfo* type, Eris::View* w);
-
-    /** retrieve this factory's priority level; higher priority factories
-    get first chance to process a recieved Atlas entity. The default implementation
-    returns one. */
-    virtual int priority();
+	 Will always return true.
+	 */
+	virtual bool accept(const Atlas::Objects::Entity::RootEntity &ge, Eris::TypeInfo* type);
 
 	/**
-	Returns the main world entity.
-	*/
+	 Creates instances of EmberEntity and its subclasses.
+	 */
+	virtual Eris::Entity* instantiate(const Atlas::Objects::Entity::RootEntity &ge, Eris::TypeInfo* type, Eris::View* w);
+
+	/** retrieve this factory's priority level; higher priority factories
+	 get first chance to process a recieved Atlas entity. The default implementation
+	 returns one. */
+	virtual int priority();
+
+	/**
+	 Returns the main world entity.
+	 */
 	WorldEmberEntity* getWorld() const;
 
 	/**
@@ -91,19 +93,17 @@ public:
 	 * @param command
 	 * @param args
 	 */
-	virtual	void runCommand(const std::string &command, const std::string &args);
-
+	virtual void runCommand(const std::string &command, const std::string &args);
 
 	/**
-	Command for setting whether models should be shown or not.
-	*/
+	 Command for setting whether models should be shown or not.
+	 */
 	const Ember::ConsoleCommandWrapper ShowModels;
 
 	/**
-	Dumps the attributes of a supplied entity to the std::out.
-	*/
+	 Dumps the attributes of a supplied entity to the std::out.
+	 */
 	const Ember::ConsoleCommandWrapper DumpAttributes;
-
 
 	/**
 	 *    Dumps the attributes of the entity with the supplied id to the std::out.
@@ -114,28 +114,27 @@ public:
 
 protected:
 
-
 	/**
-	Creates a WorldEmberEntity instance.
-	*/
-	Eris::Entity* createWorld(const Atlas::Objects::Entity::RootEntity & ge,Eris::TypeInfo* type, Eris::View *world);
+	 Creates a WorldEmberEntity instance.
+	 */
+	Eris::Entity* createWorld(const Atlas::Objects::Entity::RootEntity & ge, Eris::TypeInfo* type, Eris::View *world);
 	/**
-	Creates a avatar instance.
-	*/
+	 Creates a avatar instance.
+	 */
 	EmberEntity* createAvatarEntity(const Atlas::Objects::Entity::RootEntity &ge, Eris::TypeInfo* type, Eris::View *world);
 
 	void gotAvatarCharacter(Eris::Entity* entity);
 
-	Eris::TypeService* mTypeService;
+	Eris::TypeService& mTypeService;
 	Eris::TypeInfo* mTerrainType;
 
 	Eris::Avatar* getErisAvatar();
 
 	WorldEmberEntity *mWorldEntity;
 
-	Eris::View* mView;
+	Eris::View& mView;
 
-
+	Authoring::AuthoringManager* mAuthoringManager;
 };
 
 }
