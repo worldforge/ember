33b2f8073c7eee94ea4568d62c22a39069a5a98b
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2010-01-13 14:19:46 +0100

    Fixed crash caused by normalizing a zero length vector. I'm not completely sure whether wfmath does the right thing here (i.e. produces an invalid NaN filled vector).

--- src/components/ogre/MovementController.cpp
+++ src/components/ogre/MovementController.cpp
@@ -1,28 +1,26 @@
 /*
-    Copyright (C) 2004  Erik Hjortsberg
-    some parts Copyright (C) 2004 bad_camel at Ogre3d forums
+ Copyright (C) 2004  Erik Hjortsberg
+ some parts Copyright (C) 2004 bad_camel at Ogre3d forums
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
-
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 #include "MovementController.h"
-
 
 #include "components/ogre/EmberEntity.h"
 #include "components/ogre/GUIManager.h"
@@ -45,15 +43,13 @@
 
 #include <OgreRoot.h>
 
-
 using namespace Ogre;
 using namespace Ember;
-namespace EmberOgre {
+namespace EmberOgre
+{
 
-
-
-MovementControllerInputListener::MovementControllerInputListener(MovementController& controller)
-: mController(controller)
+MovementControllerInputListener::MovementControllerInputListener(MovementController& controller) :
+	mController(controller)
 {
 	Input::getSingleton().EventMouseButtonPressed.connect(sigc::mem_fun(*this, &MovementControllerInputListener::input_MouseButtonPressed));
 	Input::getSingleton().EventMouseButtonReleased.connect(sigc::mem_fun(*this, &MovementControllerInputListener::input_MouseButtonReleased));
@@ -74,30 +70,15 @@ void MovementControllerInputListener::input_MouseButtonReleased(Input::MouseButt
 	}
 }
 
-MovementController::MovementController(Avatar& avatar)
-: RunToggle("+run", this, "Toggle running mode.")
-, ToggleCameraAttached("toggle_cameraattached", this, "Toggle between the camera being attached to the avatar and free flying.")
-, MovementMoveForward("+movement_move_forward", this, "Move forward.")
-, MovementMoveBackward("+movement_move_backward", this, "Move backward.")
-, MovementMoveDownwards("+movement_move_downwards", this, "Move downwards.")
-, MovementMoveUpwards("+movement_move_upwards", this, "Move upwards.")
-, MovementStrafeLeft("+movement_strafe_left", this, "Strafe left.")
-, MovementStrafeRight("+movement_strafe_right", this, "Strafe right.")
-/*, MovementRotateLeft("+Movement_rotate_left", this, "Rotate left.")
-, MovementRotateRight("+Movement_rotate_right", this, "Rotate right.")*/
-//, MoveCameraTo("movecamerato", this, "Moves the camera to a point.")
-, mMovementCommandMapper("movement", "key_bindings_movement")
-, mIsRunning(false)
-, mMovementDirection(WFMath::Vector<3>::ZERO())
-, mDecalObject(0)
-, mDecalNode(0)
-, mControllerInputListener(*this)
-, mAvatar(avatar)
-, mFreeFlyingNode(0)
-, mIsFreeFlying(false)
+MovementController::MovementController(Avatar& avatar) :
+	RunToggle("+run", this, "Toggle running mode."), ToggleCameraAttached("toggle_cameraattached", this, "Toggle between the camera being attached to the avatar and free flying."), MovementMoveForward("+movement_move_forward", this, "Move forward."), MovementMoveBackward("+movement_move_backward", this, "Move backward."), MovementMoveDownwards("+movement_move_downwards", this, "Move downwards."), MovementMoveUpwards("+movement_move_upwards", this, "Move upwards."), MovementStrafeLeft("+movement_strafe_left", this, "Strafe left."), MovementStrafeRight("+movement_strafe_right", this, "Strafe right.")
+	/*, MovementRotateLeft("+Movement_rotate_left", this, "Rotate left.")
+	 , MovementRotateRight("+Movement_rotate_right", this, "Rotate right.")*/
+	//, MoveCameraTo("movecamerato", this, "Moves the camera to a point.")
+			, mMovementCommandMapper("movement", "key_bindings_movement"), mIsRunning(false), mMovementDirection(WFMath::Vector<3>::ZERO()), mDecalObject(0), mDecalNode(0), mControllerInputListener(*this), mAvatar(avatar), mFreeFlyingNode(0), mIsFreeFlying(false)
 {
 
-	mMovementCommandMapper.restrictToInputMode(Input::IM_MOVEMENT );
+	mMovementCommandMapper.restrictToInputMode(Input::IM_MOVEMENT);
 
 	Ogre::Root::getSingleton().addFrameListener(this);
 
@@ -136,16 +117,13 @@ void MovementController::setCameraFreeFlying(bool freeFlying)
 	}
 }
 
-
-
 void MovementController::runCommand(const std::string &command, const std::string &args)
 {
 	if (RunToggle == command) {
 		mIsRunning = true;
 	} else if (RunToggle.getInverseCommand() == command) {
 		mIsRunning = false;
-	} else if (ToggleCameraAttached == command)
-	{
+	} else if (ToggleCameraAttached == command) {
 		if (mIsFreeFlying) {
 			setCameraFreeFlying(false);
 		} else {
@@ -175,33 +153,32 @@ void MovementController::runCommand(const std::string &command, const std::strin
 		mMovementDirection.z() = -1;
 	} else if (MovementMoveDownwards.getInverseCommand() == command) {
 		mMovementDirection.z() = 0;
-/*	} else if (MovementRotateLeft == command) {
-		mAvatarCamera->yaw(Ogre::Degree(-15));
-	} else if (MovementRotateRight == command) {
-		mAvatarCamera->yaw(Ogre::Degree(15));*/
-//	} else if (MoveCameraTo == command) {
-//		if (!mIsAttached) {
-//			Ember::Tokeniser tokeniser;
-//			tokeniser.initTokens(args);
-//			std::string x = tokeniser.nextToken();
-//			std::string y = tokeniser.nextToken();
-//			std::string z = tokeniser.nextToken();
-//
-//			if (x == "" || y == "" || z == "") {
-//				return;
-//			} else {
-//				Ogre::Vector3 position(Ogre::StringConverter::parseReal(x),Ogre::StringConverter::parseReal(y),Ogre::StringConverter::parseReal(z));
-//				mFreeFlyingCameraNode->setPosition(position);
-//			}
-//		}
+		/*	} else if (MovementRotateLeft == command) {
+		 mAvatarCamera->yaw(Ogre::Degree(-15));
+		 } else if (MovementRotateRight == command) {
+		 mAvatarCamera->yaw(Ogre::Degree(15));*/
+		//	} else if (MoveCameraTo == command) {
+		//		if (!mIsAttached) {
+		//			Ember::Tokeniser tokeniser;
+		//			tokeniser.initTokens(args);
+		//			std::string x = tokeniser.nextToken();
+		//			std::string y = tokeniser.nextToken();
+		//			std::string z = tokeniser.nextToken();
+		//
+		//			if (x == "" || y == "" || z == "") {
+		//				return;
+		//			} else {
+		//				Ogre::Vector3 position(Ogre::StringConverter::parseReal(x),Ogre::StringConverter::parseReal(y),Ogre::StringConverter::parseReal(z));
+		//				mFreeFlyingCameraNode->setPosition(position);
+		//			}
+		//		}
 	}
 }
 
 bool MovementController::frameStarted(const Ogre::FrameEvent& event)
 {
 
-	if (mDecalObject)
-	{
+	if (mDecalObject) {
 		///hide the decal when we're close to it
 		if (mDecalNode->_getDerivedPosition().distance(mAvatar.getAvatarSceneNode()->_getDerivedPosition()) < 1) {
 			mDecalNode->setVisible(false);
@@ -216,7 +193,6 @@ MovementControllerMode::Mode MovementController::getMode() const
 	return mIsRunning ? MovementControllerMode::MM_RUN : MovementControllerMode::MM_WALK;
 }
 
-
 void MovementController::moveToPoint(const Ogre::Vector3& point)
 {
 	if (!mDecalNode) {
@@ -224,12 +200,12 @@ void MovementController::moveToPoint(const Ogre::Vector3& point)
 	}
 
 	//TODO: reapply the terrain lookup without using the terrain manager directly
-//	if (mDecalNode) {
-//		///make sure it's at the correct height, since the visibility of it is determined by the bounding box
-//		Ogre::Real height = EmberOgre::getSingleton().getTerrainManager()->getAdapter()->getHeightAt(point.x, point.z);
-//		mDecalNode->setPosition(Ogre::Vector3(point.x, height, point.z));
-//		mDecalNode->setVisible(true);
-//	}
+	//	if (mDecalNode) {
+	//		///make sure it's at the correct height, since the visibility of it is determined by the bounding box
+	//		Ogre::Real height = EmberOgre::getSingleton().getTerrainManager()->getAdapter()->getHeightAt(point.x, point.z);
+	//		mDecalNode->setPosition(Ogre::Vector3(point.x, height, point.z));
+	//		mDecalNode->setVisible(true);
+	//	}
 
 	WFMath::Vector<3> atlasVector = Convert::toWF<WFMath::Vector<3> >(point);
 	WFMath::Point<3> atlasPos(atlasVector.x(), atlasVector.y(), atlasVector.z());
@@ -237,23 +213,30 @@ void MovementController::moveToPoint(const Ogre::Vector3& point)
 	Ember::EmberServices::getSingletonPtr()->getServerService()->moveToPoint(atlasPos);
 }
 
-
 void MovementController::teleportTo(const Ogre::Vector3& point, EmberEntity* locationEntity)
 {
 	WFMath::Vector<3> atlasVector = Convert::toWF<WFMath::Vector<3> >(point);
 	WFMath::Point<3> atlasPos(atlasVector.x(), atlasVector.y(), atlasVector.z());
-
 
 	Ember::EmberServices::getSingletonPtr()->getServerService()->place(&mAvatar.getEmberEntity(), locationEntity, atlasPos);
 }
 
 WFMath::Vector<3> MovementController::getMovementForCurrentFrame() const
 {
-	//When not running, we assume that the speed is half of the max speed (which is 1.0)
-	if (mIsRunning) {
-		return WFMath::Vector<3>(mMovementDirection).normalize(1.0);
+	if (mMovementDirection.isValid()) {
+		//If the vector is null we'll get an error if we normalize, so we'll just return a zero vector then.
+		if (mMovementDirection != WFMath::Vector<3>::ZERO()) {
+			//When not running, we assume that the speed is half of the max speed (which is 1.0)
+			if (mIsRunning) {
+				return WFMath::Vector<3>(mMovementDirection).normalize(1.0);
+			} else {
+				return WFMath::Vector<3>(mMovementDirection).normalize(1.0) * 0.5;
+			}
+		} else {
+			return mMovementDirection;
+		}
 	} else {
-		return WFMath::Vector<3>(mMovementDirection).normalize(1.0) * 0.5;
+		return WFMath::Vector<3>::ZERO();
 	}
 }
 
@@ -264,14 +247,11 @@ void MovementController::createDecal(Ogre::Vector3 position)
 		Ogre::SceneManager* sceneManager = EmberOgre::getSingleton().getSceneManager();
 		Ogre::NameValuePairList params;
 		params["materialName"] = "/global/ember/terraindecal";
-		params["width"] = StringConverter::toString( 2 );
-		params["height"] = StringConverter::toString( 2 );
+		params["width"] = StringConverter::toString(2);
+		params["height"] = StringConverter::toString(2);
 		params["sceneMgrInstance"] = sceneManager->getName();
 
-		mDecalObject = sceneManager->createMovableObject(
-			"MovementControllerMoveToDecal",
-			"PagingLandScapeMeshDecal",
-			&params );
+		mDecalObject = sceneManager->createMovableObject("MovementControllerMoveToDecal", "PagingLandScapeMeshDecal", &params);
 
 		// Add MeshDecal to Scene
 		mDecalNode = sceneManager->createSceneNode("MovementControllerMoveToDecalNode");
@@ -279,16 +259,14 @@ void MovementController::createDecal(Ogre::Vector3 position)
 		EmberOgre::getSingleton().getWorldSceneNode()->addChild(mDecalNode);
 		mDecalNode->setPosition(position);
 		mDecalNode->attachObject(mDecalObject);
-	// 	mDecalNode->showBoundingBox(true);
+		// 	mDecalNode->showBoundingBox(true);
 
 
-// 		mPulsatingController = new Ogre::WaveformControllerFunction(Ogre::WFT_SINE, 1, 0.33, 0.25);
-	} catch (const Ogre::Exception& ex)
-	{
+		// 		mPulsatingController = new Ogre::WaveformControllerFunction(Ogre::WFT_SINE, 1, 0.33, 0.25);
+	} catch (const Ogre::Exception& ex) {
 		S_LOG_WARNING("Error when creating terrain decal." << ex);
 	}
 }
-
 
 }
 
