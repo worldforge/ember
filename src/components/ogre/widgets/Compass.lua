f53bac97b716ea2a703eb6adbf4683f1ea3438df
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2010-05-26 13:29:25 +0200

    Make the compass an object instance, so that it can more easily be deleted when not needed anymore.

--- src/components/ogre/widgets/Compass.lua
+++ src/components/ogre/widgets/Compass.lua
@@ -11,114 +11,116 @@ zoomInButton = nil,
 anchor = nil
 }
 
-function Compass.Refresh_Clicked(args)
-	Compass.helper:refresh()
-	Compass.helper:getMap():render()
+function Compass:Refresh_Clicked(args)
+	self.helper:refresh()
+	self.helper:getMap():render()
 	return true
 end
 
-function Compass.ZoomIn_Clicked(args)
-	local newResolution = Compass.helper:getMap():getResolution() - 0.2
+function Compass:ZoomIn_Clicked(args)
+	local newResolution = self.helper:getMap():getResolution() - 0.2
 	--prevent the user from zooming in to much (at which point only one pixel from the head of the avatar will be seen
 	if newResolution > 0.2 then
-		Compass.helper:getMap():setResolution(newResolution)
-		Compass.helper:getMap():render()
-		Compass.helper:refresh()
+		self.helper:getMap():setResolution(newResolution)
+		self.helper:getMap():render()
+		self.helper:refresh()
 	end
 	return true
 end
-function Compass.ZoomOut_Clicked(args)
-	local newResolution = Compass.helper:getMap():getResolution() + 0.2
+function Compass:ZoomOut_Clicked(args)
+	local newResolution = self.helper:getMap():getResolution() + 0.2
 	--we'll use the arbitrary resolution of 5 as the max
 	if newResolution < 5 then
-		Compass.helper:getMap():setResolution(Compass.helper:getMap():getResolution() + 0.2)
-		Compass.helper:getMap():render()
-		Compass.helper:refresh()
+		self.helper:getMap():setResolution(self.helper:getMap():getResolution() + 0.2)
+		self.helper:getMap():render()
+		self.helper:refresh()
 	end
 	return true
 end
 
 
 
-function Compass.repositionAtAvatar()
+function Compass:repositionAtAvatar()
 	local pos = emberOgre:getAvatar():getClientSideAvatarPosition()
-	Compass.helper:reposition(pos.x(), -pos.y())
+	self.helper:reposition(pos.x(), -pos.y())
 end
 
-function Compass.framestarted(frameEvent)
-	if Compass.updateFrameCountDown > 0 then
-		Compass.updateFrameCountDown = Compass.updateFrameCountDown - 1
-		if Compass.updateFrameCountDown == 0 then
+function Compass:framestarted(frameEvent)
+	if self.updateFrameCountDown > 0 then
+		self.updateFrameCountDown = self.updateFrameCountDown - 1
+		if self.updateFrameCountDown == 0 then
 			--if we haven't created any anchor yet, it means that the whole compass is uninitialized and needs to be shown, else we can just rerender the map
-			if Compass.anchor == nil then
-				Compass.initialize()
+			if self.anchor == nil then
+				self:initialize()
 			else
-				Compass.helper:getMap():render()
-				Compass.helper:refresh()
+				self.helper:getMap():render()
+				self.helper:refresh()
 			end
-			Compass.updateFrameCountDown = -1
+			self.updateFrameCountDown = -1
 		end
 	end
 end
 
-function Compass.TerrainPageGeometryUpdated(page)
+function Compass:TerrainPageGeometryUpdated(page)
 	--wait six frames until we rerender the map. This is a hack because apparently the event this listens for doesn't actually guarantee that the page will be rendered next frame. We need to add another event which is emitted when a page actually is rendered the first time.
-	Compass.updateFrameCountDown = 6
+	self.updateFrameCountDown = 6
 end
 
-function Compass.initialize()
-	Compass.anchor = EmberOgre.Gui.CompassCameraAnchor:new_local(Compass.helper, emberOgre:getMainOgreCamera())
-	if Compass.widget ~= nil then
-		Compass.widget:show()
+function Compass:initialize()
+	self.anchor = EmberOgre.Gui.CompassCameraAnchor:new_local(self.helper, emberOgre:getMainOgreCamera())
+	if self.widget ~= nil then
+		self.widget:show()
 	end
 end
 
-function Compass.CreatedAvatarEntity(avatarEntity)
-	connect(Compass.connectors, guiManager.EventFrameStarted, "Compass.framestarted")
+function Compass:CreatedAvatarEntity(avatarEntity)
+	connect(self.connectors, guiManager.EventFrameStarted, self.framestarted, self)
 end
 
-function Compass.buildWidget(terrainManager)
-	Compass.helperImpl = EmberOgre.Gui.RenderedCompassImpl:new_local()
+function Compass:buildWidget(terrainManager)
+	self.helperImpl = EmberOgre.Gui.RenderedCompassImpl:new_local()
 
-	Compass.helper = EmberOgre.Gui.Compass:new_local(Compass.helperImpl)
-	Compass.map = Compass.helper:getMap()
+	self.helper = EmberOgre.Gui.Compass:new_local(self.helperImpl)
+	self.map = self.helper:getMap()
 	
-	
-	Compass.buildCEGUIWidget()
+	self:buildCEGUIWidget()
 	
 	--don't show the compass here, instead wait until we've gotten some terrain (by listening 
-	connect(Compass.connectors, emberOgre.EventCreatedAvatarEntity, "Compass.CreatedAvatarEntity")
-	connect(Compass.connectors, terrainManager.EventTerrainPageGeometryUpdated, "Compass.TerrainPageGeometryUpdated")
+	connect(self.connectors, emberOgre.EventCreatedAvatarEntity, self.CreatedAvatarEntity, self)
+	connect(self.connectors, terrainManager.EventTerrainPageGeometryUpdated, self.TerrainPageGeometryUpdated, self)
 
 end
 
 -- Call this method to build the cegui widget.
-function Compass.buildCEGUIWidget()
-	Compass.widget = guiManager:createWidget()
-	Compass.widget:loadMainSheet("Compass.layout", "Compass/")
-	Compass.widget:setIsActiveWindowOpaque(false)
-	Compass.renderImage = Compass.widget:getWindow("RenderImage")
-	Compass.pointerImage = Compass.widget:getWindow("Pointer")
+function Compass:buildCEGUIWidget()
+	self.widget = guiManager:createWidget()
+	self.widget:loadMainSheet("Compass.layout", "Compass/")
+	self.widget:setIsActiveWindowOpaque(false)
+	self.renderImage = self.widget:getWindow("RenderImage")
+	self.pointerImage = self.widget:getWindow("Pointer")
 	
 	local assetManager = EmberOgre.Gui.AssetsManager:new_local()
 	
 	--set up the main background image
-	if Compass.helperImpl:getTexture():isNull() == false then
-		local texturePair = assetManager:createTextureImage(Compass.helperImpl:getTexture(), "CompassMap")
+	if self.helperImpl:getTexture():isNull() == false then
+		local texturePair = assetManager:createTextureImage(self.helperImpl:getTexture(), "CompassMap")
 		if texturePair:hasData() then 
-			Compass.renderImage:setProperty("Image", CEGUI.PropertyHelper:imageToString(texturePair:getTextureImage()))
+			self.renderImage:setProperty("Image", CEGUI.PropertyHelper:imageToString(texturePair:getTextureImage()))
 		end
 	end
 		
-	if Compass.helperImpl:getPointerTexture():isNull() == false then
+	if self.helperImpl:getPointerTexture():isNull() == false then
 		--also set up the pointer image
-		texturePair = assetManager:createTextureImage(Compass.helperImpl:getPointerTexture(), "CompassPointer")
+		texturePair = assetManager:createTextureImage(self.helperImpl:getPointerTexture(), "CompassPointer")
 		if texturePair:hasData() then 
-			Compass.pointerImage:setProperty("Image", CEGUI.PropertyHelper:imageToString(texturePair:getTextureImage()))
+			self.pointerImage:setProperty("Image", CEGUI.PropertyHelper:imageToString(texturePair:getTextureImage()))
 		end
 	end
 	
-	Compass.widget:hide()
+	self.widget:getWindow("ZoomOut"):subscribeEvent("Clicked", self.ZoomOut_Clicked, self) 
+	self.widget:getWindow("ZoomIn"):subscribeEvent("Clicked", self.ZoomIn_Clicked, self)
+	
+	self.widget:hide()
 end
 
-connect(Compass.connectors, emberOgre.EventTerrainManagerCreated, Compass.buildWidget)
+connect(Compass.connectors, emberOgre.EventTerrainManagerCreated, Compass.buildWidget, Compass)
