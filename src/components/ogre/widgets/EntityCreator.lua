53b744f7223ff7d4cb85ffeaae7cb71fb0e85ebf
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2010-05-30 22:28:08 +0200

    Make the entity creator shut down properly when disconnected.

--- src/components/ogre/widgets/EntityCreator.lua
+++ src/components/ogre/widgets/EntityCreator.lua
@@ -1,8 +1,8 @@
-EntityCreator = {connectors={}}
+EntityCreator = {}
 
 -- Fills recipes list at start
-function EntityCreator.fillRecipesList()
-	local list = EntityCreator.recipesList
+function EntityCreator:fillRecipesList()
+	local list = self.recipesList
 	list:resetList()
 
 	local recipeMgr = EmberOgre.Authoring.EntityRecipeManager:getSingleton()
@@ -18,84 +18,84 @@ function EntityCreator.fillRecipesList()
 end
 
 -- Handles click on recipes list
-function EntityCreator.RecipesList_SelectionChanged(args)
-	EntityCreator.clear()
+function EntityCreator:RecipesList_SelectionChanged(args)
+	self:clear()
 
-	EntityCreator.widget:getWindow("CreateSection"):setVisible(false)
-	EntityCreator.widget:getWindow("WaitForTypeInfo"):setVisible(true)
-	local item = EntityCreator.recipesList:getFirstSelectedItem()
+	self.widget:getWindow("CreateSection"):setVisible(false)
+	self.widget:getWindow("WaitForTypeInfo"):setVisible(true)
+	local item = self.recipesList:getFirstSelectedItem()
 	if item ~= nil then
 		local name = item:getText()
 		local recipeMgr = EmberOgre.Authoring.EntityRecipeManager:getSingleton()
 		local recipePtr = recipeMgr:getByName(name)
 		recipePtr = tolua.cast(recipePtr, "EmberOgre::Authoring::EntityRecipePtr")
-		EntityCreator.recipe = recipePtr:get()
+		self.recipe = recipePtr:get()
 		--We just set the recipe here and wait for the EventTypeInfoLoaded event, which will call the showRecipe function when the recipe is ready
-		EntityCreator.helper:setRecipe(EntityCreator.recipe)
+		self.helper:setRecipe(self.recipe)
 	else
-		EntityCreator.recipe = nil
+		self.recipe = nil
 	end
 end
 
 -- Handles create button press
-function EntityCreator.Create_Click(args)
-	EntityCreator.helper:toggleCreateMode()
+function EntityCreator:Create_Click(args)
+	self.helper:toggleCreateMode()
 end
 
 -- Handles toggling the randomizing of the orientation on and off
-function EntityCreator.RandomizeOrientation_CheckStateChanged(args)
-	local checkbox = EntityCreator.widget:getWindow("RandomizeOrientation")
+function EntityCreator:RandomizeOrientation_CheckStateChanged(args)
+	local checkbox = self.widget:getWindow("RandomizeOrientation")
 	checkbox = CEGUI.toCheckbox(checkbox)
 
-	EntityCreator.helper:setRandomizeOrientation(checkbox:isSelected())
+	self.helper:setRandomizeOrientation(checkbox:isSelected())
 end
 
 -- Clears widget if recipe is selected
-function EntityCreator.clear()
-	if EntityCreator.recipe ~= nil then
+function EntityCreator:clear()
+	if self.recipe ~= nil then
 		-- Detaching adapters
-		for k,v in EntityCreator.recipe:getGUIAdapters():pairs() do
+		for k,v in self.recipe:getGUIAdapters():pairs() do
 			v:detach()
 		end
 		-- Clearing container, from end to start
-		for i = EntityCreator.container:getChildCount() - 1, 0, -1 do
+		for i = self.container:getChildCount() - 1, 0, -1 do
 		    log.verbose("Destroying adapter container N" .. i)
-		    local window = EntityCreator.container:getChildAtIdx(i)
+		    local window = self.container:getChildAtIdx(i)
 			windowManager:destroyWindow(window)
 		end
 	end
-	EntityCreator.createButton:setEnabled(false)
+	self.createButton:setEnabled(false)
 
 end
 
 -- Shows selected recipe
-function EntityCreator.showRecipe()
-	local recipe = EntityCreator.recipe
+function EntityCreator:showRecipe()
+	local recipe = self.recipe
 	local author = recipe:getAuthor()
 	local description = recipe:getDescription()
 	
-	EntityCreator.createButton:setEnabled(true)
+	self.createButton:setEnabled(true)
 
 	
-	EntityCreator.widget:getWindow("CreateSection"):setVisible(true)
-	EntityCreator.widget:getWindow("WaitForTypeInfo"):setVisible(false)
+	self.widget:getWindow("CreateSection"):setVisible(true)
+	self.widget:getWindow("WaitForTypeInfo"):setVisible(false)
 	if author ~= "" then
-		description = "Author: " .. author .. "\n" .. description;
+		description = "Author: " .. author .. "\n" .. description
 	end
-	EntityCreator.recipeDescription:setText(description)
+	self.recipeDescription:setText(description)
 
 	for k,v in recipe:getGUIAdapters():pairs() do
-		log.verbose("Creating adapter " .. k .. " of type " .. v:getType())
+		log.verbose("Creating adapter '" .. k .. "' of type '" .. v:getType() .. "'.")
 
 		local container = guiManager:createWindow("DefaultGUISheet")
 		v:attach(container)
 
-		EntityCreator.addAdapter(v, v:getTitle(), container, EntityCreator.container)
+		self:addAdapter(v, v:getTitle(), container, self.container)
 	end
 end
 
 -- Adds adapter
-function EntityCreator.addAdapter(adapter, title, container, parentContainer)
+function EntityCreator:addAdapter(adapter, title, container, parentContainer)
 	local textWidth = 75
 	local outercontainer = guiManager:createWindow("DefaultGUISheet")
 	--outercontainer:setRiseOnClickEnabled(false)
@@ -120,48 +120,69 @@ function EntityCreator.addAdapter(adapter, title, container, parentContainer)
 	function syncWindowHeights(args)
 		outercontainer:setHeight(container:getHeight())
 	end
-	local SizedConnection = container:subscribeEvent("Sized", syncWindowHeights)
+	local SizedConnection = container:subscribeEvent("Sized", syncWindowHeights, self)
 
 	outercontainer:addChildWindow(label)
 	outercontainer:addChildWindow(container)
 
 	parentContainer:addChildWindow(outercontainer)
+	parentContainer:notifyScreenAreaChanged(true)
 	return outercontainer
 end
 
--- Builds widget
-function EntityCreator.buildWidget()
+function EntityCreator:shutdown()
+	guiManager:destroyWidget(self.widget)
+	self.helper:delete()
+end
 
-	-- Initializing helper classes
-	EntityCreator.helper = EmberOgre.Gui.EntityCreator()
+-- Builds widget
+function EntityCreator.buildWidget(avatar)
+
+	entityCreator = {connectors={}}
+	setmetatable(entityCreator, {__index = EntityCreator})
+		
 	
 	-- Loading widget layout
-	EntityCreator.widget = guiManager:createWidget()
+	entityCreator.widget = guiManager:createWidget()
 	local setup = function()
+		-- Initializing helper classes
+		debugObject(avatar)
+		entityCreator.helper = EmberOgre.Gui.EntityCreator:new(avatar:getConnection())
 
-		connect(EntityCreator.connectors, EntityCreator.helper.EventTypeInfoLoaded, EntityCreator.showRecipe)
-		EntityCreator.helper.mWidget = EntityCreator.widget
-		EntityCreator.factory = EmberOgre.Gui.Adapters.Atlas.AdapterFactory("EntityCreator")
+		connect(entityCreator.connectors, entityCreator.helper.EventTypeInfoLoaded, entityCreator.showRecipe, entityCreator)
+		entityCreator.helper.mWidget = entityCreator.widget
+		entityCreator.factory = EmberOgre.Gui.Adapters.Atlas.AdapterFactory("EntityCreator")
 	
 		-- Creating container for storing adapters
-		EntityCreator.recipeDescription = EntityCreator.widget:getWindow("RecipeDescription")
-		EntityCreator.container = EntityCreator.widget:getWindow("AdaptersContainer")
-		EntityCreator.stackableContainer = EmberOgre.Gui.StackableContainer(EntityCreator.container)
+		entityCreator.recipeDescription = entityCreator.widget:getWindow("RecipeDescription")
+		entityCreator.container = entityCreator.widget:getWindow("AdaptersContainer")
+		entityCreator.stackableContainer = EmberOgre.Gui.StackableContainer(entityCreator.container)
 	
 		-- Filling list of recipes
-		EntityCreator.recipesList = CEGUI.toListbox(EntityCreator.widget:getWindow("RecipesList"))
-		EntityCreator.fillRecipesList()
+		entityCreator.recipesList = CEGUI.toListbox(entityCreator.widget:getWindow("RecipesList"))
+		entityCreator.recipesList:subscribeEvent("ItemSelectionChanged", EntityCreator.RecipesList_SelectionChanged, entityCreator) 
+		entityCreator:fillRecipesList()
 		
-		EntityCreator.createButton = CEGUI.toPushButton(EntityCreator.widget:getWindow("Create"))
-		EntityCreator.createButton:setEnabled(false)
+		entityCreator.createButton = CEGUI.toPushButton(entityCreator.widget:getWindow("Create"))
+		entityCreator.createButton:subscribeEvent("Clicked", EntityCreator.Create_Click, entityCreator) 
+		entityCreator.createButton:setEnabled(false)
+
+		entityCreator.widget:getWindow("RandomizeOrientation"):subscribeEvent("CheckStateChanged", EntityCreator.RandomizeOrientation_CheckStateChanged, entityCreator) 
 	
 		-- Finalizing
-		EntityCreator.widget:enableCloseButton()
+		entityCreator.widget:enableCloseButton()
 	end
-	connect(EntityCreator.connectors, EntityCreator.widget.EventFirstTimeShown, setup)
-	EntityCreator.widget:registerConsoleVisibilityToggleCommand("advEntityCreator")
-	EntityCreator.widget:loadMainSheet("EntityCreator.layout", "EntityCreator/")
+	connect(entityCreator.connectors, entityCreator.widget.EventFirstTimeShown, setup)
+	entityCreator.widget:registerConsoleVisibilityToggleCommand("advEntityCreator")
+	entityCreator.widget:loadMainSheet("EntityCreator.layout", "EntityCreator/")
+	
+	connect(entityCreator.connectors, emberServices:getServerService().DestroyedAccount, function()
+			entityCreator:shutDown()
+			entityCreator = nil
+		end
+		, entityCreator)
 	
 end
 
-EntityCreator.buildWidget()
+connect(connectors, emberServices:getServerService().GotAvatar, EntityCreator.buildWidget)
+
