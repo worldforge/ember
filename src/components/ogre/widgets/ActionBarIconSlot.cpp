836ab384d15c2dc75cab6d2ba9640293d610c24a
Tiberiu Paunescu <4lyfestile@gmail.com>
2010-07-12 07:45:02 -0700

    Add missing ActionBarIconSlot class.

---
+++ src/components/ogre/widgets/ActionBarIconSlot.cpp
@@ -0,0 +1,111 @@
+//
+// C++ Implementation: ActionBarIconSlot
+//
+// Description:
+//
+//	Author Tiberiu Paunescu <tpa12@sfu.ca>, (C) 2010
+//	Based on the EntityIcon class by Erik Hjortsberg <erik.hjortsberg@gmail.com>, (C) 2007
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "ActionBarIconSlot.h"
+#include "Widget.h"
+#include "ActionBarIconManager.h"
+#include "ActionBarIcon.h"
+#include <CEGUI.h>
+#include <CEGUIWindowManager.h>
+
+using namespace CEGUI;
+namespace EmberOgre {
+
+namespace Gui {
+
+ActionBarIconSlot::ActionBarIconSlot(CEGUI::Window* container)
+: ActionBarIconDragDropTarget(container), mContainer(container), mContainedIcon(0)
+{
+}
+
+ActionBarIconSlot::~ActionBarIconSlot()
+{
+	removeActionBarIcon();
+	CEGUI::WindowManager::getSingleton().destroyWindow(mContainer);
+}
+
+
+bool ActionBarIconSlot::addActionBarIcon(ActionBarIcon* icon)
+{
+	if (icon) {
+		if (!mContainedIcon) {
+			//Resize the icon window to fit entirely in the slot window.
+			icon->getDragContainer()->setSize(this->getWindow()->getSize());
+			mContainedIcon = icon;
+			mContainer->addChildWindow(icon->getDragContainer());
+			icon->setSlot(this);
+			///we have to notify the container that things have changed else it won't update itself when we add the entity without dragging (cegui bug?)
+			mContainer->notifyScreenAreaChanged();
+			mContainer->invalidate();
+		} else {
+			S_LOG_WARNING("Trying to add entity icon to slot that already has one icon contained.");
+			return false;
+		}
+	}
+	return true;
+}
+
+ActionBarIcon* ActionBarIconSlot::removeActionBarIcon()
+{
+	if (mContainedIcon) {
+		mContainer->removeChildWindow(mContainedIcon->getDragContainer());
+		ActionBarIcon* icon = mContainedIcon;
+		mContainedIcon = 0;
+		icon->setSlot(0);
+		///we have to notify the container that things have changed else it won't update itself when we remove the entity without dragging (cegui bug?)
+		mContainer->notifyScreenAreaChanged();
+		mContainer->invalidate();
+		return icon;
+	} else {
+		return 0;
+	}
+}
+
+ActionBarIcon* ActionBarIconSlot::getActionBarIcon()
+{
+	return mContainedIcon;
+}
+
+
+void ActionBarIconSlot::notifyIconRemoved()
+{
+	removeActionBarIcon();
+	mContainedIcon = 0;
+}
+
+CEGUI::Window* ActionBarIconSlot::getWindow()
+{
+	return mContainer;
+}
+
+void ActionBarIconSlot::notifyIconDraggedOff(ActionBarIcon* actionBarIcon)
+{
+	EventIconDraggedOff.emit(actionBarIcon);
+}
+
+}
+
+}
---
+++ src/components/ogre/widgets/ActionBarIconSlot.h
@@ -0,0 +1,132 @@
+//
+// C++ Interface: ActionBarIconSlot
+//
+// Description:
+//
+//	Author Tiberiu Paunescu <tpa12@sfu.ca>, (C) 2010
+//	Based on the EntityIcon class by Erik Hjortsberg <erik.hjortsberg@gmail.com>, (C) 2007
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef EMBEROGRE_GUIACTIONBARICONSLOT_H
+#define EMBEROGRE_GUIACTIONBARICONSLOT_H
+
+#include <sigc++/signal.h>
+#include "ActionBarIconDragDropTarget.h"
+
+namespace CEGUI {
+class DragContainer;
+class Window;
+class EventArgs;
+}
+
+namespace EmberOgre {
+
+namespace Gui {
+
+class ActionBarIcon;
+class ActionBarIconManager;
+
+/**
+@author Erik Hjortsberg <erik.hjortsberg@gmail.com>
+
+@brief A slot for an entity icon.
+
+An instance of this represent a slot into which an entity icon can be placed. Icons can be dragged both to and from a slot.
+A typical usage example would be an inventory, where each entity icon is stored in a slot. By dragging entity icons between different slots the inventory is rearranged.
+
+@note You don't normally directly create or destroy an instance of this. Instead use the EntityIconManager.
+*/
+class ActionBarIconSlot : public ActionBarIconDragDropTarget
+{
+friend class ActionBarIconManager;
+friend class ActionBarIcon;
+public:
+
+	/**
+	 * @brief Adds an EntityIcon to the slot. If the slot already contains an icon, the method will return false and an error will be logged.
+	 * @param icon
+	 * @return true if successful, else false
+	 */
+
+	bool addActionBarIcon(ActionBarIcon* icon);
+
+	/**
+	 * @brief Removes and returns the contained EntityIcon. If none is contained, nothing will happen.
+	 * @return The contained EntityIcon or null if none contained.
+	 */
+	ActionBarIcon* removeActionBarIcon();
+
+	/**
+	 * @brief Gets the contained EntityIcon
+	 * @return
+	 */
+	ActionBarIcon* getActionBarIcon();
+
+	/**
+	 * @brief Gets the window which this slot is using.
+	 * @returns The window used by this slot.
+	 */
+	CEGUI::Window* getWindow();
+
+	/**
+	 * @brief Call this from the gui code whenever an icon is dragged away from the slot.
+	 */
+	void notifyIconDraggedOff(ActionBarIcon* entityIcon);
+
+	/**
+	 * @brief Emitted when an icon has been dragged away from the slot as a result of an user action.
+	 */
+	sigc::signal<void, ActionBarIcon*> EventIconDraggedOff;
+
+
+
+protected:
+	/**
+	 * @brief Ctor.
+	 *
+	 * @param The main entity icon manager.
+	 * @param container The CEGUI window which will be used by the slot. This can be any window, but should preferably be something which makes it clear that it's a slot onto which an icon can be dragged and dropped.
+	 */
+	ActionBarIconSlot(CEGUI::Window* container);
+
+	/**
+	 * @brief Dtor.
+	 * Any icon contained in the slot will be removed upon destruction.
+	 */
+	virtual ~ActionBarIconSlot();
+
+	/**
+	 * @brief The CEGUI window representing the slot.
+	 */
+	CEGUI::Window* mContainer;
+
+	/**
+	 * @brief Holds any entity icon currently contained in the slot.
+	 */
+	ActionBarIcon* mContainedIcon;
+
+	/**
+	 * @brief Internal method called by the EntityIcon when it's removed from the slot.
+	 */
+	void notifyIconRemoved();
+
+};
+
+}
+
+}
+
+#endif
