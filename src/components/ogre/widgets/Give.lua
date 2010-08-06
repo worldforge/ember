7f671b006b83033d08e51aa918a0550955697347
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2010-08-06 19:58:17 +0200

    Use proper OO techniques and clean up when leaving the world.

--- src/components/ogre/widgets/Give.lua
+++ src/components/ogre/widgets/Give.lua
@@ -3,85 +3,95 @@
 --A widget for giving something from the inventory to someone. Right now just shows a list of stuff that can be given. This will need to be extended to something much nicer. Perhaps even something which interacts with other inventory widgets.
 
 -----------------------------------------
-Give = {connectors={}}
-Give.listbox = nil
-Give.targetEntityId = nil
-Give.listboxMap = {}
-Give.widget = guiManager:createWidget()
+Give = {}
 
-function Give.buildWidget()
-	
-	Give.widget:loadMainSheet("Give.layout", "Give/")
-	
-	Give.widget:hide()
-	
-	connect(Give.connectors, emberOgre.EventCreatedAvatarEntity, "Give.createdAvatarEmberEntity")
-	
-	giveButton = Give.widget:getWindow("Give")
-	giveButton:subscribeEvent("Clicked", "Give.Give_Click")
-	
-	cancelButton = Give.widget:getWindow("Cancel")
-	cancelButton:subscribeEvent("Clicked", "Give.Cancel_Click")
+function Give:buildWidget(avatar)
 
-	connect(Give.connectors, guiManager.EventEntityAction, "Give.handleAction")
+	self.widget = guiManager:createWidget()
 	
-	local widget = Give.widget:getWindow("ListBox")
-	Give.listbox = CEGUI.toListbox(widget)
+	self.widget:loadMainSheet("Give.layout", "Give/")
+	
+	self.widget:hide()
+	
+	connect(self.connectors, avatar.EventAddedEntityToInventory, self.addedEntity, self)
+	connect(self.connectors, avatar.EventRemovedEntityFromInventory, self.removedEntity, self)
+	
+	giveButton = self.widget:getWindow("Give")
+	giveButton:subscribeEvent("Clicked", self.Give_Click, self)
+	
+	cancelButton = self.widget:getWindow("Cancel")
+	cancelButton:subscribeEvent("Clicked", self.Cancel_Click, self)
+
+	connect(self.connectors, guiManager.EventEntityAction, self.handleAction, self)
+	
+	local widget = self.widget:getWindow("ListBox")
+	self.listbox = CEGUI.toListbox(widget)
 end
 
-function Give.createdAvatarEmberEntity(avatarEntity)
-	connect(Give.connectors, emberOgre:getWorld():getAvatar().EventAddedEntityToInventory, "Give.addedEntity")
-	connect(Give.connectors, emberOgre:getWorld():getAvatar().EventRemovedEntityFromInventory, "Give.removedEntity")
-end
-
-function Give.addedEntity(entity)
+function Give:addedEntity(entity)
 	local name = entity:getType():getName() .. " (" .. entity:getId() .. " : " .. entity:getName() .. ")"
 	local item = EmberOgre.Gui.ColouredListItem:new(name, entity:getId(), entity)
-	Give.listboxMap[entity] = item
+	self.listboxMap[entity] = item
 	--we need to cast it down
-	Give.listbox:addItem(item)
+	self.listbox:addItem(item)
 end
 
-function Give.removedEntity(entity) 
-	local item = Give.listboxMap[entity];
+function Give:removedEntity(entity) 
+	local item = self.listboxMap[entity];
 	if item ~= nil then
-		Give.listbox:removeItem(tolua.cast(item, "CEGUI::ListboxItem"))
-		Give.listboxMap[entity] = nil
+		self.listbox:removeItem(tolua.cast(item, "CEGUI::ListboxItem"))
+		self.listboxMap[entity] = nil
 	end
 end
 
-function Give.Give_Click(args)
-	local item = Give.listbox:getFirstSelectedItem()
+function Give:Give_Click(args)
+	local item = self.listbox:getFirstSelectedItem()
 	while (item ~= nil) do
 		local entityId = item:getID()
 		local entity = emberOgre:getWorld():getEmberEntity(entityId);
 		if (entity ~= nil) then
-			emberOgre:doWithEntity(Give.targetEntityId, function (targetEntity) 
+			emberOgre:doWithEntity(self.targetEntityId, function (targetEntity) 
 				emberServices:getServerService():place(entity, targetEntity)
 			end)
 		end
-		item = Give.listbox:getNextSelected(item)
+		item = self.listbox:getNextSelected(item)
 	end
 end
 
-function Give.Cancel_Click(args)
-	Give.widget:hide()
+function Give:Cancel_Click(args)
+	self.widget:hide()
 end
 
-function Give.handleAction(action, entity) 
+function Give:handleAction(action, entity) 
 
 	if action == "give" then
-		Give.show(entity)
+		self:show(entity)
 	end
 end
 
-function Give.show(entity)
-	Give.targetEntityId = entity:getId()
-	Give.widget:show()
-	local textWidget = Give.widget:getWindow("Text")
+function Give:show(entity)
+	self.targetEntityId = entity:getId()
+	self.widget:show()
+	local textWidget = self.widget:getWindow("Text")
 	local text = "Give to " .. entity:getName() .. " ( a " .. entity:getType():getName() .. ")"
 	textWidget:setText(text)
 
 end
 
-Give.buildWidget()
+function Give:shutdown()
+	disconnectAll(self.connectors)
+	guiManager:destroyWidget(self.widget)
+end
+
+Give.startConnector = EmberOgre.LuaConnector:new_local(emberOgre.EventCreatedAvatarEntity):connect(function()
+		local give = {connectors={}, listbox = nil, targetEntityId = nil, listboxMap = {}}
+		setmetatable(give, {__index = Give})
+		give:buildWidget(emberOgre:getWorld():getAvatar())
+		
+		connect(give.connectors, emberOgre.EventWorldDestroyed, function()
+			give:shutdown()
+			give = nil
+		end
+		)
+	end
+)
