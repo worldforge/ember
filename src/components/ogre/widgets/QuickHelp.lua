5a0a4f2e6b95b6d8ca6bb9cdc02f635498a45844
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2010-08-18 22:31:21 +0200

    Adapt the time the help window is shown to the number of words in it.

--- src/components/ogre/widgets/QuickHelp.lua
+++ src/components/ogre/widgets/QuickHelp.lua
@@ -3,8 +3,8 @@ QuickHelp = {}
 function QuickHelp.buildWidget()
 	quickhelp = {
 		helper = nil,
-		timeToShowBlurb = 0,
-		timeToFade = 0,
+		timeToShowBlurb = 4,
+		timeToFade = 5,
 		timeBlurbShown = 0,
 		hidden = false,
 		updateText_connector = nil,
@@ -13,10 +13,6 @@ function QuickHelp.buildWidget()
 	setmetatable(quickhelp, {__index = QuickHelp})
 		
 	quickhelp.helper = EmberOgre.Gui.QuickHelpCursor:new_local()
-	
-	quickhelp.timeToShowBlurb = 4
-	quickhelp.timeToFade = 5
-	quickhelp.timeBlurbShown = 0
 	
 	quickhelp.updateText_connector = createConnector(quickhelp.helper.EventUpdateText):connect(quickhelp.updateText, quickhelp)
 	quickhelp.toggleVisibility_connector = createConnector(EmberOgre.Gui.QuickHelp:getSingleton().EventToggleWidgetVisibility):connect(quickhelp.toggleVisibility, quickhelp)
@@ -71,6 +67,7 @@ function QuickHelp:toggleVisibility()
 end
 
 function QuickHelp:updateText(helpMessage)
+	local text = helpMessage:getHelp()
 	if not self.hidden then
 		if not self.widget:isVisible() then
 			if self.updateAlpha_connector then
@@ -78,11 +75,16 @@ function QuickHelp:updateText(helpMessage)
 			end
 			self.updateAlpha_connector = createConnector(self.widget.EventFrameStarted):connect(self.frameStarted, self)
 			self.timeBlurbShown = 0
+			
+			--Adapt the time the help widget is shown to how many words there are in the text
+			local words = 0
+			for word in string.gfind(text, "[^%s]+") do words=words+1 end
+			self.timeToShowBlurb = math.max(words / 2.0, 4) --a normal human reads about 250 words per minute, which is ~4 words per second. We'll go for 3 words per second. Show it no lesser than four seconds 
 		end
 		self.widget:show()
 		self.widget:getMainWindow():setAlpha(1.0)
 	end
-	self.textWindow:setText(helpMessage:getHelp())
+	self.textWindow:setText(text)
 	self.widget:getMainWindow():setText("Help - " .. helpMessage:getTitle())
 	self.messagePosition:setText(self.helper:getCursorLocation() .. "/" .. self.helper:getSize())
 	self:updateButtons()
