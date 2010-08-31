65e78878b801b66dfab924f4c207a903b4e5eff8
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2010-09-01 00:18:02 +0200

    Added a tooltip type which automatically can show detailed information about an entity.

---
+++ src/components/ogre/widgets/EntityTooltip.cpp
@@ -0,0 +1,167 @@
+/*
+ Copyright (C) 2010  Erik Hjortsberg <erik.hjortsberg@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "EntityTooltip.h"
+#include "components/ogre/World.h"
+#include "components/ogre/EmberEntity.h"
+#include "components/ogre/widgets/icons/IconManager.h"
+#include "components/ogre/widgets/icons/Icon.h"
+
+#include <Eris/TypeInfo.h>
+
+#include <CEGUISubscriberSlot.h>
+#include <CEGUIPropertyHelper.h>
+#include <CEGUIWindowManager.h>
+#include <CEGUIWindowFactoryManager.h>
+#include <CEGUITplWindowFactory.h>
+#include <CEGUIExceptions.h>
+
+#include <Atlas/Message/Element.h>
+
+#include <vector>
+namespace EmberOgre
+{
+namespace Gui
+{
+
+static const CEGUI::String WidgetTypeName;
+
+EmberEntityTooltipWidget::EmberEntityTooltipWidget(const CEGUI::String& type, const CEGUI::String& name) :
+	CEGUI::Tooltip(type, name)
+{
+}
+
+CEGUI::Size EmberEntityTooltipWidget::getTextSize_impl() const
+{
+	return CEGUI::Size(180, 80);
+}
+
+const CEGUI::String EmberEntityTooltipWidget::WidgetTypeName("Ember/EntityTooltip");
+
+void EntityTooltip::registerFactory()
+{
+	CEGUI::WindowFactoryManager::addFactory<CEGUI::TplWindowFactory<EmberEntityTooltipWidget> >();
+}
+
+EntityTooltip::EntityTooltip(World& world, EmberEntityTooltipWidget& tooltip, Icons::IconManager& iconManager) :
+	mWorld(world), mTooltip(tooltip), mIconManager(iconManager), isUpdatingOurselves(false), mImageWindow(0), mTextWindow(0)
+{
+	mImageWindow = CEGUI::WindowManager::getSingleton().createWindow("EmberLook/StaticImage");
+	mImageWindow->setSize(CEGUI::UVector2(CEGUI::UDim(0, 64), CEGUI::UDim(0, 64)));
+	mImageWindow->setProperty("FrameEnabled", "false");
+	mImageWindow->setProperty("BackgroundEnabled", "false");
+	tooltip.addChildWindow(mImageWindow);
+
+	mTextWindow = CEGUI::WindowManager::getSingleton().createWindow("EmberLook/StaticText");
+	mTextWindow->setSize(CEGUI::UVector2(CEGUI::UDim(1, -64), CEGUI::UDim(1, 0)));
+	mTextWindow->setPosition(CEGUI::UVector2(CEGUI::UDim(0, 64), CEGUI::UDim(0, 0)));
+	mTextWindow->setProperty("HorzFormatting", "WordWrapLeftAligned");
+	mTextWindow->setProperty("VertFormatting", "TopAligned");
+	mTextWindow->setProperty("FrameEnabled", "false");
+	mTextWindow->setProperty("BackgroundEnabled", "false");
+	tooltip.addChildWindow(mTextWindow);
+
+	//listen to the text being changed, since that indicates that the tooltip has been activated for a new window (alas there's no signal when the tooltip is attached to a target window, so this is the best we can do)
+	tooltip.subscribeEvent(CEGUI::Window::EventTextChanged, CEGUI::Event::Subscriber(&EntityTooltip::tooltip_TextChanged, this));
+}
+
+EntityTooltip::~EntityTooltip()
+{
+	CEGUI::WindowManager::getSingleton().destroyWindow(mImageWindow);
+	CEGUI::WindowManager::getSingleton().destroyWindow(mTextWindow);
+}
+
+CEGUI::Tooltip& EntityTooltip::getTooltipWindow() const
+{
+	return mTooltip;
+}
+
+bool EntityTooltip::tooltip_TextChanged(const CEGUI::EventArgs &e)
+{
+	//Check the guard so we don't end up in an infinite loop, as we'll be resetting the text of the tooltip.
+	if (!isUpdatingOurselves) {
+		EmberEntity* entity = getActiveEntity();
+		if (entity) {
+			isUpdatingOurselves = true;
+			mTextWindow->setText(composeEntityInfoText(*entity));
+			Icons::Icon* icon = mIconManager.getIcon(64, entity);
+			if (icon) {
+				mImageWindow->setProperty("Image", CEGUI::PropertyHelper::imageToString(icon->getImage()));
+			} else {
+				mImageWindow->setProperty("Image", "");
+			}
+			mTooltip.setWidth(CEGUI::UDim(0, 120));
+			mTooltip.setHeight(CEGUI::UDim(0, 80));
+			mTooltip.positionSelf();
+		}
+		mTooltip.setText(""); //The text has contained the id of the entity and should now be removed.
+	}
+	isUpdatingOurselves = false;
+	return true;
+}
+
+std::string EntityTooltip::composeEntityInfoText(EmberEntity& entity)
+{
+	std::stringstream ss;
+	if (entity.getName() != "") {
+		ss << entity.getName() << " (of type " << entity.getType()->getName() << ")";
+	} else {
+		ss << entity.getType()->getName();
+	}
+	if (entity.hasAttr("biomass")) {
+		ss << std::endl << "Edible";
+	}
+	if (entity.hasAttr("right_hand_wield")) {
+		ss << std::endl << "Wieldable";
+	}
+	if (entity.hasAttr("worn")) {
+		const Atlas::Message::Element& element = entity.valueOfAttr("worn");
+		if (element.isString()) {
+			ss << std::endl << "Worn on the " << element.asString();
+		}
+	}
+	std::vector<std::string> actions = entity.getActions();
+	if (actions.size()) {
+		for (std::vector<std::string>::const_iterator I = actions.begin(); I != actions.end(); ++I) {
+			ss << std::endl << "Can be used to " << *I;
+		}
+	}
+	std::vector<std::string> operations = entity.getDefaultUseOperators();
+	if (operations.size()) {
+		for (std::vector<std::string>::const_iterator I = operations.begin(); I != operations.end(); ++I) {
+			ss << std::endl << "Can be used to " << *I;
+		}
+	}
+
+	return ss.str();
+}
+
+EmberEntity* EntityTooltip::getActiveEntity()
+{
+	if (mTooltip.getTargetWindow()) {
+		return mWorld.getEmberEntity(mTooltip.getTargetWindow()->getTooltipText().c_str());
+	}
+	return 0;
+}
+
+}
+}
---
+++ src/components/ogre/widgets/EntityTooltip.h
@@ -0,0 +1,158 @@
+/*
+ Copyright (C) 2010  Erik Hjortsberg <erik.hjortsberg@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef EMBEROGRE_GUI_ENTITYTOOLTIP_H_
+#define EMBEROGRE_GUI_ENTITYTOOLTIP_H_
+
+#include <string>
+#include <elements/CEGUITooltip.h>
+
+namespace CEGUI {
+class Tooltip;
+class EventArgs;
+class Window;
+}
+
+namespace EmberOgre {
+class EmberEntity;
+class World;
+namespace Gui {
+
+namespace Icons {
+class IconManager;
+}
+
+/**
+ * @author Erik Hjortsberg <erik.hjortsberg@gmail.com>
+ * @brief A Tooltip subclass which allows for an alternative way to calculate the size of the tooltip.
+ *
+ * This is used to show entity bound tooltip information.
+ *
+ */
+class EmberEntityTooltipWidget: public CEGUI::Tooltip
+{
+
+public:
+	static const CEGUI::String WidgetTypeName;
+
+	EmberEntityTooltipWidget(const CEGUI::String& type, const CEGUI::String& name);
+
+	CEGUI::Size getTextSize_impl() const;
+};
+
+/**
+ * @author Erik Hjortsberg <erik.hjortsberg@gmail.com>
+ * @brief Allows advanced entity specific tooltips to be used.
+ *
+ * This class provides mechanism through which a CEGUI::Tooltip can be setup to show information for a certain entity. This is done by setting the tooltip text of the window for which you want this functionality to the entity id, and then tell the window to use the specific EmberEntityTooltipWidget instance wrapped by this class.
+ * This will set everything up so that when the tooltip is shown, the entity id will be used to fetch the correct entity from the World, and information about it will be shown.
+ */
+class EntityTooltip
+{
+public:
+
+	/**
+	 * @brief Ctor.
+	 * @param world The world, from which entities will be extracted.
+	 * @param tooltip A tooltip instance. Ownership will not be passed. This window will be altered.
+	 * @param iconManager The icon manager, from which icons for the entities will be extracted.
+	 */
+	EntityTooltip(World& world, EmberEntityTooltipWidget& tooltip, Icons::IconManager& iconManager);
+
+	/**
+	 * @brief Dtor.
+	 */
+	virtual ~EntityTooltip();
+
+	/**
+	 * @brief Accessor for the tooltip window.
+	 *
+	 * @returns The tooltip window.
+	 */
+	CEGUI::Tooltip& getTooltipWindow() const;
+
+	/**
+	 * @brief Registers the EmberEntityTooltipWidget widget factory, which allows widgets of type "Ember/EntityTooltip" to be created.
+	 * @note Call this as soon as you can, after CEGUI has been initialized.
+	 */
+	static void registerFactory();
+
+protected:
+
+	/**
+	 * @brief The world, used for querying for entities.
+	 */
+	World& mWorld;
+
+	/**
+	 * @brief The tooltip widget instance, not owned by this class.
+	 */
+	EmberEntityTooltipWidget& mTooltip;
+
+	/**
+	 * @brief The icon manager, from which we'll extract entity icons.
+	 */
+	Icons::IconManager& mIconManager;
+
+	/**
+	 * @brief A guard to prevent infinite loops when both listening to the TextChanged event of the tooltip and in that method also updating the text itself.
+	 */
+	bool isUpdatingOurselves;
+
+	/**
+	 * @brief A window containing an image of the entity.
+	 */
+	CEGUI::Window* mImageWindow;
+
+	/**
+	 * @brief A window containing text about the entity.
+	 */
+	CEGUI::Window* mTextWindow;
+
+	/**
+	 * @brief Catch the text being changed for the tooltip, as this indicates that the target window has changed.
+	 *
+	 * Preferably we would like to listen to a signal emitted when the target window changes, but alas there is no such signal.
+	 * @param e Events args.
+	 */
+	bool tooltip_TextChanged(const CEGUI::EventArgs &e);
+
+	/**
+	 * @brief Tries to extract the active entity of the tooltip target window.
+	 *
+	 * This is done by using the text of the tooltip as the entity key, and querying the World instance for it.
+	 * If no entity could be found (it might have been deleted) null will be returned.
+	 *
+	 * @returns The active entity, or null if no entity could be found.
+	 */
+	EmberEntity* getActiveEntity();
+
+	/**
+	 * @brief Composes an informative text about the entity.
+	 *
+	 * @param The entity for which we want a descriptive text.
+	 * @returns A descriptive text of the entity.
+	 */
+	std::string composeEntityInfoText(EmberEntity& entity);
+
+};
+
+}
+}
+
+#endif /* ENTITYTOOLTIP_H_ */
