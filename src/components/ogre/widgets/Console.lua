88f30a6fde8d21404680aeea8b3760b7729eca12
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2010-10-27 22:23:33 +0200

    Properly clean up when done.

--- src/components/ogre/widgets/Console.lua
+++ src/components/ogre/widgets/Console.lua
@@ -3,33 +3,26 @@
 ----------------------------------------------------
 
 
-Console = {
-connectors={}, 
-widget = guiManager:createWidget(), 
-gameTab = {unviewedCount = 0},
-systemTab = {unviewedCount = 0},
-consoleAdapter = nil,
-consoleInputWindow = nil
-}
+Console = {}
 
 --Set up the widget.
-function Console.buildWidget()
-	Console.tabs = {Console.gameTab, Console.systemTab}
+function Console:buildWidget()
+	self.tabs = {self.gameTab, self.systemTab}
 	
-	Console.widget:loadMainSheet("Console.layout", "Console/")
+	self.widget:loadMainSheet("Console.layout", "Console/")
 	
-	Console.gameTab.textWindow = CEGUI.toMultiLineEditbox(Console.widget:getWindow("GameTextBox"))
-	Console.gameTab.tabWindow = CEGUI.toMultiLineEditbox(Console.widget:getWindow("GamePanel"))
-	Console.gameTab.prefix = Console.gameTab.tabWindow:getText()
+	self.gameTab.textWindow = CEGUI.toMultiLineEditbox(self.widget:getWindow("GameTextBox"))
+	self.gameTab.tabWindow = CEGUI.toMultiLineEditbox(self.widget:getWindow("GamePanel"))
+	self.gameTab.prefix = self.gameTab.tabWindow:getText()
 	
-	Console.systemTab.textWindow = CEGUI.toMultiLineEditbox(Console.widget:getWindow("SystemTextBox"))
-	Console.systemTab.tabWindow = CEGUI.toMultiLineEditbox(Console.widget:getWindow("SystemPanel"))
-	Console.systemTab.prefix = Console.systemTab.tabWindow:getText()
+	self.systemTab.textWindow = CEGUI.toMultiLineEditbox(self.widget:getWindow("SystemTextBox"))
+	self.systemTab.tabWindow = CEGUI.toMultiLineEditbox(self.widget:getWindow("SystemPanel"))
+	self.systemTab.prefix = self.systemTab.tabWindow:getText()
 
 	--When a tab has been selected and shown, the unread indicator should be reset	
-	local tabControl = CEGUI.toTabControl(Console.widget:getWindow("MainTabControl"))
-	Console.widget:getWindow("MainTabControl"):subscribeEvent("TabSelectionChanged", function(args)
-			local selectedTab = Console.tabs[tabControl:getSelectedTabIndex() + 1] --Lua uses 1 as the first index
+	local tabControl = CEGUI.toTabControl(self.widget:getWindow("MainTabControl"))
+	self.widget:getWindow("MainTabControl"):subscribeEvent("TabSelectionChanged", function(args)
+			local selectedTab = self.tabs[tabControl:getSelectedTabIndex() + 1] --Lua uses 1 as the first index
 			selectedTab.tabWindow:setText(selectedTab.prefix)
 			selectedTab.unviewedCount = 0
 			return true	
@@ -37,67 +30,68 @@ function Console.buildWidget()
 	)
 	
 	
-	Console.consoleInputWindow = CEGUI.toEditbox(Console.widget:getWindow("InputBox"))
+	self.consoleInputWindow = CEGUI.toEditbox(self.widget:getWindow("InputBox"))
 	
 	--this will bring console functionality to the editbox (such as history, tab completion etc.)
-	Console.consoleAdapter = EmberOgre.Gui.ConsoleAdapter:new_local(Console.consoleInputWindow);
+	self.consoleAdapter = EmberOgre.Gui.ConsoleAdapter:new_local(self.consoleInputWindow);
 	
 	
-	connect(Console.connectors, guiManager.AppendIGChatLine, "Console.appendIGChatLine")
-	connect(Console.connectors, guiManager.AppendOOGChatLine, "Console.appendIGChatLine")
-	connect(Console.connectors, guiManager.AppendAvatarImaginary, "Console.appendAvatarImaginary")
-	connect(Console.connectors, Console.consoleAdapter.EventCommandExecuted, "Console.consoleAdapter_CommandExecuted")
+	connect(self.connectors, guiManager.AppendIGChatLine, self.appendIGChatLine, self)
+	connect(self.connectors, guiManager.AppendOOGChatLine, self.appendIGChatLine, self)
+	connect(self.connectors, guiManager.AppendAvatarImaginary, self.appendAvatarImaginary, self)
+	connect(self.connectors, self.consoleAdapter.EventCommandExecuted, self.consoleAdapter_CommandExecuted, self)
 	
 	
 	
-	Console.widget:show()
-	connect(Console.connectors, console.GotMessage, "Console.consoleGotMessage")
+	self.widget:show()
+	connect(self.connectors, console.GotMessage, self.consoleGotMessage, self)
 	
-	Console.consoleObject = Ember.Lua.LuaConsoleObject:new_local("console_focus", "Console.console_focus")
+	self.consoleObject = Ember.Lua.LuaConsoleObject:new("console_focus", self.console_focus)
+	self.consoleObject:setSelf(self)
 	
 
 end
 
-function Console.consoleAdapter_CommandExecuted(command)
+function Console:consoleAdapter_CommandExecuted(command)
 	--if the user presses enter with nothing entered, toggle to movement mode. This will allow for easy switching between chat and movement
 	if command == "" then
 		--remove focus from the console
-		Console.widget:getMainWindow():deactivate()
+		self.widget:getMainWindow():deactivate()
 		Ember.Input:getSingleton():setInputMode(Ember.Input.IM_MOVEMENT)
 	end
 end
 
-function Console.console_focus()
-	Console.consoleInputWindow:activate()
+function Console:console_focus()
+	self.consoleInputWindow:activate()
 	--switch to gui mode, so that this command can be called even when the user is in movement mode
 	Ember.Input:getSingleton():setInputMode(Ember.Input.IM_GUI)
 end
 
 --handler for Out Of Game chat event
 --adds messages to the top of the textbox
-function Console.appendOOGChatLine(line, entity)
+function Console:appendOOGChatLine(line, entity)
 	if entity ~= nil then
-		Console.appendLine("{" .. entity:getName() .. "}" .. line, Console.gameTab)
+		self:appendLine("{" .. entity:getName() .. "}" .. line, self.gameTab)
 	else 
-		Console.appendLine(line, Console.gameTab)
+		self:appendLine(line, self.gameTab)
 	end
 end
 
 --handler for In Game chat events
 --adds messages to the top of the textbox
-function Console.appendIGChatLine(line, entity)
+function Console:appendIGChatLine(line, entity)
 	if entity ~= nil then
-		Console.appendLine("<" .. entity:getName() .. ">" .. line, Console.gameTab)
+		self:appendLine("<" .. entity:getName() .. ">" .. line, self.gameTab)
 	else
-		Console.appendLine(line, Console.gameTab)
+		self:appendLine(line, self.gameTab)
 	end
 end
 
-function Console.appendAvatarImaginary(line)
-	Console.appendLine(line, Console.gameTab)
+function Console:appendAvatarImaginary(line)
+	self:appendLine(line, self.gameTab)
 end
 
-function Console.appendLine(line, tab)
+function Console:appendLine(line, tab)
 	local window = tab.textWindow
 --	chatString = "<" .. entity:getName() .. ">" .. line .. "\n" .. chatString
 	window:setText(window:getText() .. line)
@@ -110,9 +104,30 @@ function Console.appendLine(line, tab)
 	end 
 end
 
-function Console.consoleGotMessage(message)
-	Console.appendLine(message, Console.systemTab)
+function Console:consoleGotMessage(message)
+	self:appendLine(message, self.systemTab)
 	return true
 end
 
-Console.buildWidget()
+function Console:shutdown()
+	deleteSafe(self.consoleObject)
+end
+
+
+connect(connectors, emberOgre.EventGUIManagerInitialized, function(guiManager)
+	local console = {connectors={}, 
+		widget = guiManager:createWidget(), 
+		gameTab = {unviewedCount = 0},
+		systemTab = {unviewedCount = 0},
+		consoleAdapter = nil,
+		consoleInputWindow = nil
+	}
+	setmetatable(console, {__index = Console})
+	
+	console:buildWidget()
+
+	connect(console.connectors, emberOgre.EventGUIManagerBeingDestroyed, function()
+		console:shutdown()
+		console = nil
+	end)
+end)
