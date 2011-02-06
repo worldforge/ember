8a86b07b8e091b9420eb7ceb888d767b19c29df1
Erik Hjortsberg <erik.hjortsberg@gmail.com>
2011-02-06 17:13:51 +0100

    Added a terrain observer.
    This is an interface which allows for observation of terrain changes in
    the scene manager. We'll also provide a concrete implementation for the
    PagingLandscape scene manager currently used.

---
+++ src/components/ogre/terrain/ITerrainObserver.h
@@ -0,0 +1,83 @@
+//
+// C++ Interface: ITerrainObserver
+//
+// Description:
+//
+//
+// Author: Erik Hjortsberg <erik.hjortsberg@gmail.com>, (C) 2011
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+
+#include <sigc++/signal.h>
+
+namespace Ogre {
+template<typename>
+class TRect;
+}
+
+namespace WFMath
+{
+template<int>
+class AxisBox;
+}
+
+namespace Ember
+{
+namespace OgreView
+{
+namespace Terrain
+{
+
+/**
+ * Listener interface for an terrain observer, which is observes a particular terrain area.
+ * @author Erik Hjortsberg
+ */
+class ITerrainObserver
+{
+public:
+
+	/**
+	 * @brief Instructs the observer to observe a certain area.
+	 * @param area The area to observe.
+	 */
+	virtual void observeArea(const WFMath::AxisBox<2>& area) = 0;
+
+	/**
+	 * @brief Instructs the observer to observe a certain area.
+	 * @param area The area to observe.
+	 */
+	virtual void observeArea(const Ogre::TRect<int>& area) = 0;
+
+	/**
+	 * @brief Emitted when a piece of the area being observed is shown.
+	 */
+	sigc::signal<void> EventAreaShown;
+
+protected:
+
+	/**
+	 * @brief Dtor.
+	 *
+	 * Made protected to prevent abstract instances from being deleted.
+	 */
+	virtual ~ITerrainObserver()
+	{
+	}
+
+};
+}
+}
+}
