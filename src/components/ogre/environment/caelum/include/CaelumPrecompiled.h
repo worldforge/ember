35f7bd0ea5dae3d372698d126b4e65693de2431c
Peter Szücs <peter.szucs.dev@gmail.com>
2011-04-12 14:49:26 +0200

    Allows to enable precompiled headers in Caelum.
    To enable this feature:
    -Define CAELUM_USE_PRECOMPILED.
    -Set OgrePagingLandScapePrecompiledHeaders.h as the precompiled header.
    -Set OgrePagingLandScapePrecompiledHeaders.cpp as the precompiled
    object(gcc don't needs precompiled object).

--- src/components/ogre/environment/caelum/include/CaelumPrecompiled.h
+++ src/components/ogre/environment/caelum/include/CaelumPrecompiled.h
@@ -17,9 +17,10 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with Caelum. If not, see <http://www.gnu.org/licenses/>.
 */
-
-//#ifdef __APPLE__
-//#include "Ogre/Ogre.h"
-//#else
-//#include "Ogre.h"
-//#endif
+#ifdef CAELUM_USE_PRECOMPILED
+#ifdef __APPLE__
+#include "Ogre/Ogre.h"
+#else
+#include "Ogre.h"
+#endif
+#endif
