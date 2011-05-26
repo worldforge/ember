edc8dcdf823d510a2e17ad4e7a19f717ea20c099
Martin Preisler <preisler.m@gmail.com>
2011-05-26 17:08:20 +0200

    Fixed several issues with QuickHelp, still work in progress
    Adds hide, show and hideSlow transitions. I still have to iron out
    some minor problems.

--- src/components/ogre/widgets/QuickHelp.lua
+++ src/components/ogre/widgets/QuickHelp.lua
@@ -7,12 +7,14 @@ function QuickHelp.buildWidget()
 		timeToFade = 5,
 		timeBlurbShown = 0,
 		timeBlurbLastUpdate = -1,
+		showTransitionStarted = false,
+		hideTransitionStarted = false,
 		hidden = false,
 		updateText_connector = nil,
 		updateAlpha_connector = nil
 		}
 	setmetatable(quickhelp, {__index = QuickHelp})
-		
+	
 	quickhelp.helper = Ember.OgreView.Gui.QuickHelpCursor:new_local()
 	
 	quickhelp.updateText_connector = createConnector(quickhelp.helper.EventUpdateText):connect(quickhelp.updateText, quickhelp)
@@ -31,7 +33,8 @@ function QuickHelp:buildCEGUIWidget()
 	
 	self.frameWindow:setRollupEnabled(false)
 	
-	self.widget:enableCloseButton()
+	--self.widget:enableCloseButton()
+	self.frameWindow:subscribeEvent("CloseClicked", "QuickHelp.CloseClicked", self)
 	
 	self.widget:getWindow("Hide"):subscribeEvent("Clicked", "QuickHelp.Hide_Click", self)
 	self.widget:getWindow("Next"):subscribeEvent("Clicked", "QuickHelp.Next_Click", self)
@@ -41,36 +44,77 @@ function QuickHelp:buildCEGUIWidget()
 	self.widget:hide()
 end
 
+-- Shows the QuickHelp widget immediately, no transitions
+function QuickHelp:show()
+	self.frameWindow:fireEvent("InterruptTransitions", CEGUI.WindowEventArgs:new(self.frameWindow))
+	self.showTransitionStarted = false
+	self.hideTransitionStarted = false
+	
+	self.widget:show()
+	-- ensure a "valid" alpha is set
+	self.frameWindow:setAlpha(1.0)
+end
+
+function QuickHelp:showWithTransition()
+	self:show()
+	
+	self.showTransitionStarted = true
+	self.frameWindow:fireEvent("StartShowTransition", CEGUI.WindowEventArgs:new(self.frameWindow))
+end
+
+-- Hides the QuickHelp widget immediately, no transitions
+function QuickHelp:hide()
+	self.frameWindow:fireEvent("InterruptTransitions", CEGUI.WindowEventArgs:new(self.frameWindow))
+	self.showTransitionStarted = false
+	self.hideTransitionStarted = false
+	
+	self.timer:setText("")
+	self.timeBlurbShown = 0
+	self.widget:hide()
+	self:disableAlphaConnector()
+end
+
+function QuickHelp:hideWithTransition()
+	-- make sure we store that we already sent the event, otherwise a never ending event firing starts
+	self.hideTransitionStarted = true
+	-- by firing this event, we allow looknfeel of the QuickHelp window to act accordingly
+	-- (at the time of writing this, it fades the FrameWindow out over 5 seconds)
+	self.frameWindow:fireEvent("StartHideTransition", CEGUI.WindowEventArgs:new(self.frameWindow))
+end
+
+function QuickHelp:hideWithSlowTransition()
+	-- make sure we store that we already sent the event, otherwise a never ending event firing starts
+	self.hideTransitionStarted = true
+	-- by firing this event, we allow looknfeel of the QuickHelp window to act accordingly
+	-- (at the time of writing this, it fades the FrameWindow out over 5 seconds)
+	self.frameWindow:fireEvent("StartSlowHideTransition", CEGUI.WindowEventArgs:new(self.frameWindow))
+end
+
 function QuickHelp:frameStarted(timeSinceLastUpdate)
 	if self.widget:isActive() then 
 		self.timer:setText("")
 		self:disableAlphaConnector()
-		self.frameWindow:setAlpha(1.0)
 		
-		-- we haven't yet sent the "HideRequest" event to the CEGUI widget
-		self.hideRequestSent = false
+		-- ensure the widget is shown at this point no matter what
+		self:show()
 	else
 		self.timeBlurbShown = timeSinceLastUpdate + self.timeBlurbShown
 		local timeLeft = math.floor(math.max(self.timeToShowBlurb-self.timeBlurbShown, 0))
+		
 		--only update the text when it has changed
 		if self.timeBlurbLastUpdate ~= timeLeft then
 			self.timer:setText("Hiding in " .. timeLeft .. " seconds.")
 			self.timeBlurbLastUpdate = timeLeft
 		end
+		
 		if self.timeBlurbShown > self.timeToShowBlurb then
-			if not self.hideRequestSent then
-				-- make sure we store that we already sent the event, otherwise a never ending event firing starts
-				self.hideRequestSent = true
-				-- by firing this event, we allow looknfeel of the QuickHelp window to act accordingly
-				-- (at the time of writing this, it fades the FrameWindow out over 5 seconds)
-				self.frameWindow:fireEvent("HideRequest", CEGUI.WindowEventArgs:new(self.frameWindow))
+			if not self.hideTransitionStarted then
+				self:hideWithSlowTransition()
 			end
 			
-			if self.timeBlurbShown > (self.timeToShowBlurb+self.timeToFade) then
-				self.timer:setText("")
-				self.timeBlurbShown = 0
-				self.widget:hide()
-				self:disableAlphaConnector()
+			if self.timeBlurbShown > (self.timeToShowBlurb + self.timeToFade) then
+				-- ensure that it's hidden no matter what at this point
+				self:hide()
 			end
 		end
 	end
@@ -79,29 +123,33 @@ end
 function QuickHelp:toggleVisibility()
 	self.hidden = false
 	self.widget:show()
-	self.frameWindow:setAlpha(1.0)
 end
 
 function QuickHelp:updateText(helpMessage)
 	local text = helpMessage:getHelp()
 	if not self.hidden then
+		--Adapt the time the help widget is shown to how many words there are in the text
+		local words = 0
+		for word in string.gfind(text, "[^%s]+") do words=words+1 end
+		--a normal human reads about 250 words per minute, which is ~4 words per second. We'll go for two words per second
+		--as the user might not concentrate on the help message at first. Show it no lesser than four seconds 
+		self.timeToShowBlurb = math.max(words / 2.0, 4)
+		self.timeBlurbLastUpdate = -1
+		self.timeBlurbShown = 0
+		
 		if not self.widget:isVisible() then
 			if self.updateAlpha_connector then
 				self.updateAlpha_connector:disconnect()
 			end
 			self.updateAlpha_connector = createConnector(self.widget.EventFrameStarted):connect(self.frameStarted, self)
-			self.timeBlurbShown = 0
 			
-			--Adapt the time the help widget is shown to how many words there are in the text
-			local words = 0
-			for word in string.gfind(text, "[^%s]+") do words=words+1 end
-			self.timeToShowBlurb = math.max(words / 2.0, 4) --a normal human reads about 250 words per minute, which is ~4 words per second. We'll go for two words per second as the user might not concentrate on the help message at first. Show it no lesser than four seconds 
-			self.timeBlurbLastUpdate = -1
+			self:showWithTransition()
 		end
-		
-		self.widget:show()
-		self.frameWindow:setAlpha(1.0)
+		if self.hideTransitionStarted then
+			self:showWithTransition()
+		end
 	end
+	
 	self.textWindow:setText(text)
 	self.widget:getMainWindow():setText("Help - " .. helpMessage:getTitle())
 	self.messagePosition:setText(self.helper:getCursorLocation() .. "/" .. self.helper:getSize())
@@ -125,7 +173,14 @@ end
 
 function QuickHelp:Hide_Click(args)
 	self.hidden = true
-	self.widget:hide()
+	self:hideWithTransition()
+	
+	return true
+end
+
+function QuickHelp:CloseClicked(args)
+	self:hideWithTransition()
+	
 	return true
 end
 
