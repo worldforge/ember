6556cdaacb41a9e9e3dbf22b675c5b022fabee62
Martin Preisler <preisler.m@gmail.com>
2011-07-05 20:18:07 +0200

    LayoutHelper to aid in loading representations' layouts
    (mostly for handling prefixes automatically)
    I was not sure where to put this, it's location is not firmly set, first I
    thought it has to go into RepresentationBase or some other non-templated base
    class of it. However not all representations have to load layouts per-se, so
    I kept it separate for now.

---
+++ src/components/ogre/widgets/representations/LayoutHelper.cpp
@@ -0,0 +1,59 @@
+//
+// C++ Implementation: LayoutHelper
+//
+// Description: 
+//
+//
+// Author: Martin Preisler <preisler.m@gmail.com>, (C) 2011
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "LayoutHelper.h"
+#include "components/ogre/GUIManager.h"
+#include <CEGUIWindowManager.h>
+#include <sstream>
+
+namespace Ember {
+namespace OgreView {
+
+namespace Gui {
+
+namespace Representations {
+
+unsigned int LayoutHelper::mLastPrefixBase = 0;
+
+CEGUI::Window* LayoutHelper::loadLayout(const std::string& layoutFile, CEGUI::String& prefix)
+{
+	CEGUI::WindowManager& windowManager = CEGUI::WindowManager::getSingleton();
+
+	std::string finalFileName(GUIManager::getSingleton().getLayoutDir() + layoutFile);
+	std::stringstream ss;
+	ss << "Representation" << ++mLastPrefixBase;
+	prefix = ss.str();
+	
+	return windowManager.loadWindowLayout(finalFileName, prefix);
+}
+
+}
+    
+}
+
+}
+
+}
---
+++ src/components/ogre/widgets/representations/LayoutHelper.h
@@ -0,0 +1,62 @@
+//
+// C++ Interface: ComboboxRepresentation
+//
+// Description: 
+//
+// Author: Martin Preisler <preisler.m@gmail.com>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.//
+//
+#ifndef EMBEROGRE_GUI_REPRESENTATIONS_LAYOUTHELPER_H
+#define EMBEROGRE_GUI_REPRESENTATIONS_LAYOUTHELPER_H
+
+#include <string>
+
+namespace CEGUI
+{
+	class String;
+	class Window;
+}
+
+namespace Ember {
+namespace OgreView {
+
+namespace Gui {
+
+namespace Representations {
+
+/**
+ * @brief class that takes care of layout loading and all the fuss associated with it (prefixes mostly)
+ * 
+ * @todo I am not sure I like the way this is done, perhaps I will move this to Representation base class
+ */
+class LayoutHelper
+{
+public:
+	static CEGUI::Window* loadLayout(const std::string& layout, CEGUI::String& prefix);
+	
+private:
+	static unsigned int mLastPrefixBase;
+};
+
+}
+
+}
+
+}
+
+}
+
+#endif
