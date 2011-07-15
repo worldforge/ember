b2bbc8d4a6e064093702bf6948e34f0066c03667
Martin Preisler <preisler.m@gmail.com>
2011-07-15 13:37:37 +0200

    Tabs for categories in the settings

--- src/components/ogre/widgets/Settings.lua
+++ src/components/ogre/widgets/Settings.lua
@@ -6,6 +6,38 @@ function SettingsWidget:buildWidget()
 	
 	self.window = self.widget:getWindow("MainWindow")
 	self.window:subscribeEvent("CloseClicked", self.CloseClicked, self)
+	
+	self.tabs = self.widget:getWindow("MainWindow/Tabs")
+	
+	self:buildSettingsUi()
+end
+
+function SettingsWidget:buildSettingsUi()
+	--This is a place where the settings contents are declared and (later) used to construct the GUI
+	--to manipulace them
+	
+	local settings =
+	{
+		Audio =
+		{
+		},
+		Graphics =
+		{
+		}
+	}
+	
+	for category, representations in pairs(settings) do
+		local wnd = self:buildUiFor(representations)
+		
+		wnd:setText(category)
+		self.tabs:addChildWindow(wnd)
+	end
+end
+
+function SettingsWidget:buildUiFor(representations)
+	local ret = CEGUI.WindowManager:getSingleton():createWindow("DefaultWindow")
+	
+	return ret
 end
 
 function SettingsWidget:CloseClicked(args)
