261e8e856483e18ce3c3a17f7f846e2c74655bb3
Peter Szücs <peter.szucs.dev@gmail.com>
2011-08-01 17:06:04 +0200

    Fix tolua generator for mac.

--- scripts/update_lua_bindings.sh
+++ scripts/update_lua_bindings.sh
@@ -10,4 +10,4 @@ if [ x${TOLUAXX} = x ]; then
 fi
 ${TOLUAXX} -n $1 $2 > $3
 grep -q '** tolua internal error' $3 && cat $3 && exit 1
-sed -i -e 's/const,/const /g' -e 's/tolua_outside//g' -e 's/tolua++\.h/components\/lua\/tolua++\.h/' $3
+sed -i "" -e 's/const,/const /g' -e 's/tolua_outside//g' -e 's/tolua++\.h/components\/lua\/tolua++\.h/' $3
