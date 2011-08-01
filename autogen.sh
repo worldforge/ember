56784d511ccb2cd0d8c43e3c52e29de4dc067e04
Peter Szücs <peter.szucs.dev@gmail.com>
2011-08-01 17:02:40 +0200

    Fix autogen for mac.

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,15 @@ if test -d /usr/local/share/aclocal ; then
 	ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I /usr/local/share/aclocal"
 fi
 aclocal $ACLOCAL_FLAGS
-libtoolize --force --copy
+
+if [[ $OSTYPE == *darwin* ]] ; then
+LIBTOOLIZE="glibtoolize"
+else
+LIBTOOLIZE="libtoolize"
+fi
+
+$LIBTOOLIZE --force --copy
+
 autoheader
 automake --gnu --add-missing --copy
 autoconf
