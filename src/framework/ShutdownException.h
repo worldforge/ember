179568e3e89f86fcc915b5bc7ce14ed20849505f
Peter Szucs <peter.szucs.dev@gmail.com>
2011-11-05 20:46:47 +0100

    Document ShutdownException.

--- src/framework/ShutdownException.h
+++ src/framework/ShutdownException.h
@@ -1,14 +1,26 @@
 #include <string>
 #include <ostream>
 
-//Exception, which can be thrown ONLY at game loading to abort.
-//This is meant to NOT inherit from std::exception!
+/**
+ * @brief Exception, which can be thrown ONLY at game loading to abort.
+ * 
+ * This is meant to NOT inherit from std::exception!
+ */
 class ShutdownException {
 public:
+	/**
+	 * @brief Ctor
+	 * @param reason The reason for the exception. You can also call it error message.
+	 */
 	ShutdownException(const std::string& reason) :
 		mReason(reason)
 	{
 	}
+	
+	/**
+	 * @brief Allows to read the reason for the exception. 
+	 * @return Returns the reason passed to the Ctor as string.
+	 */
 	std::string getReason()
 	{
 		return mReason;
