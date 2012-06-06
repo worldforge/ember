08ee7bc0f93a5e5976d5103e02dfe2fd98330092
Arjun Kumar <arjun1991@gmail.com>
2012-06-07 01:04:56 +0530

    Add functionality to the foliage level manager.
    - Add map defaultDensityStore that maps plant types to densities.
    - Add function findDefaultDensity to find the default density of a plant type as retrieved from the terrain file.
    - Add function changeFoliageDensity to change the density of a particular plant type.

--- src/components/ogre/FoliageLevelManager.h
+++ src/components/ogre/FoliageLevelManager.h
@@ -1,8 +1,13 @@
 #include <sigc++/signal.h>
 
+#include <string>
+#include <map>
+
 namespace Ember {
 namespace OgreView {
 
+class AutomaticGraphicsLevelManager;	
+	
 /**
  * @brief This class manages the detail level of foliage by responding to the changeRequired signal from IGraphicalManager.
  * Acts as a sub-component of the automatic handling of graphics system.
@@ -13,7 +18,7 @@ public:
 	/**
 	 * @brief Constructor.
 	 */
-	FoliageLevelManager();
+	FoliageLevelManager(AutomaticGraphicsLevelManager &automaticGraphicsLevelManager);
 	
 	/**
 	 * @brief Destructor.
@@ -25,17 +30,40 @@ public:
 	 */
 	sigc::signal <void,int> foliageLevelChanged;
 	
+	
+	/**
+	 * @brief Used to change the density of a particular type of foliage.
+	 * @param foliage The plant type, eg. grass, grass_green etc.
+	 * @param density The new density for the plant type.
+	 * @return True if foliage change was possible.
+	 */
+	bool changeFoliageDensity(String foliage, float density);
+
+protected:
+	
 	/**
 	 * @brief Changes the detail level of foliage if the asked level is above threshold level of this class.
 	 * @return True if a change is possible and is made.
 	 */
 	bool changeLevel(float level);
-
-protected:
+	
+	/**
+	 * @brief Used to find the default density of a particular plant type.
+	 * @return The default density of plantType passed to this function.
+	 * Uses the map defaultDensityStore to find the matching density of the plantType.
+	 */
+	float findDefaultDensity(std::string plantType);
+	
 	/**
 	 * The threshold level of this subcomponent passing which it responds by changing the detail level of foliage.
 	 */
 	float thresholdLevel;
+	
+	/**
+	 * Holds a map of the original density levels of each plant type as retrieved by the TerrainLayerDefinitionManager.
+	 */
+	std::map <std::string,float> defaultDensityStore;
+	
 	
 };
 	
