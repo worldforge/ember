dbeeca5700622028e7891562965717241530043b
Arjun Kumar <arjun1991@gmail.com>
2012-06-07 07:05:17 +0530

    Add functions to edit foliage density.
    Add various functions to edit the density of a foliage by:
    - Stepping it up by a step.
    - Stepping it down by a step.
    - Setting it to an absolute float value
    
    updateFoliageDensity can be called to reload the foliage with the new densities.

--- src/components/ogre/FoliageLevelManager.h
+++ src/components/ogre/FoliageLevelManager.h
@@ -37,9 +37,25 @@ public:
 	 * @param density The new density for the plant type.
 	 * @return True if foliage change was possible.
 	 */
-	bool changeFoliageDensity(String foliage, float density);
+	bool changeFoliageDensity(std::string foliage, float density);
 
 protected:
+	
+	/**
+	 * @brief Steps the density of the particular foliage up by value passed.
+	 * @param foliage The plant type, eg. grass, grass_green etc.
+	 * @param step The amount by which the density of the plant type is stepped up.
+	 * @returns True if step up was possible.
+	 */
+	bool stepUpFoliageDensity(std::string foliage, float step);
+	
+	/**
+	 * @brief Steps the density of the particular foliage down by value passed.
+	 * @param foliage The plant type, eg. grass, grass_green etc.
+	 * @param step The amount by which the density of the plant type is stepped down.
+	 * @returns True if step down was possible.
+	 */
+	bool stepDownFoliageDensity(std::string foliage, float step);
 	
 	/**
 	 * @brief Changes the detail level of foliage if the asked level is above threshold level of this class.
@@ -55,14 +71,30 @@ protected:
 	float findDefaultDensity(std::string plantType);
 	
 	/**
+	 * @brief Updates foliage densities.
+	 * Changes the foliage densities of the main foliage definitions at TerrainLayerDefinitionManager and then reloads the foliage.
+	 */
+	void updateFoliageDensity();
+	
+	/**
 	 * The threshold level of this subcomponent passing which it responds by changing the detail level of foliage.
 	 */
-	float thresholdLevel;
-	
+	float mThresholdLevel;
+
 	/**
 	 * Holds a map of the original density levels of each plant type as retrieved by the TerrainLayerDefinitionManager.
 	 */
-	std::map <std::string,float> defaultDensityStore;
+	std::map <std::string,float> mDefaultDensityStore;
+	
+	/**
+	 * The value by which density of a foliage is stepped up or down while adjusting foliage detail.
+	 */
+	float mDefaultDensityStep;
+	
+	/**
+	 * Holds an updated map of plant types and their density levels,
+	 */
+	std::map <std::string,float> mUpdatedtDensityStore;
 	
 	
 };
