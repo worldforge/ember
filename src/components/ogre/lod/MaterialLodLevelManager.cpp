ce5d527b198db4ac87a2b6463578945a287a033d
Arjun Kumar <arjun1991@gmail.com>
2012-07-04 08:29:11 +0530

    Add implementation for MaterialLodLevelManager.
    - Implemented only for the distance strategy.
    - Have set minimum lod factor  to 0.2f for now to avoid setting a distance to 0.

---
+++ src/components/ogre/lod/MaterialLodLevelManager.cpp
@@ -0,0 +1,106 @@
+#include "MaterialLodLevelManager.h"
+
+#include "components/ogre/AutoGraphicsLevelManager.h"
+
+#include "OgreMaterialManager.h"
+#include "OgreLodStrategy.h"
+#include "OgreMath.h"
+
+#include "sigc++/signal.h"
+
+namespace Ember
+{
+namespace OgreView
+{
+namespace Lod
+{
+
+MaterialLodLevelManager::MaterialLodLevelManager(AutomaticGraphicsLevelManager& automaticGraphicsLevelManager) :
+		mThresholdLevel(1.0f), mLodFactor(1.0f), mDefaultStep(0.3f), mMinLodFactor(0.2f), mMaxLodFactor(2.0f)
+{
+	automaticGraphicsLevelManager.getGraphicalAdapter().changeRequired.connect(&MaterialLodLevelManager::changeLevel);
+}
+
+MaterialLodLevelManager::~MaterialLodLevelManager()
+{
+
+}
+
+bool MaterialLodLevelManager::setLodBiasAll(float factor)
+{
+	//additional check if factor is set from outside
+	if (factor > mMaxLodFactor || factor < mMinLodFactor) {
+		return false;
+	}
+
+	Ogre::ResourceManager::ResourceMapIterator resources = Ogre::MaterialManager::getSingleton().getResourceIterator();
+	while (resources.hasMoreElements()) {
+		resources.moveNext();
+		Ogre::MaterialPtr material = static_cast<Ogre::MaterialPtr>(resources.peekNextValue());
+
+		//implementation for distance strategy
+		if (material->getLodStrategy()->getName() == "Distance") {
+			Ogre::Material::LodValueList lodValues;
+			Ogre::Material::LodValueIterator I = material->getLodValueIterator();
+
+			// Ignoring the first 0 lod value that getLodValueIterator returns since setLodLevels needs values from level 1 onwards.
+			I.moveNext();
+
+			while (I.hasMoreElements()) {
+				I.moveNext();
+
+				//The lod values are stored as squares of user provided values, but expected in unsquared values.
+				lodValues.push_back(Ogre::Math::Sqrt(I.peekNext() * factor));
+			}
+			material->setLodLevels(lodValues);
+		} else {
+			//TODO: Implementation for Pixel Count lod strategy.
+		}
+	}
+	return true;
+}
+
+bool MaterialLodLevelManager::changeLevel(float level)
+{
+	if (std::abs(level) < mThresholdLevel) {
+		return false;
+	} else {
+		bool changeMade = false;
+		if (level > 1.0f) {
+			changeMade |= stepDownLodBias(mDefaultStep);
+		} else {
+			changeMade |= stepUpLodBias(mDefaultStep);
+		}
+		return changeMade;
+	}
+}
+
+bool MaterialLodLevelManager::stepDownLodBias(float step)
+{
+	if (mLodFactor > step) { //step down only if existing lod is greater than step
+		mLodFactor -= step;
+		return setLodBiasAll(mLodFactor);
+	} else if (mLodFactor < step && mLodFactor > mMinLodFactor) { //if there is still some positive lod left which is smaller than step, set it to the minimum lod
+		mLodFactor = mMinLodFactor;
+		return setLodBiasAll(mLodFactor);
+	} else { //step down not possible
+		return false;
+	}
+}
+
+bool MaterialLodLevelManager::stepUpLodBias(float step)
+{
+	if (mLodFactor + step <= mMaxLodFactor) { //step up only if the step doesn't cause lod to go over maximum lod.
+		mLodFactor += step;
+		return setLodBiasAll(mLodFactor);
+	} else if (mLodFactor < mMaxLodFactor) { //if the lod is still below maximum lod but a default step causes it to go over it.
+		mLodFactor = mMaxLodFactor;
+		return setLodBiasAll(mLodFactor);
+	} else {
+		return false; //step up not possible
+	}
+}
+
+}
+}
+}
--- src/components/ogre/lod/MaterialLodLevelManager.h
+++ src/components/ogre/lod/MaterialLodLevelManager.h
@@ -2,6 +2,8 @@ namespace Ember
 {
 namespace OgreView
 {
+class AutomaticGraphicsLevelManager;
+
 namespace Lod
 {
 
@@ -11,7 +13,7 @@ public:
 	/**
 	 * @brief Constructor.
 	 */
-	MaterialLodLevelManager();
+	MaterialLodLevelManager(AutomaticGraphicsLevelManager& automaticGraphicsLevelManager);
 
 	/**
 	 * @brief Destructor.
@@ -20,21 +22,11 @@ public:
 
 	/**
 	 * @brief Sets the lod bias factor.
-	 * Using this function it is possible to influence the lod behavior of all materials. It shifts the lod behavior of all materials
-	 * currently loaded up or down.
-	 * @param factor Proportional factor to apply to the distance at which LOD is changed. Higher values increase the distance at which 
-	 * higher LODs are displayed (eg. 2.0 is twice the normal distance, 0.5 is half).
+	 * Using this function it is possible to influence the lod behavior of all materials. It shifts the lod behavior 
+	 * of all materials currently loaded up or down.
+	 * @param factor Proportional factor to apply to the distance at which LOD is changed. Higher values increase the distance at which higher LODs are displayed (eg. 2.0 is twice the normal distance, 0.5 is half).
 	 */
 	bool setLodBiasAll(float factor);
-
-	/**
-	 * @brief Sets the lod bias factor for passed material categories.
-	 * @see setLodBiasAll
-	 * @param materialCategories List of categories the materials belong to.
-	 * @param factor Proportional factor to apply to the distance at which LOD is changed.
-	 * @note Current categories include: characters, creatures, environment, items, plants, primitives, structures.
-	 */
-	bool setLodBias(std::list<std::string> materialCategories, float factor);
 
 protected:
 	/**
@@ -42,7 +34,45 @@ protected:
 	 * This function is used to listen to the changes required by the automatic graphics detail system.
 	 */
 	bool changeLevel(float level);
-
+	
+	/**
+	 * @brief Steps up the material lod bias by provided step.
+	 * @param step The value to step the bias up by.
+	 */
+	bool stepUpLodBias(float step);
+	
+	/**
+	 * @brief Steps down the material lod bias by provided step.
+	 * @param step The value to step the bias down by.
+	 */
+	bool stepDownLodBias(float step);
+	
+	/**
+	 * The threshold level after which this component starts responding to change in detail required signals.
+	 */
+	float mThresholdLevel;
+	
+	/**
+	 * The current global material lod bias. This value is used to affect the level of detail distance values 
+	 * of all materials.
+	 */
+	float mLodFactor;
+	
+	/**
+	 * The minimum material lod bias that the lod bias factor can be set to.
+	 */
+	float mMinLodFactor;
+	
+	/**
+	 * The maximum material lod bias that the lod bias factor can be set to.
+	 */
+	float mMaxLodFactor;
+	
+	/**
+	 * The default step that is taken when adjusting the factors of this component.
+	 */
+	float mDefaultStep;
+	
 };
 
 }
