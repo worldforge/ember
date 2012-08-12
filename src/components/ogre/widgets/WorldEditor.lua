5d218ab17c63449b8af769af3cc4c03ed955a554
Erik Ogenvik <erik@ogenvik.org>
2012-08-12 23:16:16 +0200

    Added a world editor.
    Through this one can dump and load the world.

---
+++ src/components/ogre/widgets/WorldEditor.lua
@@ -0,0 +1,55 @@
+WorldEditor = {}
+
+function WorldEditor:buildWidget()
+
+	self.widget = guiManager:createWidget()
+
+	local setup = function()
+		
+		self.widget:getWindow("DumpWorld"):subscribeEvent("Clicked", function(args)
+			local worldDumper = Ember.WorldDumper:new(emberServices:getServerService():getAccount())
+			self.widget:getWindow("DumpStatus"):setText("Dumping...")
+			createConnector(worldDumper.EventCompleted):connect(function()
+				self.widget:getWindow("DumpStatus"):setText("Done dumping.")
+				worldDumper:delete()			
+			end)
+			worldDumper:start(emberServices:getConfigService():getHomeDirectory() .. "/world.xml")
+			return true
+		end)
+
+		self.widget:getWindow("LoadWorld"):subscribeEvent("Clicked", function(args)
+--			local worldDumper = Ember.WorldDumper:new(emberServices:getServerService():getAccount())
+--			worldDumper:start(emberServices:getConfigService():getHomeDirectory .. "/world.xml")
+			return true
+		end)
+		
+			
+		self.widget:enableCloseButton()
+	end
+
+	connect(self.connectors, self.widget.EventFirstTimeShown, setup)
+	self.widget:loadMainSheet("WorldEditor.layout", "WorldEditor/")
+	self.widget:registerConsoleVisibilityToggleCommand("worldEditor")
+
+end
+
+
+function WorldEditor:shutdown()
+	disconnectAll(self.connectors)
+	guiManager:destroyWidget(self.widget)
+end
+
+WorldEditor.createdWorldConnector = createConnector(emberOgre.EventWorldCreated):connect(function(world)
+		worldEditor = {connectors={}, world=world}
+		setmetatable(worldEditor, {__index = WorldEditor})
+		
+		worldEditor:buildWidget()
+		connect(worldEditor.connectors, emberOgre.EventWorldDestroyed, function()
+				worldEditor:shutdown()
+				worldEditor = nil
+			end
+		)
+	end
+)
+
+
