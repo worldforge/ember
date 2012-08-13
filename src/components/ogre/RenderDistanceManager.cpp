f4f6d23a57a984d6894eca4c045ef7911fa18d99
Arjun Kumar <arjun1991@gmail.com>
2012-08-14 04:14:06 +0530

    Use better better parameters for changing render far distance.
    - Change minimum far distance to 0.7 * original distance.
    
    - Remove adding a fog effect to hide clipped areas since it made the missing effect
    worse.

--- src/components/ogre/RenderDistanceManager.cpp
+++ src/components/ogre/RenderDistanceManager.cpp
@@ -12,7 +12,7 @@ namespace OgreView
 {
 
 RenderDistanceManager::RenderDistanceManager(IGraphicalChangeAdapter& iGraphicalChangeAdapter, Environment::IFog& fog, Ogre::Camera& mainCamera) :
-		mIGraphicalChangeAdapter(iGraphicalChangeAdapter), mFog(fog), mMainCamera(mainCamera), mDefaultFarRenderDistance(1000), mFarRenderDistance(1000), mMaxFarRenderDistanceFactor(1.5f), mMinFarRenderDistanceFactor(0.5f), mRenderDistanceThreshold(5.0f), mFarRenderDistanceFactor(1.0f), mDefaultRenderDistanceStep(0.3f)
+		mIGraphicalChangeAdapter(iGraphicalChangeAdapter), mFog(fog), mMainCamera(mainCamera), mDefaultFarRenderDistance(1000), mFarRenderDistance(1000), mMaxFarRenderDistanceFactor(1.5f), mMinFarRenderDistanceFactor(0.7f), mRenderDistanceThreshold(5.0f), mFarRenderDistanceFactor(1.0f), mDefaultRenderDistanceStep(0.3f)
 {
 }
 
@@ -40,7 +40,11 @@ bool RenderDistanceManager::setCompensatedFarRenderDistance(float factor)
 {
 	mFarRenderDistance = factor * mDefaultFarRenderDistance;
 	mMainCamera.setFarClipDistance(mFarRenderDistance);
-	mFog.setDensity(factor);
+	float fogDensity = mMaxFarRenderDistanceFactor - factor;
+	if (factor > 0.3) {
+		fogDensity = 0.3;
+	}
+	mFog.setDensity(fogDensity);
 }
 
 bool RenderDistanceManager::changeLevel(float level)
@@ -63,11 +67,11 @@ bool RenderDistanceManager::stepDownFarRenderDistance(float step)
 	bool stepPossible = false;
 	if (mFarRenderDistanceFactor > step) { //step down only if existing render distance factor is greater than step
 		mFarRenderDistanceFactor -= step;
-		setCompensatedFarRenderDistance(mFarRenderDistanceFactor);
+		setFarRenderDistance(mFarRenderDistanceFactor);
 		stepPossible = true;
 	} else if (mFarRenderDistanceFactor < step && mFarRenderDistanceFactor > mMinFarRenderDistanceFactor) { //if there is still some positive distance left which is smaller than step, set it to the minimum distance
 		mFarRenderDistanceFactor = mMinFarRenderDistanceFactor;
-		setCompensatedFarRenderDistance(mFarRenderDistanceFactor);
+		setFarRenderDistance(mFarRenderDistanceFactor);
 		stepPossible = true;
 	} else { //step down not possible
 		stepPossible = false;
@@ -80,11 +84,11 @@ bool RenderDistanceManager::stepUpFarRenderDistance(float step)
 	bool stepPossible = false;
 	if (mFarRenderDistanceFactor + step <= mMaxFarRenderDistanceFactor) { //step up only if the step doesn't cause render far distance to go over maximum render far distance.
 		mFarRenderDistanceFactor += step;
-		setCompensatedFarRenderDistance(mFarRenderDistanceFactor);
+		setFarRenderDistance(mFarRenderDistanceFactor);
 		stepPossible = true;
 	} else if (mFarRenderDistanceFactor < mMaxFarRenderDistanceFactor) { //if the render far distance is still below maximum render far distance but a default step causes it to go over it.
 		mFarRenderDistanceFactor = mMaxFarRenderDistanceFactor;
-		setCompensatedFarRenderDistance(mFarRenderDistanceFactor);
+		setFarRenderDistance(mFarRenderDistanceFactor);
 		stepPossible = true;
 	} else {
 		stepPossible = false; //step up not possible
