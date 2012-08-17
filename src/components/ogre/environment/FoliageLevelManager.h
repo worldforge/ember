51309e5b7032b3d71df82f75bf09f07acb414f15
Arjun Kumar <arjun1991@gmail.com>
2012-08-17 06:08:22 +0530

    Add license information and remove useless headers.

--- src/components/ogre/environment/FoliageLevelManager.h
+++ src/components/ogre/environment/FoliageLevelManager.h
@@ -1,8 +1,25 @@
+/*
+ * Copyright (C) 2012 Arjun Kumar <arjun1991@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include <sigc++/signal.h>
 #include <sigc++/connection.h>
 
 #include <string>
-#include <map>
 
 namespace varconf
 {
