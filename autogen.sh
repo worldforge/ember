90100fe539c922b606372a7ade50313eff6f12be
Erik Ogenvik <erik@ogenvik.org>
2013-01-01 14:23:01 +0100

    Prefer 'autoreconf' if available.

--- autogen.sh
+++ autogen.sh
@@ -2,19 +2,24 @@
 
 rm -f config.cache
 
-if test -d /usr/local/share/aclocal ; then
-	ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I /usr/local/share/aclocal"
-fi
-aclocal -I m4 $ACLOCAL_FLAGS
-
-if [ "`echo $OSTYPE | grep darwin`" != "" ] ; then
-LIBTOOLIZE="glibtoolize"
+#Check if the autoreconf command is available, and use that if so.
+if command -v autoreconf >/dev/null 2>&1 ; then
+  autoreconf --install
 else
-LIBTOOLIZE="libtoolize"
+  if test -d /usr/local/share/aclocal ; then
+	  ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I /usr/local/share/aclocal"
+  fi
+  aclocal -I m4 $ACLOCAL_FLAGS
+
+  if [ "`echo $OSTYPE | grep darwin`" != "" ] ; then
+  LIBTOOLIZE="glibtoolize"
+  else
+  LIBTOOLIZE="libtoolize"
+  fi
+
+  $LIBTOOLIZE --force --copy
+
+  autoheader
+  automake --gnu --add-missing --copy
+  autoconf
 fi
-
-$LIBTOOLIZE --force --copy
-
-autoheader
-automake --gnu --add-missing --copy
-autoconf
