4bc074a3c8ddcf2aa6010b34fa3f541b521c0bf5
Erik Ogenvik <erik@ogenvik.org>
2013-01-14 21:19:08 +0100

    Center the window more correctly on win32.

--- src/components/ogre/OgreSetup.cpp
+++ src/components/ogre/OgreSetup.cpp
@@ -299,11 +299,7 @@ Ogre::Root* OgreSetup::configure(void)
 	}
 #endif
 
-	//If there's already a window created (i.e. from the configuration) we need to center it, else it will look strange.
-	//If there is no window yet we should instead rely on the WM to handle positioning.
-	bool centerWindow = Input::getSingleton().hasWindow();
-
-	std::string windowId = Input::getSingleton().createWindow(width, height, fullscreen, true, centerWindow, handleOpenGL);
+	std::string windowId = Input::getSingleton().createWindow(width, height, fullscreen, true, true, handleOpenGL);
 
 	mRoot->initialise(false, "Ember");
 	Ogre::NameValuePairList misc;
