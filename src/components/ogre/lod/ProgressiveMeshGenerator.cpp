0c98777619d601868df3fabb0f0f3869d99fa9a2
Peter Szücs <peter.szucs.dev@gmail.com>
2013-01-23 14:25:07 +0100

    Relicense Automatic Mesh Lod System to MIT.

--- src/components/ogre/lod/ProgressiveMeshGenerator.cpp
+++ src/components/ogre/lod/ProgressiveMeshGenerator.cpp
@@ -1,23 +1,4 @@
-/*
- * Copyright (C) 2012 Peter Szucs <peter.szucs.dev@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
-// The collapse cost calculation in this file is based heavily on Ogre's implementation:
-/*
+﻿/*
  * -----------------------------------------------------------------------------
  * This source file is part of OGRE
  * (Object-oriented Graphics Rendering Engine)
