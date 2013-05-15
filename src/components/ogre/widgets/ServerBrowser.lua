ebecde8643df678a8aec95affa306709ae9acf30
Erik Ogenvik <erik@ogenvik.org>
2013-05-15 21:55:58 +0200

    Detect if a local server is running.
    And present the user with choices for logging in as a creator or to
    connect as normal or to not connect at all.

--- src/components/ogre/widgets/ServerBrowser.lua
+++ src/components/ogre/widgets/ServerBrowser.lua
@@ -2,36 +2,36 @@ ServerBrowser = {}
 
 function ServerBrowser:connectToMetaServer()
 	local metaServer = emberServices:getMetaserverService():getMetaServer()
-	
+
 	connect(self.connectors, metaServer.ReceivedServerInfo, self.MetaServer_ReceivedServerInfo, self)
 	--only refresh if it's enabled in the config
 	if emberServices:getConfigService():isItemSet("metaserver", "enabled", "true") then
-    	metaServer:refresh()
-    end
+		metaServer:refresh()
+	end
 end
 
 
 function ServerBrowser:buildWidget()
 	self.widget = guiManager:createWidget()
 	self.widget:loadMainSheet("ServerBrowser.layout", "ServerBrowser/")
-	
+
 	local wee
 	wee = self.widget:getWindow("ServerList")
 	self.serverList = CEGUI.toMultiColumnList(wee)
 	self.serverList:subscribeEvent("MouseDoubleClick", self.ServerList_DoubleClick, self)
-	
+
 	wee = self.widget:getWindow("Refresh")
 	local refreshButton = CEGUI.toPushButton(wee)
 	refreshButton:subscribeEvent("Clicked", self.Refresh_Click, self)
-	
+
 	wee = self.widget:getWindow("Connect")
 	local connectButton = CEGUI.toPushButton(wee)
 	connectButton:subscribeEvent("Clicked", self.Connect_Click, self)
-		
+
 	wee = self.widget:getWindow("ManualServerName")
 	self.manualServerNameTextbox = CEGUI.toPushButton(wee)
 	self.manualServerNameTextbox:subscribeEvent("TextAccepted", self.manualServerNameTextbox_TextAcceptedEvent, self)
-	
+
 	wee = self.widget:getWindow("HideOldServers")
 	self.hideOldServersCheckbox = CEGUI.toCheckbox(wee)
 	--Only show the checkbox for filtering old servers if the metaserver:minimumversion value is set in the config
@@ -54,33 +54,76 @@ function ServerBrowser:buildWidget()
 			end
 		end
 	end
-		
-	
-	
+
+
+
 	local serverService = emberServices:getServerService()
 	connect(self.connectors, serverService.GotConnection, self.Server_GotConnection, self)
-	
+
 	connect(self.connectors, serverService.EventStatusChanged, self.Server_StatusChanged, self)
 
 	self.widget:getWindow("CancelConnection"):subscribeEvent("Clicked", self.CancelConnection_Click, self)
-	
+
 	self:connectToMetaServer()
-	self.widget:show()
-	self.widget:getMainWindow():activate()
-	
-	--If the "autoconnect" value is set, try to connect to the specified server
-	if emberServices:getConfigService():itemExists("metaserver", "autoconnect") then
-		local serverConfAddress = emberServices:getConfigService():getValue("metaserver", "autoconnect")
-		if serverConfAddress ~= nil then
-			if serverConfAddress:is_string() then
-				local address = serverConfAddress:as_string()
-				Ember.EmberServices:getSingleton():getServerService():connect(address)
+	local showWindow = function()
+		self.widget:show()
+		self.widget:getMainWindow():activate()
+
+		--If the "autoconnect" value is set, try to connect to the specified server
+		if emberServices:getConfigService():itemExists("metaserver", "autoconnect") then
+			local serverConfAddress = emberServices:getConfigService():getValue("metaserver", "autoconnect")
+			if serverConfAddress ~= nil then
+				if serverConfAddress:is_string() then
+					local address = serverConfAddress:as_string()
+					Ember.EmberServices:getSingleton():getServerService():connect(address)
+				end
 			end
 		end
-    end
+	end
 
 
-    
+	--The socket path is currently hard coded to cyphesis. The default is that Ember 
+	--is installed in the same location as cyphesis (i.e. using the same prefix
+	local socketPath = emberServices:getConfigService():getPrefix() .. "/var/tmp/cyphesis_cyphesis.sock"
+	if emberServices:getConfigService():itemExists("general", "socket") then
+		local configSocketPath = emberServices:getConfigService():getValue("general", "socket")
+		if configSocketPath then
+			if configSocketPath:is_string() then
+				socketPath = configSocketPath:as_string()
+			end
+		end
+	end
+
+	if serverService:hasLocalSocket(socketPath) then
+		self.widget:hide()
+		local socketDetectedWindow = guiManager:createWidget()
+		socketDetectedWindow:loadMainSheet("ServerSocketDetected.layout", "ServerSocketDetected/")
+		socketDetectedWindow:show();
+		socketDetectedWindow:getMainWindow():activate()
+		
+		socketDetectedWindow:getWindow("DontConnect"):subscribeEvent("Clicked", function(args)
+			guiManager:destroyWidget(socketDetectedWindow)
+			socketDetectedWindow = nil
+			showWindow()
+			return true
+		end)
+		
+		socketDetectedWindow:getWindow("ConnectNormally"):subscribeEvent("Clicked", function(args)
+			guiManager:destroyWidget(socketDetectedWindow)
+			socketDetectedWindow = nil
+			serverService:connectLocal(socketPath)
+			return true
+		end)
+
+		socketDetectedWindow:getWindow("ConnectAsAdmin"):subscribeEvent("Clicked", function(args)
+			guiManager:destroyWidget(socketDetectedWindow)
+			socketDetectedWindow = nil
+			showWindow()
+			return true
+		end)
+	else
+		showWindow()
+	end
 
 end
 
@@ -89,7 +132,7 @@ function ServerBrowser:connectWithColumnList()
 	local serverName
 	if self.serverList:getFirstSelectedItem() ~= nil then
 		local selectedRowIndex = self.serverList:getItemRowIndex(self.serverList:getFirstSelectedItem())
-	
+
 		if selectedRowIndex ~= -1 then
 			local selectedItem = self.serverList:getItemAtGridReference(CEGUI.MCLGridRef:new_local(selectedRowIndex, 7))
 			if selectedItem ~= nil then
@@ -107,7 +150,7 @@ function ServerBrowser:doConnect()
 
 	--first we check if there is text in the ManualServerName textbox
 	--if so, we try to connect to the server specified there
-	
+
 	if self.manualServerNameTextbox:getText() ~= "" then
 		serverName = self.manualServerNameTextbox:getText()
 		--Try to separate the port number, if available.
@@ -119,7 +162,7 @@ function ServerBrowser:doConnect()
 			Ember.EmberServices:getSingleton():getServerService():connect(serverName)
 		end
 	elseif self.serverList:getFirstSelectedItem() ~= nil then
-		--if ManualServerName is empty we try to connect to the server selected from the list 
+		--if ManualServerName is empty we try to connect to the server selected from the list
 		self:connectWithColumnList()
 	end
 end
@@ -127,9 +170,9 @@ end
 function ServerBrowser:Server_GotConnection(connection)
 	self.widget:hide()
 	connect(self.connectors, connection.Disconnected, function()
-			 self.widget:show()
-			 self.widget:getMainWindow():activate()
-		end)
+		self.widget:show()
+		self.widget:getMainWindow():activate()
+	end)
 end
 
 function ServerBrowser:Server_StatusChanged(status)
@@ -147,7 +190,7 @@ function ServerBrowser:Server_StatusChanged(status)
 	end
 	self.widget:getWindow("ConnectionOverlayStatus"):setText(text)
 	self.widget:getWindow("ConnectionOverlay"):setVisible(showOverlay)
-	
+
 end
 
 function ServerBrowser:CancelConnection_Click(args)
@@ -186,7 +229,7 @@ function ServerBrowser:refreshServerList()
 	local metaServer = emberServices:getMetaserverService():getMetaServer()
 	self.serverList:resetList()
 	local numberOfServerInfos = metaServer:getGameServerCount()
-	
+
 	for i = 0, numberOfServerInfos - 1 do
 		local sInfo = metaServer:getInfoForServer(i)
 		if sInfo:getStatus() == Eris.ServerInfo.VALID then
@@ -209,26 +252,26 @@ function ServerBrowser:addRow(sInfo)
 	self.serverList:addRow()
 
 	local item = Ember.OgreView.Gui.ColouredListItem:new(self:getSavedAccount(sInfo))
-	self.serverList:setItem(item, 0, rowNumber);	
+	self.serverList:setItem(item, 0, rowNumber);
 
 	local item = Ember.OgreView.Gui.ColouredListItem:new(sInfo:getServername())
 	self.serverList:setItem(item, 1, rowNumber)
-	
+
 	local item = Ember.OgreView.Gui.ColouredListItem:new(sInfo:getPing())
 	self.serverList:setItem(item, 2, rowNumber)
-	
+
 	local item = Ember.OgreView.Gui.ColouredListItem:new(sInfo:getNumClients())
 	self.serverList:setItem(item, 3,rowNumber)
-	
+
 	local item = Ember.OgreView.Gui.ColouredListItem:new(sInfo:getRuleset())
 	self.serverList:setItem(item, 4, rowNumber)
-	
+
 	local item = Ember.OgreView.Gui.ColouredListItem:new(sInfo:getServer())
 	self.serverList:setItem(item, 5, rowNumber)
-	
+
 	local item = Ember.OgreView.Gui.ColouredListItem:new(sInfo:getVersion())
 	self.serverList:setItem(item, 6, rowNumber)
-	
+
 	local item = Ember.OgreView.Gui.ColouredListItem:new(sInfo:getHostname())
 	self.serverList:setItem(item, 7, rowNumber)
 end
@@ -253,7 +296,7 @@ function ServerBrowser:MetaServer_ReceivedServerInfo(sInfo)
 	--we got some server info, add it to the server list
 
 	self:addRow(sInfo)
-	
+
 end
 
 serverBrowser = {connectors={}, hideOldServers = false, minimumVersion = '', minumumentitycount = 0, rows = {}}
