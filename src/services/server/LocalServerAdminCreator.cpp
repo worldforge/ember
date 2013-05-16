069b9fdabc9e296b1bbabcf64802a2d06c0d2c39
Erik Ogenvik <erik@ogenvik.org>
2013-05-16 23:00:52 +0200

    Add class for local admin login.
    When connecting to a local socket the user has the option to log in as
    an admin client even though no password or likewise is known. This class
    helps with the flow, hooking into the various steps the server service
    goes through to both create an account and to log in the user as a
    "creator" character.

---
+++ src/services/server/LocalServerAdminCreator.cpp
@@ -0,0 +1,91 @@
+/*
+ Copyright (C) 2013 Erik Ogenvik
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "LocalServerAdminCreator.h"
+
+#include "services/server/ServerService.h"
+
+#include <Eris/Account.h>
+#include <Eris/Connection.h>
+#include <Eris/Response.h>
+#include <Eris/CustomEntities.h>
+
+#include <Atlas/Objects/Anonymous.h>
+#include <Atlas/Objects/Operation.h>
+
+#include <wfmath/MersenneTwister.h>
+#include <sstream>
+
+using Atlas::Message::Element;
+using Atlas::Message::ListType;
+using Atlas::Message::MapType;
+using Atlas::Objects::Root;
+using Atlas::Objects::Entity::Anonymous;
+using Atlas::Objects::Operation::Create;
+using Atlas::Objects::Operation::Login;
+using Atlas::Objects::Operation::RootOperation;
+
+namespace Ember
+{
+
+LocalServerAdminCreator::LocalServerAdminCreator(ServerService& serverService) :
+		mServerService(serverService)
+{
+	serverService.GotAccount.connect(sigc::mem_fun(*this, &LocalServerAdminCreator::server_GotAccount));
+	serverService.LoginSuccess.connect(sigc::mem_fun(*this, &LocalServerAdminCreator::server_LoginSuccess));
+}
+
+LocalServerAdminCreator::~LocalServerAdminCreator()
+{
+}
+
+void LocalServerAdminCreator::server_GotAccount(Eris::Account* account)
+{
+	//When connecting to a local socket we should use a "sys" account.
+	//This works very much like an "admin" account, but is not persisted on the server.
+	//Thus we will first create a new account on the server, with a random name and password.
+
+	std::stringstream ss_password, ss_username;
+
+	WFMath::MTRand rand;
+	for (int i = 0; i < 20; ++i) {
+		ss_password << rand.randInt(9);
+	}
+
+	ss_username << "admin_ember_";
+	for (int i = 0; i < 5; ++i) {
+		ss_username << rand.randInt(9);
+	}
+
+	Atlas::Objects::Entity::Sys sysAccountOp;
+
+	sysAccountOp->setAttr("username", ss_username.str());
+	sysAccountOp->setAttr("password", ss_password.str());
+	account->createAccount(sysAccountOp);
+
+}
+
+void LocalServerAdminCreator::server_LoginSuccess(Eris::Account* account)
+{
+	mServerService.createCharacter("admin", "female", "creator", "", "", Atlas::Message::MapType());
+}
+}
---
+++ src/services/server/LocalServerAdminCreator.h
@@ -0,0 +1,70 @@
+/*
+ Copyright (C) 2013 Erik Ogenvik
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef LOCALSERVERADMINCREATOR_H_
+#define LOCALSERVERADMINCREATOR_H_
+
+#include <Atlas/Objects/ObjectsFwd.h>
+#include <sigc++/trackable.h>
+
+namespace Eris
+{
+class Account;
+}
+
+namespace Ember
+{
+class ServerService;
+
+/**
+ * @author Erik Ogenvik
+ * @brief Handles the flow of creating a "sys" account and logging in the user.
+ *
+ * This is meant to be used when the connected to a local server through a socket
+ * and the user wants to enter the world as a creator entity.
+ *
+ * Note that you need to delete any instance as soon as the user has logged in to
+ * prevent it from interacting with any other attempt to log in to a server.
+ */
+class LocalServerAdminCreator: public sigc::trackable
+{
+public:
+	/**
+	 * @brief Ctor.
+	 * @param serverService The server service.
+	 */
+	LocalServerAdminCreator(ServerService& serverService);
+
+	/**
+	 * @brief Dtor.
+	 */
+	virtual ~LocalServerAdminCreator();
+private:
+
+	/**
+	 * @brief The server service.
+	 */
+	ServerService& mServerService;
+
+	void server_GotAccount(Eris::Account* account);
+	void server_LoginSuccess(Eris::Account* account);
+
+};
+
+}
+#endif /* LOCALSERVERADMINCREATOR_H_ */
