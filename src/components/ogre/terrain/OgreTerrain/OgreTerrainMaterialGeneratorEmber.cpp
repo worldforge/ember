06792c8e349f6b7e4e8e2f7ecc368d6fab38e150
Erik Ogenvik <erik@ogenvik.org>
2013-12-16 22:18:27 +0100

    Remove invalid copyright assignment.

--- src/components/ogre/terrain/OgreTerrain/OgreTerrainMaterialGeneratorEmber.cpp
+++ src/components/ogre/terrain/OgreTerrain/OgreTerrainMaterialGeneratorEmber.cpp
@@ -1,6 +1,5 @@
 /*
  Copyright (C) 2013 Samuel Kogler
- Some parts Copyright (c) 2000-2013 Torus Knot Software Ltd
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
