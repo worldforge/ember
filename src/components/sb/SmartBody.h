9beb3143a37e4cb1b5c090f5765b7d6564c07c7d
Celine NOEL <laefy22@gmail.com>
2014-06-12 16:50:55 +0200

    Addition of a global header : <sb/SmartBody.h>

---
+++ src/components/sb/SmartBody.h
@@ -0,0 +1,14 @@
+#ifndef __SMARTBODY_H__
+#define __SMARTBODY_H__
+
+	#include <sb/SBScene.h>
+	#include <sb/SBCharacter.h>
+	#include <sb/SBSkeleton.h>
+	#include <sb/SBJointMap.h>
+	#include <sb/SBJointMapManager.h>
+	#include <sb/SBAssetManager.h>
+	#include <sb/SBRetarget.h>
+	#include <sb/SBRetargetManager.h>
+	#include <sb/SBMotion.h>
+
+#endif
