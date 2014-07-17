2a35286e04f7477795fe28875c0d924c756b0a67
Celine NOEL <laefy22@gmail.com>
2014-07-17 19:26:37 +0200

    Add missing documentation in SmartBodyAnimation.h.

--- src/components/sb/SmartBodyAnimation.h
+++ src/components/sb/SmartBodyAnimation.h
@@ -8,6 +8,7 @@ namespace Ember
 
 /**
  * @brief This class is used as a mapping between the name of an animation and the corresponding bml request.
+ *			(not finished).
  *
  * @author Céline NOEL <celine.noel.7294@gmail.com>
  *
@@ -17,19 +18,40 @@ class SmartBodyAnimation
 
 public:
 
-	enum Name 
+	/**
+	 * @brief The different name an animation can take.
+	 */
+	enum class Name 
 	{
-		IDLE, WALK, RUN, NOD, ANIMATIONS_COUNT
+		IDLE, WALK, RUN,			//Body postures.
+		NOD, 						//Head animations.
+
+		ANIMATIONS_COUNT			//Gives the number of supported animations.
 	};
 
+	/**
+	 * @brief Ctor.
+	 */
 	SmartBodyAnimation(SmartBodyAnimation::Name name);
+
+	/**
+	 * @brief Dtor.
+	 */
 	~SmartBodyAnimation();
 
+
+	/**
+	 * @brief Accessor for the request that is to be sent to the BMLProcessor to execute an animation on a character.
+	 * @return mBmlRequest.
+	 */
 	const std::string& getBmlRequest() const;
 
 
 private:
 
+	/**
+	 * @brief The corresponding request to send to the BMLProcessor to execute an animation on a character.
+	 */
 	std::string mBmlRequest;
 	
 };
