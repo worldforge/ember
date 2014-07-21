9549f573ca2914fd1727ac1ba6057000e9bdfb6b
Celine NOEL <laefy22@gmail.com>
2014-07-21 16:02:48 +0200

    Addition of the boilerplate.

--- src/components/ogre/model/ModelHumanoidAttachment.h
+++ src/components/ogre/model/ModelHumanoidAttachment.h
@@ -1,3 +1,21 @@
+/*
+ Copyright (C) 2014 Céline NOËL <celine.noel.7294@gmail.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #ifndef MODELHUMANOIDATTACHMENT_H
 #define MODELHUMANOIDATTACHMENT_H
 
@@ -19,6 +37,8 @@ namespace Model
  * This class derives from ModelAttachment. It overrides setPosition(): when an model is moved through SmartBody, the movement has to
  * be smoothed in order to seem natural. Consequently, the position retrieved from the server must be adjusted every frame to fit the 
  * animation. 
+ *
+ * @author Céline NOËL <celine.noel.7294@gmail.com>
  */
 class ModelHumanoidAttachment: public ModelAttachment
 {
