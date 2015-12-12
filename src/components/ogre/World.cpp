d5c0c1d135a1b6a286e2834c633500bf086443e9
Erik Ogenvik <erik@ogenvik.org>
2015-12-12 20:49:59 +0100

    Fix issue with newer versions of gcc.
    We must declare structs used in sigc:ref.

--- src/components/ogre/World.cpp
+++ src/components/ogre/World.cpp
@@ -30,6 +30,7 @@
 #include "ForestRenderingTechnique.h"
 #include "RenderDistanceManager.h"
 #include "AvatarCameraWarper.h"
+#include "GraphicalChangeAdapter.h"
 #include "components/ogre/AvatarCameraMotionHandler.h"
 #include "components/ogre/authoring/AuthoringManager.h"
 #include "components/ogre/authoring/AuthoringMoverConnector.h"
