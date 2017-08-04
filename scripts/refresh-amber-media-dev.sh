52719729a98ce69d7fdb65f9edae04e55fb87ebe
Erik Ogenvik <erik@ogenvik.org>
2017-08-05 00:57:08 +0200

    Altered how media is supplied through amber.

--- scripts/refresh-amber-media-dev.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-#updates amber with the needed media. This will only work for me (Erik Ogenvik) since only I know the password (as found in rsyncpasswd)
-#usage: ./refresh-amber-media-dev.sh <media svn directory> 
-
-scripts/make_dist_media.py $1 ${PWD}/media-dev 256 && rsync -uavz --password-file rsyncpasswd  media-dev/media/* amber.worldforge.org::media-dev-update
