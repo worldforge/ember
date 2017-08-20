1381d0fa3e4d605ecabfa35ef9274497529dade6
Erik Ogenvik <erik@ogenvik.org>
2017-08-20 19:49:25 +0200

    Moved main loop from Eris to here.

--- src/main/Application.cpp
+++ src/main/Application.cpp
@@ -52,26 +52,42 @@
 #include "components/lua/embertolua++.h"
 
 TOLUA_API int tolua_Ogre_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Eris_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_EmberServices_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_EmberOgre_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Helpers_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Framework_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_AtlasAdapters_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Adapters_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Representations_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Atlas_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Varconf_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Lua_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_ConnectorDefinitions_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Domain_open(lua_State* tolua_S);
+
 TOLUA_API int tolua_Cegui_open(lua_State* tolua_S);
 
 #include <boost/thread.hpp>
 #include <framework/TimedLog.h>
 
 #ifndef HAVE_SIGHANDLER_T
-typedef void (*sighandler_t)(int);
+
+typedef void (* sighandler_t)(int);
+
 #endif
 
 extern "C"
@@ -83,15 +99,13 @@ sighandler_t oldSignals[NSIG];
 using boost::posix_time::microsec_clock;
 using boost::posix_time::ptime;
 
-namespace Ember
-{
+namespace Ember {
 
 /**
  * @author Erik Ogenvik <erik@ogenvik.org>
  * @brief A simple listener class for the general:desiredfps config setting, which configures the capped fps.
  */
-class DesiredFpsListener: public ConfigListenerContainer
-{
+class DesiredFpsListener : public ConfigListenerContainer {
 protected:
 
 	/**
@@ -106,8 +120,7 @@ protected:
 	 */
 	long mMicrosecondsPerFrame;
 
-	void Config_DesiredFps(const std::string& section, const std::string& key, varconf::Variable& variable)
-	{
+	void Config_DesiredFps(const std::string& section, const std::string& key, varconf::Variable& variable) {
 		//Check for double, but cast to int. That way we'll catch all numbers.
 		if (variable.is_double()) {
 			mDesiredFps = static_cast<int>(variable);
@@ -126,8 +139,7 @@ public:
 	 * A listener will be set up listening for the general:desiredfps config setting.
 	 */
 	DesiredFpsListener() :
-			mDesiredFps(0), mMicrosecondsPerFrame(0)
-	{
+			mDesiredFps(0), mMicrosecondsPerFrame(0) {
 		registerConfigListener("general", "desiredfps", sigc::mem_fun(*this, &DesiredFpsListener::Config_DesiredFps));
 	}
 
@@ -135,8 +147,7 @@ public:
 	 * @brief Accessor for the desired framed per second.
 	 * If 0 no capping should occur.
 	 */
-	long getDesiredFps() const
-	{
+	long getDesiredFps() const {
 		return mDesiredFps;
 	}
 
@@ -144,13 +155,12 @@ public:
 	 * @brief Accessor for the minimum length (in microseconds) that each frame should take in order for the desired fps to be kept.
 	 * If 0 no capping will occur.
 	 */
-	long getMicrosecondsPerFrame() const
-	{
+	long getMicrosecondsPerFrame() const {
 		return mMicrosecondsPerFrame;
 	}
 };
 
-template<> Application *Singleton<Application>::ms_Singleton = 0;
+template<> Application* Singleton<Application>::ms_Singleton = 0;
 
 Application::Application(const std::string prefix, const std::string homeDir, const ConfigMap& configSettings) :
 		mSession(new Eris::Session()),
@@ -166,13 +176,11 @@ Application::Application(const std::string prefix, const std::string homeDir, co
 		mConfigSettings(configSettings),
 		mConsoleBackend(new ConsoleBackend()), Quit("quit", this, "Quit Ember."),
 		ToggleErisPolling("toggle_erispolling", this, "Switch server polling on and off."),
-		mScriptingResourceProvider(nullptr)
-{
+		mScriptingResourceProvider(nullptr) {
 
 }
 
-Application::~Application()
-{
+Application::~Application() {
 	// before shutting down, we write out the user config to user's ember home directory
 	ConfigService& configService = mServices->getConfigService();
 	configService.saveConfig(configService.getHomeDirectory(BaseDirType_CONFIG) + "ember.conf", varconf::USER);
@@ -195,29 +203,26 @@ Application::~Application()
 /**
  * Detach the input system, else the mouse will be locked.
  */
-extern "C" void shutdownHandler(int sig)
-{
+extern "C" void shutdownHandler(int sig) {
 	std::cerr << "Crashed with signal " << sig << ", will try to detach the input system gracefully. Please report bugs at https://bugs.launchpad.net/ember" << std::endl << std::flush;
 	if (Input::hasInstance()) {
 		Input::getSingleton().shutdownInteraction();
 	}
-	if (oldSignals[sig] != SIG_DFL && oldSignals[sig] != SIG_IGN ) {
+	if (oldSignals[sig] != SIG_DFL && oldSignals[sig] != SIG_IGN) {
 		/* Call saved signal handler. */
 		oldSignals[sig](sig);
 	} else {
 		/* Reraise the signal. */
-		signal(sig, SIG_DFL );
+		signal(sig, SIG_DFL);
 		raise(sig);
 	}
 }
 
-void Application::registerComponents()
-{
+void Application::registerComponents() {
 	mOgreView = new OgreView::EmberOgre();
 }
 
-void Application::mainLoop()
-{
+void Application::mainLoop() {
 	DesiredFpsListener desiredFpsListener;
 	Eris::EventService& eventService = mSession->getEventService();
 	Input& input(Input::getSingleton());
@@ -226,19 +231,20 @@ void Application::mainLoop()
 		try {
 			Log::sCurrentFrameStartMilliseconds = microsec_clock::local_time();
 
-//			TimedLog log("mainLoop");
-
 			unsigned int frameActionMask = 0;
 			boost::posix_time::microseconds desiredMicrosecondsPerFrame(desiredFpsListener.getMicrosecondsPerFrame());
 			TimeFrame timeFrame = TimeFrame(desiredMicrosecondsPerFrame);
 
+			mSession->getIoService().poll_one();
+
+			eventService.processOneHandler();
+
 			if (mWorldView) {
 				mWorldView->update();
-//				log.report("mWorldView->update()");
 			}
 
+
 			bool updatedRendering = mOgreView->renderOneFrame(timeFrame);
-//			log.report("updatedRendering");
 			if (updatedRendering) {
 				frameActionMask |= MainLoopController::FA_GRAPHICS;
 				frameActionMask |= MainLoopController::FA_INPUT;
@@ -249,11 +255,40 @@ void Application::mainLoop()
 
 			mServices->getSoundService().cycle();
 			frameActionMask |= MainLoopController::FA_SOUND;
-//			log.report("getSoundService().cycle()");
 
-			//Keep on running IO and handlers until we need to render again
-			eventService.processEvents(timeFrame.getRemainingTime(), mShouldQuit);
-//			log.report("processEvents");
+			//If there's time left this frame, poll any outstanding io handlers.
+			if (timeFrame.isTimeLeft()) {
+				size_t handersRun = 0;
+				do {
+					handersRun = mSession->getIoService().poll_one();
+				} while (handersRun != 0 && timeFrame.isTimeLeft());
+			}
+
+			//If there's still time left this frame, process any outstanding main thread handlers.
+			if (timeFrame.isTimeLeft()) {
+				size_t handersRun = 0;
+				do {
+					handersRun = eventService.processOneHandler();
+				} while (handersRun != 0 && timeFrame.isTimeLeft());
+			}
+
+			//And if there's yet still time left this frame, wait until time is up, and do io in the meantime.
+			if (timeFrame.isTimeLeft()) {
+				boost::asio::deadline_timer deadlineTimer(mSession->getIoService());
+				deadlineTimer.expires_at(boost::asio::time_traits<boost::posix_time::ptime>::now() + timeFrame.getRemainingTime());
+
+				bool exitLoop = false;
+				deadlineTimer.async_wait([&](boost::system::error_code ec) {
+					if (!ec) {
+						exitLoop = true;
+					}
+				});
+
+				while (timeFrame.isTimeLeft() && !exitLoop) {
+					mSession->getIoService().run_one();
+				}
+				deadlineTimer.cancel();
+			}
 
 			mMainLoopController.EventFrameProcessed(timeFrame, frameActionMask);
 
@@ -272,12 +307,10 @@ void Application::mainLoop()
 	} while (!mShouldQuit);
 }
 
-void Application::prepareComponents()
-{
+void Application::prepareComponents() {
 }
 
-void Application::initializeServices()
-{
+void Application::initializeServices() {
 	// Initialize Ember services
 	std::cout << "Initializing Ember services" << std::endl;
 
@@ -296,7 +329,7 @@ void Application::initializeServices()
 
 	//output all logging to ember.log
 	std::string filename(configService.getHomeDirectory(BaseDirType_DATA) + "ember.log");
-	mLogOutStream = std::unique_ptr < std::ofstream > (new std::ofstream(filename.c_str()));
+	mLogOutStream = std::unique_ptr<std::ofstream>(new std::ofstream(filename.c_str()));
 
 	//write to the log the version number
 	*mLogOutStream << "Ember version " << VERSION << std::endl;
@@ -317,7 +350,7 @@ void Application::initializeServices()
 
 	int result = chdir(configService.getHomeDirectory(BaseDirType_CONFIG).c_str());
 	if (result) {
-		S_LOG_WARNING("Could not change directory to '"<< configService.getHomeDirectory(BaseDirType_CONFIG).c_str() <<"'.");
+		S_LOG_WARNING("Could not change directory to '" << configService.getHomeDirectory(BaseDirType_CONFIG).c_str() << "'.");
 	}
 
 	//load the config file. Note that this will load the shared config file, and then the user config file if available.
@@ -402,23 +435,19 @@ void Application::initializeServices()
 	EventServicesInitialized.emit();
 }
 
-void Application::Server_GotView(Eris::View* view)
-{
+void Application::Server_GotView(Eris::View* view) {
 	mWorldView = view;
 }
 
-void Application::Server_DestroyedView()
-{
+void Application::Server_DestroyedView() {
 	mWorldView = 0;
 }
 
-Eris::View* Application::getMainView()
-{
+Eris::View* Application::getMainView() {
 	return mWorldView;
 }
 
-void Application::startScripting()
-{
+void Application::startScripting() {
 	//this should be defined in some kind of text file, which should be different depending on what game you're playing (like deeds)
 	try {
 		//load the bootstrap script which will load all other scripts
@@ -473,8 +502,7 @@ void Application::startScripting()
 	}
 }
 
-void Application::start()
-{
+void Application::start() {
 
 	try {
 		if (!mOgreView->setup(Input::getSingleton(), mMainLoopController, mSession->getEventService())) {
@@ -502,8 +530,7 @@ void Application::start()
 
 }
 
-void Application::runCommand(const std::string& command, const std::string& args)
-{
+void Application::runCommand(const std::string& command, const std::string& args) {
 	if (command == Quit.getCommand()) {
 		mShouldQuit = true;
 	} else if (ToggleErisPolling == command) {
