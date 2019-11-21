5fca83efd37c82a9d3fd2b5f2b4c82457e56abda
Erik Ogenvik <erik@ogenvik.org>
2019-11-21 17:56:29 +0100

    Fixed tests.

--- tests/TestTasks.cpp
+++ tests/TestTasks.cpp
@@ -17,15 +17,14 @@
 #include <boost/asio.hpp>
 
 #define _GLIBCXX_USE_NANOSLEEP 1
+
 #include <thread>
 #include <chrono>
 #include <condition_variable>
 
-namespace Ember
-{
+namespace Ember {
 
-class CounterTask: public Tasks::ITask
-{
+class CounterTask : public Tasks::ITask {
 public:
 
 	int& mCounter;
@@ -33,13 +32,11 @@ public:
 	int mSleep;
 
 	CounterTask(int& counter, int sleep = 0) :
-		mCounter(counter), mSleep(sleep)
-	{
+			mCounter(counter), mSleep(sleep) {
 		mCounter += 2;
 	}
 
-	void executeTaskInBackgroundThread(Tasks::TaskExecutionContext& context) override
-	{
+	void executeTaskInBackgroundThread(Tasks::TaskExecutionContext& context) override {
 		if (mSleep) {
 			std::this_thread::sleep_for(std::chrono::milliseconds(mSleep));
 		}
@@ -50,8 +47,7 @@ public:
 	 * @brief Executes the task in the main thread, after executeTaskInBackgroundThread() has been called.
 	 * Since this will happen in the main thread you shouldn't do any time consuming things here, since it will lock up the rendering.
 	 */
-	bool executeTaskInMainThread() override
-	{
+	bool executeTaskInMainThread() override {
 		mCounter--;
 		return true;
 	}
@@ -67,32 +63,27 @@ public:
 
 };
 
-class TimeTask: public Tasks::ITask
-{
-public:
+struct TimeTask : public Tasks::ITask {
 
 	TimeHolder& timeHolder;
-	ITask* subtask;
+	std::unique_ptr<ITask> subtask;
 	Tasks::ITaskExecutionListener* listener;
 
-	TimeTask(TimeHolder& timeHolder, ITask* subtask = 0, Tasks::ITaskExecutionListener* listener = 0)
-	: timeHolder(timeHolder), subtask(subtask), listener(listener)
-	{
+	explicit TimeTask(TimeHolder& timeHolder, std::unique_ptr<ITask> subtask = {}, Tasks::ITaskExecutionListener* listener = nullptr)
+			: timeHolder(timeHolder), subtask(std::move(subtask)), listener(listener) {
 	}
 
-	void executeTaskInBackgroundThread(Tasks::TaskExecutionContext& context) override
-	{
+	void executeTaskInBackgroundThread(Tasks::TaskExecutionContext& context) override {
 		if (subtask) {
 			//sleep a little so that we get different times on the tasks
 			std::this_thread::sleep_for(std::chrono::milliseconds(5));
-			context.executeTask(subtask,listener);
+			context.executeTask(std::move(subtask), listener);
 		}
 		//sleep a little so that we get different times on the tasks
 		std::this_thread::sleep_for(std::chrono::milliseconds(5));
 	}
 
-	bool executeTaskInMainThread() override
-	{
+	bool executeTaskInMainThread() override {
 		//sleep a little so that we get different times on the tasks
 		std::this_thread::sleep_for(std::chrono::milliseconds(5));
 		timeHolder.time = WFMath::TimeStamp::now();
@@ -104,13 +95,12 @@ public:
 	}
 };
 
-class CounterTaskBackgroundException: public CounterTask {
+class CounterTaskBackgroundException : public CounterTask {
 public:
 	CounterTaskBackgroundException(int& counter) : CounterTask(counter) {
 	}
 
-	virtual void executeTaskInBackgroundThread(Tasks::TaskExecutionContext& context)
-	{
+	virtual void executeTaskInBackgroundThread(Tasks::TaskExecutionContext& context) {
 		throw Ember::Exception();
 	}
 };
@@ -129,60 +119,54 @@ public:
 
 	}
 
-	virtual void executionStarted()
-	{
+	virtual void executionStarted() {
 		startedTime = WFMath::TimeStamp::now();
 		started = true;
 	}
 
-	virtual void executionEnded()
-	{
+	virtual void executionEnded() {
 		endedTime = WFMath::TimeStamp::now();
 		ended = true;
 	}
 
-	virtual void executionError(const Ember::Exception& exception)
-	{
+	virtual void executionError(const Ember::Exception& exception) {
 		errorTime = WFMath::TimeStamp::now();
 		error = true;
 	}
 };
 
-class TaskTestCase: public CppUnit::TestFixture
-{
-	CPPUNIT_TEST_SUITE(TaskTestCase);
-	CPPUNIT_TEST(testSimpleTaskRun);
-	CPPUNIT_TEST(testSimpleTaskRunAndPoll);
-	CPPUNIT_TEST(testSimpleTaskRunTwoExecs);
-	CPPUNIT_TEST(testSimpleTaskRunTwoTasks);
-	CPPUNIT_TEST(testSimpleTaskRunTwoTasksTwoExecs);
-	CPPUNIT_TEST(testListener);
-	CPPUNIT_TEST(testBackgroundException);
-	CPPUNIT_TEST(testTaskOrder);
-	CPPUNIT_TEST(testSubTaskOrder);
+class TaskTestCase : public CppUnit::TestFixture {
+CPPUNIT_TEST_SUITE(TaskTestCase);
+		CPPUNIT_TEST(testSimpleTaskRun);
+		CPPUNIT_TEST(testSimpleTaskRunAndPoll);
+		CPPUNIT_TEST(testSimpleTaskRunTwoExecs);
+		CPPUNIT_TEST(testSimpleTaskRunTwoTasks);
+		CPPUNIT_TEST(testSimpleTaskRunTwoTasksTwoExecs);
+		CPPUNIT_TEST(testListener);
+		CPPUNIT_TEST(testBackgroundException);
+		CPPUNIT_TEST(testTaskOrder);
+		CPPUNIT_TEST(testSubTaskOrder);
 
 	CPPUNIT_TEST_SUITE_END();
 
 	boost::asio::io_service io_service;
 public:
-	void testSimpleTaskRun()
-	{
+	void testSimpleTaskRun() {
 		int counter = 0;
 		{
 			Eris::EventService es(io_service);
 			Tasks::TaskQueue taskQueue(1, es);
-			taskQueue.enqueueTask(new CounterTask(counter));
+			taskQueue.enqueueTask(std::make_unique<CounterTask>(counter));
 		}
 		CPPUNIT_ASSERT(counter == 0);
 	}
 
-	void testSimpleTaskRunAndPoll()
-	{
+	void testSimpleTaskRunAndPoll() {
 		int counter = 0;
 		{
 			Eris::EventService es(io_service);
 			Tasks::TaskQueue taskQueue(1, es);
-			taskQueue.enqueueTask(new CounterTask(counter));
+			taskQueue.enqueueTask(std::make_unique<CounterTask>(counter));
 			//200 ms should be enough... This isn't deterministic though.
 			std::this_thread::sleep_for(std::chrono::milliseconds(200));
 			es.processAllHandlers();
@@ -191,70 +175,64 @@ public:
 
 	}
 
-	void testSimpleTaskRunTwoExecs()
-	{
+	void testSimpleTaskRunTwoExecs() {
 		int counter = 0;
 		{
 			Eris::EventService es(io_service);
 			Tasks::TaskQueue taskQueue(2, es);
-			taskQueue.enqueueTask(new CounterTask(counter));
+			taskQueue.enqueueTask(std::make_unique<CounterTask>(counter));
 		}
 		CPPUNIT_ASSERT(counter == 0);
 	}
 
-	void testSimpleTaskRunTwoTasks()
-	{
+	void testSimpleTaskRunTwoTasks() {
 		int counter = 0;
 		{
 			Eris::EventService es(io_service);
 			Tasks::TaskQueue taskQueue(1, es);
-			taskQueue.enqueueTask(new CounterTask(counter, 200));
-			taskQueue.enqueueTask(new CounterTask(counter));
+			taskQueue.enqueueTask(std::make_unique<CounterTask>(counter, 200));
+			taskQueue.enqueueTask(std::make_unique<CounterTask>(counter));
 		}
 		CPPUNIT_ASSERT(counter == 0);
 	}
 
-	void testSimpleTaskRunTwoTasksTwoExecs()
-	{
+	void testSimpleTaskRunTwoTasksTwoExecs() {
 		int counter = 0;
 		{
 			Eris::EventService es(io_service);
 			Tasks::TaskQueue taskQueue(2, es);
-			taskQueue.enqueueTask(new CounterTask(counter, 200));
-			taskQueue.enqueueTask(new CounterTask(counter));
+			taskQueue.enqueueTask(std::make_unique<CounterTask>(counter, 200));
+			taskQueue.enqueueTask(std::make_unique<CounterTask>(counter));
 		}
 		CPPUNIT_ASSERT(counter == 0);
 	}
 
-	void testListener()
-	{
+	void testListener() {
 		SimpleListener listener;
 		int counter = 0;
 		{
 			Eris::EventService es(io_service);
 			Tasks::TaskQueue taskQueue(1, es);
-			taskQueue.enqueueTask(new CounterTask(counter), &listener);
+			taskQueue.enqueueTask(std::make_unique<CounterTask>(counter), &listener);
 		}
 		CPPUNIT_ASSERT(counter == 0);
 		CPPUNIT_ASSERT(listener.started);
 		CPPUNIT_ASSERT(listener.ended);
 	}
 
-	void testBackgroundException()
-	{
+	void testBackgroundException() {
 		SimpleListener listener;
 		int counter = 0;
 		{
 			Eris::EventService es(io_service);
 			Tasks::TaskQueue taskQueue(1, es);
-			taskQueue.enqueueTask(new CounterTaskBackgroundException(counter), &listener);
+			taskQueue.enqueueTask(std::make_unique<CounterTaskBackgroundException>(counter), &listener);
 		}
 		CPPUNIT_ASSERT(counter == 1);
 		CPPUNIT_ASSERT(listener.error);
 	}
 
-	void testTaskOrder()
-	{
+	void testTaskOrder() {
 		SimpleListener listener1;
 		SimpleListener listener2;
 		SimpleListener listener3;
@@ -264,9 +242,9 @@ public:
 		{
 			Eris::EventService es(io_service);
 			Tasks::TaskQueue taskQueue(1, es);
-			taskQueue.enqueueTask(new TimeTask(time1), &listener1);
-			taskQueue.enqueueTask(new TimeTask(time2), &listener2);
-			taskQueue.enqueueTask(new TimeTask(time3), &listener3);
+			taskQueue.enqueueTask(std::make_unique<TimeTask>(time1), &listener1);
+			taskQueue.enqueueTask(std::make_unique<TimeTask>(time2), &listener2);
+			taskQueue.enqueueTask(std::make_unique<TimeTask>(time3), &listener3);
 		}
 		CPPUNIT_ASSERT(listener1.startedTime < listener2.startedTime);
 		CPPUNIT_ASSERT(listener2.startedTime < listener3.startedTime);
@@ -276,8 +254,7 @@ public:
 		CPPUNIT_ASSERT(time2.time < time3.time);
 	}
 
-	void testSubTaskOrder()
-	{
+	void testSubTaskOrder() {
 		SimpleListener listener1;
 		SimpleListener listener2;
 		SimpleListener listener3;
@@ -287,8 +264,8 @@ public:
 		{
 			Eris::EventService es(io_service);
 			Tasks::TaskQueue taskQueue(1, es);
-			taskQueue.enqueueTask(new TimeTask(time1, new TimeTask(time2), &listener2), &listener1);
-			taskQueue.enqueueTask(new TimeTask(time3), &listener3);
+			taskQueue.enqueueTask(std::make_unique<TimeTask>(time1, std::make_unique<TimeTask>(time2), &listener2), &listener1);
+			taskQueue.enqueueTask(std::make_unique<TimeTask>(time3), &listener3);
 		}
 		//task 1 should start before task 2
 		CPPUNIT_ASSERT(listener1.startedTime < listener2.startedTime);
@@ -306,12 +283,11 @@ public:
 
 }
 
-CPPUNIT_TEST_SUITE_REGISTRATION( Ember::TaskTestCase);
+CPPUNIT_TEST_SUITE_REGISTRATION(Ember::TaskTestCase);
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
 	CppUnit::TextUi::TestRunner runner;
-	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+	CppUnit::TestFactoryRegistry& registry = CppUnit::TestFactoryRegistry::getRegistry();
 	runner.addTest(registry.makeTest());
 
 	// Shows a message as each test starts
