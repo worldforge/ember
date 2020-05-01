f52dd72a0136e3e837170e59bbf272d7b8c00c10
Erik Ogenvik <erik@ogenvik.org>
2020-05-01 22:01:23 +0200

    Fixed doll slot handling.

--- src/components/ogre/widgets/Inventory.lua
+++ src/components/ogre/widgets/Inventory.lua
@@ -77,170 +77,157 @@ function Inventory:buildWidget(avatarEntity)
 
 end
 
-function Inventory:createAttachmentSlot(avatarEntity, dollSlot, attachment)
-    -- 	self.doll.torso = self:createDollSlot("body", self.doll.image:getChild("Torso"), "Drop an entity here to attach it to the torso.")
-    dollSlot.droppedHandler = function(entityIcon)
-        if dollSlot.isValidDrop(entityIcon) then
-            emberOgre:getWorld():getAvatar():getErisAvatar():wield(entityIcon:getEntity(), attachment)
-            local icon = dollSlot.slot:getEntityIcon()
-            if icon then
-                self.iconView:addEntityIcon(icon)
-            end
-            dollSlot.slot:addEntityIcon(entityIcon)
-        end
-    end
-    dollSlot.entityIconDropped_connector = createConnector(dollSlot.slot.EventIconDropped):connect(dollSlot.droppedHandler)
-    dollSlot.observer = Ember.AttributeObserver:new_local(avatarEntity, dollSlot.attributePath, ".")
-    dollSlot.attributeChanged = function(element)
-        local result, entityId = Eris.Entity:extractEntityId(element, entityId)
-        if result then
-            local slotUpdateFunc = function(icon)
-                --check that we've not already have added the icon to this slot
-                if dollSlot.slot:getEntityIcon() ~= icon then
-                    local oldIcon = dollSlot.slot:removeEntityIcon()
-                    dollSlot.slot:addEntityIcon(icon)
-                    if oldIcon then
-                        self.iconView:addEntityIcon(oldIcon)
-                    end
-                end
-            end
 
-            ----Either we have created an icon for the entity yet, or we have to wait a little until it's available
-            local icon = self.iconView:getEntityIcon(entityId)
-            if icon then
-                slotUpdateFunc(icon)
-            else
-                local delayedUpdater = function(entityIcon)
-                    if entityIcon:getEntity():getId() == entityId then
-                        slotUpdateFunc(entityIcon)
-                        self.newEntityListeners[dollSlot.attributePath] = nil
-                    end
-                end
-                self.newEntityListeners[dollSlot.attributePath] = delayedUpdater
-            end
-        end
-    end
-    dollSlot.attributeChanged_connector = createConnector(dollSlot.observer.EventChanged):connect(dollSlot.attributeChanged)
-
-    dollSlot.iconDraggedOff = function()
-        --do unwield stuff
-        emberOgre:getWorld():getAvatar():getErisAvatar():wield(nil, attachment)
-    end
-    dollSlot.iconDraggedOff_connector = createConnector(dollSlot.slot.EventIconDraggedOff):connect(dollSlot.iconDraggedOff)
-
-    dollSlot.observer:forceEvaluation()
-
-end
 
 function Inventory:setupDoll(avatarEntity)
-    self.doll = {}
-    self.doll.image = self.widget:getWindow("DollImage")
-    self.doll.renderer = Ember.OgreView.Gui.ModelRenderer:new(self.doll.image, "doll")
-    self.doll.renderer:setActive(false)
+    local doll = { slots = {} }
+    doll.image = self.widget:getWindow("DollImage")
+    doll.renderer = Ember.OgreView.Gui.ModelRenderer:new(doll.image, "doll")
+    doll.renderer:setActive(false)
 
-    self.doll.handPrimary = self:createDollSlot("attached_hand_primary", self.doll.image:getChild("RightHand"), "Drop an entity here to attach it to the primary hand.", "")
-    self.doll.handPrimaryAttachmentSlot = self:createAttachmentSlot(avatarEntity, self.doll.handPrimary, "hand_primary")
+    function createDollSlot(attributePath, containerWindow, tooltipText, wearRestriction)
+        local dollSlot = {}
+        dollSlot.slot = self.entityIconManager:createSlot(self.iconsize)
+        dollSlot.container = containerWindow
+        dollSlot.container:addChild(dollSlot.slot:getWindow())
+        dollSlot.slot:getWindow():setInheritsTooltipText(true)
+        dollSlot.container:setTooltipText(tooltipText)
+        dollSlot.wearRestriction = wearRestriction
+        dollSlot.attributePath = attributePath
 
-    self.doll.handSecondary = self:createDollSlot("attached_hand_secondary", self.doll.image:getChild("LeftHand"), "Drop an entity here to attach it to the secondary hand.", "hand_secondary")
-    self.doll.handSecondaryAttachmentSlot = self:createAttachmentSlot(avatarEntity, self.doll.handSecondary, "hand_secondary")
+        dollSlot.isValidDrop = function(entityIcon)
+            --TODO: look at the constraint and do client resolving to check if it's allowed
+            if wearRestriction == "" then
+                return true
+            end
+            --TODO: this should be replaced with checking the constraint at the client instead
+            if entityIcon:getEntity():hasProperty("worn") then
+                local wornElement = entityIcon:getEntity():valueOfProperty("worn")
+                if wornElement:isString() then
+                    local worn = wornElement:asString()
+                    if worn == wearRestriction then
+                        return true
+                    end
+                end
+            end
+            return false
+        end
 
-    self.doll.torso = self:createDollSlot("attached_torso", self.doll.image:getChild("Torso"), "Drop an entity here to attach it to the torso.", "torso")
-    self.doll.torsoAttachmentSlot = self:createAttachmentSlot(avatarEntity, self.doll.torso, "torso")
+        dollSlot.entityIconDragStart = function(entityIcon)
+            if dollSlot.isValidDrop(entityIcon) then
+            else
+                dollSlot.container:setProperty("FrameEnabled", "false")
+            end
+        end
 
-    self.doll.back = self:createDollSlot("attached_back", self.doll.image:getChild("Back"), "Drop an entity here to attach it to the back.", "back")
-    self.doll.backAttachmentSlot = self:createAttachmentSlot(avatarEntity, self.doll.back, "back")
+        dollSlot.entityIconDragStop = function()
+            dollSlot.container:setProperty("FrameEnabled", "true")
+        end
 
-    self.doll.head = self:createDollSlot("attached_head", self.doll.image:getChild("Head"), "Drop an entity here to attach it to the head.", "head")
-    self.doll.headAttachmentSlot = self:createAttachmentSlot(avatarEntity, self.doll.head, "head")
+        connect(self.connectors, self.entityIconManager.EventIconDragStart, dollSlot.entityIconDragStart)
+        connect(self.connectors, self.entityIconManager.EventIconDragStop, dollSlot.entityIconDragStop)
 
-    self.doll.legs = self:createDollSlot("attached_legs", self.doll.image:getChild("Legs"), "Drop an entity here to attach it to the legs.", "legs")
-    self.doll.legsAttachmentSlot = self:createAttachmentSlot(avatarEntity, self.doll.legs, "legs")
+        dollSlot.shutdown = function()
+            self.entityIconManager:destroySlot(dollSlot.slot)
+        end
 
-    self.doll.feet = self:createDollSlot("attached_feet", self.doll.image:getChild("Feet"), "Drop an entity here to attach it to the feet.", "feet")
-    self.doll.feetAttachmentSlot = self:createAttachmentSlot(avatarEntity, self.doll.feet, "feet")
+        return dollSlot
+    end
+
+    function createAttachmentSlot(dollSlot, attachment)
+        dollSlot.droppedHandler = function(entityIcon)
+            if dollSlot.isValidDrop(entityIcon) then
+                emberOgre:getWorld():getAvatar():getErisAvatar():wield(entityIcon:getEntity(), attachment)
+                local icon = dollSlot.slot:getEntityIcon()
+                if icon then
+                    self.iconView:addEntityIcon(icon)
+                end
+                dollSlot.slot:addEntityIcon(entityIcon)
+            end
+        end
+        dollSlot.entityIconDropped_connector = createConnector(dollSlot.slot.EventIconDropped):connect(dollSlot.droppedHandler)
+        dollSlot.observer = Ember.AttributeObserver:new_local(avatarEntity, dollSlot.attributePath, ".")
+        dollSlot.attributeChanged = function(element)
+            local result, entityId = Eris.Entity:extractEntityId(element, entityId)
+            if result then
+                local slotUpdateFunc = function(icon)
+                    --check that we've not already have added the icon to this slot
+                    if dollSlot.slot:getEntityIcon() ~= icon then
+                        local oldIcon = dollSlot.slot:removeEntityIcon()
+                        dollSlot.slot:addEntityIcon(icon)
+                        if oldIcon then
+                            self.iconView:addEntityIcon(oldIcon)
+                        end
+                    end
+                end
+
+                ----Either we have created an icon for the entity yet, or we have to wait a little until it's available
+                local icon = self.iconView:getEntityIcon(entityId)
+                if icon then
+                    slotUpdateFunc(icon)
+                else
+                    local delayedUpdater = function(entityIcon)
+                        if entityIcon:getEntity():getId() == entityId then
+                            slotUpdateFunc(entityIcon)
+                            self.newEntityListeners[dollSlot.attributePath] = nil
+                        end
+                    end
+                    self.newEntityListeners[dollSlot.attributePath] = delayedUpdater
+                end
+            end
+        end
+        dollSlot.attributeChanged_connector = createConnector(dollSlot.observer.EventChanged):connect(dollSlot.attributeChanged)
+
+        dollSlot.iconDraggedOff = function()
+            --do unwield stuff
+            emberOgre:getWorld():getAvatar():getErisAvatar():wield(nil, attachment)
+        end
+        dollSlot.iconDraggedOff_connector = createConnector(dollSlot.slot.EventIconDraggedOff):connect(dollSlot.iconDraggedOff)
+
+        dollSlot.observer:forceEvaluation()
+
+    end
+
+    function createSlot(attachment, windowName, tooltipText, wearRestriction)
+        local slot = createDollSlot("attached_" .. attachment, doll.image:getChild(windowName), tooltipText, wearRestriction)
+        table.insert(doll.slots, slot)
+        createAttachmentSlot(slot, attachment)
+    end
+
+    createSlot("hand_primary", "RightHand", "Drop an entity here to attach it to the primary hand.", "")
+    createSlot("hand_secondary", "LeftHand", "Drop an entity here to attach it to the secondary hand.", "hand_secondary")
+    createSlot("torso", "Torso", "Drop an entity here to attach it to the torso.", "torso")
+    createSlot("back", "Back", "Drop an entity here to attach it to the back.", "back")
+    createSlot("head", "Head", "Drop an entity here to attach it to the head.", "head")
+    createSlot("legs", "Legs", "Drop an entity here to attach it to the legs.", "legs")
+    createSlot("feet", "Feet", "Drop an entity here to attach it to the feet.", "feet")
 
     local representationUpdate = function()
         local model = Ember.OgreView.Model.ModelRepresentation:getModelForEntity(avatarEntity)
         if model ~= nil then
-            self.doll.renderer:showModel(model:getDefinition())
-            self.doll.renderer:setCameraDistance(0.75)
-            self.doll.renderer:updateRender()
+            doll.renderer:showModel(model:getDefinition())
+            doll.renderer:setCameraDistance(0.75)
+            doll.renderer:updateRender()
         end
     end
 
     representationUpdate()
 
-    table.insert(self.connectors, createConnector(avatarEntity.EventChangedGraphicalRepresentation):connect(representationUpdate))
+    connect(self.connectors, avatarEntity.EventChangedGraphicalRepresentation, representationUpdate)
 
+    self.doll = doll
 end
 
-function Inventory:updateDoll()
-end
-
-function Inventory:createDollSlot(attributePath, containerWindow, tooltipText, wearRestriction)
-    local dollSlot = {}
-    dollSlot.slot = self.entityIconManager:createSlot(self.iconsize)
-    dollSlot.container = containerWindow
-    dollSlot.container:addChild(dollSlot.slot:getWindow())
-    dollSlot.slot:getWindow():setInheritsTooltipText(true)
-    dollSlot.container:setTooltipText(tooltipText)
-    dollSlot.wearRestriction = wearRestriction
-    dollSlot.attributePath = attributePath
-
-    dollSlot.isValidDrop = function(entityIcon)
-        if wearRestriction == "" then
-            return true
-        end
-        if entityIcon:getEntity():hasProperty("worn") then
-            local wornElement = entityIcon:getEntity():valueOfProperty("worn")
-            if wornElement:isString() then
-                local worn = wornElement:asString()
-                if worn == wearRestriction then
-                    return true
-                end
-            end
-        end
-        return false
-    end
-
-    dollSlot.entityIconDragStart = function(entityIcon)
-        if dollSlot.isValidDrop(entityIcon) then
-        else
-            dollSlot.container:setProperty("FrameEnabled", "false")
-        end
-    end
-
-    dollSlot.entityIconDragStop = function(entityIcon)
-        dollSlot.container:setProperty("FrameEnabled", "true")
-    end
-
-    dollSlot.entityIconDragStart_connector = createConnector(self.entityIconManager.EventIconDragStart):connect(dollSlot.entityIconDragStart)
-    dollSlot.entityIconDragStop_connector = createConnector(self.entityIconManager.EventIconDragStop):connect(dollSlot.entityIconDragStop)
-
-    dollSlot.shutdown = function()
-        self.entityIconManager:destroySlot(dollSlot.slot)
-        dollSlot.entityIconDragStart_connector:disconnect()
-        dollSlot.entityIconDragStop_connector:disconnect()
-    end
-
-    return dollSlot
-end
 
 function Inventory:shutdown()
     disconnectAll(self.connectors)
     deleteSafe(self.helper)
     deleteSafe(self.DragDrop)
     deleteSafe(self.iconView)
-    if self.doll then
-        if deleteSafe(self.doll.renderer) then
-            self.doll.handPrimary.shutdown()
-            self.doll.torso.shutdown()
-            self.doll.back.shutdown()
-            self.doll.head.shutdown()
-            self.doll.legs.shutdown()
-            self.doll.feet.shutdown()
-        end
+    for _, value in pairs(self.doll.slots) do
+        value:shutdown()
     end
+    deleteSafe(self.doll.renderer)
 
     guiManager:destroyWidget(self.widget)
 end
@@ -248,14 +235,9 @@ end
 Inventory.createdAvatarEntityConnector = createConnector(emberOgre.EventCreatedAvatarEntity):connect(function(avatarEntity)
     if emberOgre:getWorld():getAvatar():isAdmin() == false then
         inventory = { connectors = {},
-                      useButtons = {},
                       iconsize = 32,
-                      columns = 4,
-                      iconcounter = 0,
-                      slotcounter = 0,
                       icons = {},
                       slots = {},
-                      menu = { menuShown = false, activeEntityWrapper = nil },
                       newEntityListeners = {} }
         setmetatable(inventory, { __index = Inventory })
         inventory:buildWidget(avatarEntity)
