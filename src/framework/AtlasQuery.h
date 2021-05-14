6d9bb4c3ac15dbc62e0efb887169b814ad6e69a2
Erik Ogenvik <erik@ogenvik.org>
2021-05-14 21:11:14 +0200

    Extend Atlas Query functions.

--- src/framework/AtlasQuery.h
+++ src/framework/AtlasQuery.h
@@ -32,25 +32,63 @@ namespace Ember {
 class AtlasQuery {
 
 public:
-	template <typename T>
+	/**
+	 * Find the element with the supplied name and call on the callback.
+	 * @tparam T The type to extract from the map.
+	 * @param element An element which is a Map.
+	 * @param name The key to look for.
+	 * @param callback A callback.
+	 */
+	template<typename T>
 	static void find(const Atlas::Message::Element& element, const std::string& name, const std::function<void(const T&)>& callback);
-	template <typename T>
+
+	/**
+	 * Find the element with the supplied name and call on the callback.
+	 * @tparam T The type to extract from the map.
+	 * @param map A map.
+	 * @param name The key to look for.
+	 * @param callback A callback.
+	 */
+	template<typename T>
 	static void find(const Atlas::Message::MapType& map, const std::string& name, const std::function<void(const T&)>& callback);
-	template <typename T, typename R>
+
+	template<typename T, typename R>
 	static R extract(const Atlas::Message::Element& element, const std::string& name, const std::function<R(const T&)>& callback);
-	template <typename R>
+
+	template<typename R>
 	static R extract(const Atlas::Message::MapType& map, const std::string& name, const std::function<R(const Atlas::Message::MapType&)>& callback);
+
+	template<typename R>
+	static boost::optional<R> extract(const Atlas::Message::Element& element, const std::string& name);
+
+	/**
+	 * Checks if the supplied element is of the specified type.
+	 * @tparam R The type, one of the Atlas::Message::Element types.
+	 * @param element An element.
+	 * @return True if the element was of the type.
+	 */
+	template<typename R>
+	static bool elementCheck(const Atlas::Message::Element& element);
+
+	/**
+	 * Extracts the value from the element, using the supplied type.
+	 * Note that you must first check that it's of the correct type.
+	 * @tparam R The type, one of the Atlas::Message::Element types.
+	 * @param element An element.
+	 * @return A copy of the value.
+	 */
+	template<typename R>
+	static R elementValue(const Atlas::Message::Element& element);
 };
 
-template <typename T>
+template<typename T>
 inline void AtlasQuery::find(const Atlas::Message::Element& element, const std::string& name, const std::function<void(const T&)>& callback) {
 	if (element.isMap()) {
 		find(element.Map(), name, callback);
 	}
 }
 
-
-template <>
+template<>
 inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::string& name, const std::function<void(const Atlas::Message::MapType&)>& callback) {
 	auto I = map.find(name);
 	if (I != map.end()) {
@@ -60,7 +98,7 @@ inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::stri
 	}
 }
 
-template <>
+template<>
 inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::string& name, const std::function<void(const Atlas::Message::ListType&)>& callback) {
 	auto I = map.find(name);
 	if (I != map.end()) {
@@ -70,7 +108,7 @@ inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::stri
 	}
 }
 
-template <>
+template<>
 inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::string& name, const std::function<void(const Atlas::Message::IntType&)>& callback) {
 	auto I = map.find(name);
 	if (I != map.end()) {
@@ -80,7 +118,7 @@ inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::stri
 	}
 }
 
-template <>
+template<>
 inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::string& name, const std::function<void(const Atlas::Message::StringType&)>& callback) {
 	auto I = map.find(name);
 	if (I != map.end()) {
@@ -90,7 +128,7 @@ inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::stri
 	}
 }
 
-template <>
+template<>
 inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::string& name, const std::function<void(const Atlas::Message::FloatType&)>& callback) {
 	auto I = map.find(name);
 	if (I != map.end()) {
@@ -100,7 +138,7 @@ inline void AtlasQuery::find(const Atlas::Message::MapType& map, const std::stri
 	}
 }
 
-template <typename T, typename R>
+template<typename T, typename R>
 inline R AtlasQuery::extract(const Atlas::Message::Element& element, const std::string& name, const std::function<R(const T&)>& callback) {
 	if (element.isMap()) {
 		return extract(element.Map(), name, callback);
@@ -109,7 +147,7 @@ inline R AtlasQuery::extract(const Atlas::Message::Element& element, const std::
 }
 
 
-template <typename R>
+template<typename R>
 inline R AtlasQuery::extract(const Atlas::Message::MapType& map, const std::string& name, const std::function<R(const Atlas::Message::MapType&)>& callback) {
 	auto I = map.find(name);
 	if (I != map.end()) {
@@ -120,6 +158,68 @@ inline R AtlasQuery::extract(const Atlas::Message::MapType& map, const std::stri
 	return {};
 }
 
+template<>
+inline bool AtlasQuery::elementCheck<Atlas::Message::StringType>(const Atlas::Message::Element& element) {
+	return element.isString();
+}
+
+template<>
+inline bool AtlasQuery::elementCheck<Atlas::Message::FloatType>(const Atlas::Message::Element& element) {
+	return element.isFloat();
+}
+
+template<>
+inline bool AtlasQuery::elementCheck<Atlas::Message::MapType>(const Atlas::Message::Element& element) {
+	return element.isMap();
+}
+
+template<>
+inline bool AtlasQuery::elementCheck<Atlas::Message::ListType>(const Atlas::Message::Element& element) {
+	return element.isList();
+}
+
+template<>
+inline bool AtlasQuery::elementCheck<Atlas::Message::IntType>(const Atlas::Message::Element& element) {
+	return element.isInt();
+}
+
+template<>
+inline Atlas::Message::StringType AtlasQuery::elementValue(const Atlas::Message::Element& element) {
+	return element.String();
+}
+
+template<>
+inline Atlas::Message::FloatType AtlasQuery::elementValue(const Atlas::Message::Element& element) {
+	return element.Float();
+}
+
+template<>
+inline Atlas::Message::MapType AtlasQuery::elementValue(const Atlas::Message::Element& element) {
+	return element.Map();
+}
+
+template<>
+inline Atlas::Message::ListType AtlasQuery::elementValue(const Atlas::Message::Element& element) {
+	return element.List();
+}
+
+template<>
+inline Atlas::Message::IntType AtlasQuery::elementValue(const Atlas::Message::Element& element) {
+	return element.Int();
+}
+
+template<typename R>
+inline boost::optional<R> AtlasQuery::extract(const Atlas::Message::Element& element, const std::string& name) {
+	if (element.isMap()) {
+		auto I = element.Map().find(name);
+		if (I != element.Map().end()) {
+			if (elementCheck<R>(I->second)) {
+				return elementValue<R>(I->second);
+			}
+		}
+	}
+	return {};
+}
 
 }
 
