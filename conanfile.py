0a62cfe77e5a174065add114918dea7596728a77
Erik Ogenvik <erik@ogenvik.org>
2022-12-15 00:12:26 +0100

    Resolve Conan conflicts.

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class EmberConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     requires = ["cegui/0.8.7@worldforge/stable",
                 "ogre/13.4.2@worldforge/stable",
-                "sdl/2.24.1",
+                "sdl/2.26.0",
                 "boost/1.79.0",
                 "bullet3/2.89",
                 "openal/1.22.2",
@@ -21,7 +21,9 @@ class EmberConan(ConanFile):
                 # We need to resolve libiconv
                 "libiconv/1.17",
                 # We need to resolve openssl since it's used by both libcurl (used by wfut) and pulseaudio (used by openal presumably)
-                "openssl/1.1.1s"]
+                "openssl/1.1.1s",
+                # Resolve to fix conflict: "'xkbcommon/1.4.1' requires 'libxml2/2.9.14' while 'wayland/1.21.0' requires 'libxml2/2.10.3'."
+                "libxml2/2.10.3"]
 
     generators = ["cmake"]
 
