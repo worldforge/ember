3339cb9c1c7115a68d2d537026ee27dd039b9dc4
Erik Ogenvik <erik@ogenvik.org>
2023-01-21 20:40:49 +0100

    Fix warning in CMake.

--- tools/cmake/FindSigC++.cmake
+++ tools/cmake/FindSigC++.cmake
@@ -1,43 +1,43 @@
 # - Try to find SigC++-2.0
 # Once done this will define
 #
-#  SIGC++_FOUND - system has SigC++
-#  SIGC++_INCLUDE_DIR - the SigC++ include directory
-#  SIGC++_LIBRARY - SigC++ library
+#  SigC++_FOUND - system has SigC++
+#  SigC++_INCLUDE_DIR - the SigC++ include directory
+#  SigC++_LIBRARY - SigC++ library
 
-if(SIGC++_INCLUDE_DIR AND SIGC++_LIBRARIES)
+if(SigC++_INCLUDE_DIR AND SigC++_LIBRARIES)
     # Already in cache, be silent
-    set(SIGC++_FIND_QUIETLY TRUE)
-endif(SIGC++_INCLUDE_DIR AND SIGC++_LIBRARIES)
+    set(SigC++_FIND_QUIETLY TRUE)
+endif(SigC++_INCLUDE_DIR AND SigC++_LIBRARIES)
 
 if (NOT WIN32)
     include(UsePkgConfig)
-    pkgconfig(sigc++-2.0 _LibSIGC++IncDir _LibSIGC++LinkDir _LibSIGC++LinkFlags _LibSIGC++Cflags)
+    pkgconfig(sigc++-2.0 _LibSigC++IncDir _LibSigC++LinkDir _LibSigC++LinkFlags _LibSigC++Cflags)
 endif(NOT WIN32)
 
-find_path(SIGC++_INCLUDE_DIR sigc++/sigc++.h
+find_path(SigC++_INCLUDE_DIR sigc++/sigc++.h
         PATH_SUFFIXES sigc++-2.0
-        PATHS ${_LibSIGC++IncDir} )
+        PATHS ${_LibSigC++IncDir} )
 
-find_library(SIGC++_LIBRARY
+find_library(SigC++_LIBRARY
         NAMES sigc-2.0
-        PATHS ${_LibSIGC++LinkDir} )
+        PATHS ${_LibSigC++LinkDir} )
 
-get_filename_component(sigc++LibDir "${SIGC++_LIBRARY}" PATH)
+get_filename_component(sigc++LibDir "${SigC++_LIBRARY}" PATH)
 
 # search the sigc++config.h include dir under the same root where the library is found
 
-find_path(SIGC++_CONFIG_INCLUDE_DIR sigc++config.h
+find_path(SigC++_CONFIG_INCLUDE_DIR sigc++config.h
         PATH_SUFFIXES sigc++-2.0/include
-        PATHS ${_LibSIGC++IncDir} "${sigc++LibDir}" ${CMAKE_SYSTEM_LIBRARY_PATH})
+        PATHS ${_LibSigC++IncDir} "${sigc++LibDir}" ${CMAKE_SYSTEM_LIBRARY_PATH})
 
 # not sure if this include dir is optional or required
 # for now it is optional
-if(SIGC++_CONFIG_INCLUDE_DIR)
-    set(SIGC++_INCLUDE_DIR ${SIGC++_INCLUDE_DIR} "${SIGC++_CONFIG_INCLUDE_DIR}")
-endif(SIGC++_CONFIG_INCLUDE_DIR)
+if(SigC++_CONFIG_INCLUDE_DIR)
+    set(SigC++_INCLUDE_DIR ${SigC++_INCLUDE_DIR} "${SigC++_CONFIG_INCLUDE_DIR}")
+endif(SigC++_CONFIG_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SIGC++  DEFAULT_MSG  SIGC++_LIBRARY SIGC++_INCLUDE_DIR)
+find_package_handle_standard_args(SigC++  DEFAULT_MSG  SigC++_LIBRARY SigC++_INCLUDE_DIR)
 
-mark_as_advanced(SIGC++_INCLUDE_DIR SIGC++_LIBRARY)
+mark_as_advanced(SigC++_INCLUDE_DIR SigC++_LIBRARY)
