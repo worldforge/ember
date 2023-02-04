21ec86d8e5626e35db58256d1248607496a86ee4
Erik Ogenvik <erik@ogenvik.org>
2023-02-04 16:36:15 +0100

    Mark deps as overridden.

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,6 @@ class EmberConan(ConanFile):
     requires = ["cegui/0.8.7@worldforge/stable",
                 "ogre/13.4.2@worldforge/stable",
                 "sdl/2.26.1",
-                "boost/1.81.0",
                 "bullet3/2.89",
                 # This version builds, whereas other variants of 1.22.2 don't.
                 # See https://github.com/conan-io/conan-center-index/issues/13772
@@ -18,21 +17,22 @@ class EmberConan(ConanFile):
                 "libwfut/0.2.4@worldforge/testing",
                 "varconf/1.0.3@worldforge/testing",
                 "lua/5.3.5",
-                "zlib/1.2.13",
-                "bzip2/1.0.8",
+                ("boost/1.81.0", "override"),
+                ("zlib/1.2.13", "override"),
+                ("bzip2/1.0.8", "override"),
                 # We need to resolve libiconv
-                "libiconv/1.17",
+                ("libiconv/1.17", "override"),
                 # We need to resolve openssl since it's used by both libcurl (used by wfut) and pulseaudio (used by openal presumably)
-                "openssl/1.1.1s",
+                ("openssl/1.1.1s", "override"),
                 # Resolve to fix conflict: "'xkbcommon/1.4.1' requires 'libxml2/2.9.14' while 'wayland/1.21.0' requires 'libxml2/2.10.3'."
-                "libxml2/2.10.3"]
+                ("libxml2/2.10.3", "override")]
 
     generators = "cmake_find_package", "cmake_paths"
 
     def requirements(self):
         if self.settings.os == "Linux":
             # We need to resolve libalsa
-            self.requires("libalsa/1.2.7.2")
+            self.requires("libalsa/1.2.7.2", override=True)
 
     def imports(self):
         self.copy("*.dll", src="bin", dst="bin")
