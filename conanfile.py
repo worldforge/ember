48f38cb3572687a3792060a05e144838802691da
Erik Ogenvik <erik@ogenvik.org>
2023-05-19 01:06:30 +0200

    Ignore Windows for now, and fix requirements.

--- conanfile.py
+++ conanfile.py
@@ -5,22 +5,25 @@ from conan.tools.cmake import cmake_layout, CMakeDeps, CMakeToolchain
 class EmberConan(ConanFile):
     package_type = "application"
     settings = "os", "arch", "compiler", "build_type"
-    requires = ["cegui/0.8.7@worldforge",
-                "ogre/13.4.2@worldforge",
-                "sdl/2.26.1",
-                "bullet3/2.89",
-                "openal-soft/1.22.2",
-                "libxdg-basedir/1.2.3@worldforge",
-                "atlas/0.7.0@worldforge",
-                "eris/1.4.0@worldforge",
-                "mercator/0.4.0@worldforge",
-                "wfut/0.2.4@worldforge",
-                "varconf/1.0.3@worldforge",
-                "sigc++/2.10.0@worldforge",
-                "lua/5.3.5",
-                # We need to resolve openssl since it's used by both libcurl (used by wfut) and pulseaudio (used by openal presumably)
-                "openssl/1.1.1t"
-                ]
+    def requirements(self):
+        self.requires("cegui/0.8.7@worldforge")
+        self.requires("ogre/13.4.2@worldforge")
+        self.requires("sdl/2.26.1")
+        self.requires("bullet3/2.89")
+        self.requires("openal-soft/1.22.2")
+
+        self.requires("atlas/0.7.0@worldforge")
+        self.requires("eris/1.4.0@worldforge")
+        self.requires("mercator/0.4.0@worldforge")
+        self.requires("wfut/0.2.4@worldforge")
+        self.requires("varconf/1.0.3@worldforge")
+        self.requires("sigc++/2.10.0@worldforge")
+        self.requires("lua/5.3.5")
+        if self.settings.compiler != 'msvc':
+            self.requires("libxdg-basedir/1.2.3@worldforge")
+        # We need to resolve openssl since it's used by both libcurl (used by wfut) and pulseaudio (used by openal presumably)
+#        self.requires("openssl/1.1.1t", override=True)
+        self.requires("libxml2/2.10.4", override=True)
 
     def generate(self):
         deps = CMakeDeps(self)
